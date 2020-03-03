7cf718b798af27dc0486b5210e020a3578cda4dd
Don-Stevenson <dsteven1@gmail.com>
2020-03-02 19:08:13 -0500

    working function, does not change original array

--- learnFilter.js
+++ learnFilter.js
@@ -3,5 +3,8 @@
 const arr = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const filterByGreaterThan3 = (arr) => {
-    
+ return arr.filter(num => num > 3)   
 }
+
+console.log(filterByGreaterThan3(arr))
+console.log(arr)
