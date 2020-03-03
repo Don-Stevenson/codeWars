c97071c0de36e2505efd7e679fbd9ae2248ff61e
Don-Stevenson <dsteven1@gmail.com>
2020-03-02 19:13:59 -0500

    works with words

--- learnFilter.js
+++ learnFilter.js
@@ -1,10 +1,16 @@
 // learning filter ...
 
-const arr = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const arr = ["hey", "Jude", "don't", "make", "it", "bad"]
 
-const filterByGreaterThan3 = (arr) => {
- return arr.filter(num => num > 3)   
+// const filterByGreaterThan3 = (arr) => {
+//  return arr.filter(num => num > 3)   
+// }
+
+// console.log(filterByGreaterThan3(arr))
+// console.log(arr)
+
+const doubleDigitNos = (array) =>{
+    return array.filter(word => word.length > 3)
 }
 
-console.log(filterByGreaterThan3(arr))
-console.log(arr)
+console.log(doubleDigitNos(arr))
