5c3678c3d07a71850c457f87aa317b563f9bff08
Don-Stevenson <dsteven1@gmail.com>
2020-03-08 18:00:46 -0400

    refactor map and test solution

--- pigLatinGenerator.js
+++ pigLatinGenerator.js
@@ -1,20 +1,19 @@
 // with map and test
 // *****************
 
-// const pigIt = str => {
-//   let newStr = str.split(" ");
-//   let pigLatinStr = newStr.map(element =>
-//     /[a-zA-Z]+/.test(element)
-//       ? element.slice(1) + element[0] + "ay"
-//       : element).join(" ")
-//    return pigLatinStr
-// };
+const pigIt = str => {
+  let newStr = str.split(" ");
+  return pigLatinStr = newStr.map(element =>
+    /[a-zA-Z]+/.test(element)
+      ? element.slice(1) + element[0] + "ay"
+      : element).join(" ")
+};
 
 // with replace
 // where \w is the first character and \w+ is the rest of the word
 // which got to $1 and $2 respectively
 // ************
-const pigIt = (str) => {return str.replace(/(\w)(\w+)/g, '$2$1ay')};
+// const pigIt = (str) => {return str.replace(/(\w)(\w+)/g, '$2$1ay')};
 
 
 
