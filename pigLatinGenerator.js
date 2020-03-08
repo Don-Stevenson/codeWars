79cca460780af92fd39853105d04f96f709de1d6
Don-Stevenson <dsteven1@gmail.com>
2020-03-08 17:50:06 -0400

    working with map and test

--- pigLatinGenerator.js
+++ pigLatinGenerator.js
@@ -1,3 +1,12 @@
-const pigIt = () => {
-    
-}
+// with map and test
+
+const pigIt = str => {
+  let newStr = str.split(" ");
+  let pigLatinStr = newStr.map(element =>
+    /[a-zA-Z]+/.test(element)
+      ? element.slice(1) + element[0] + "ay"
+      : element).join(" ")
+   return pigLatinStr
+};
+
+console.log(pigIt("hello Pig latin is cool"));
