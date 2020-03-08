7b15701fb059dc5bd878ac8e06e91426b885e488
Don-Stevenson <dsteven1@gmail.com>
2020-03-08 19:02:37 -0400

    working with forEach

--- generateHashtag.js
+++ generateHashtag.js
@@ -1,12 +1,23 @@
-// function that takes in a str and returns a hashtag
+// // function that takes in a str and returns a hashtag
 
+// const generateHashtag = str => {
+//   let newStrArr = str.split(" ");
+//   let newArr = [];
+//   for (let word of newStrArr) {
+//     newArr.push(word.charAt(0).toUpperCase() + word.slice(1));
+//   }
+//   return `#${newArr.join("")}`;
+// };
+
+
+// with a forEach
 const generateHashtag = str => {
-  let newStrArr = str.split(" ");
-  let newArr = [];
-  for (let word of newStrArr) {
-    newArr.push(word.charAt(0).toUpperCase() + word.slice(1));
-  }
-  return `#${newArr.join("")}`;
+    let newStrArr = str.split(" ");
+    let newArr = [];
+    newStrArr.forEach(element => {
+        newArr.push(element.charAt(0).toUpperCase() + element.slice(1));
+    });
+    return `#${newArr.join("")}`;
 };
 
 console.log(generateHashtag("hi there I am great"));
