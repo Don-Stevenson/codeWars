94a7c2a7bc4d74a0e5d6e05dab7015899b201244
Don-Stevenson <dsteven1@gmail.com>
2020-03-08 18:58:46 -0400

    working with for loop

--- generateHashtag.js
+++ generateHashtag.js
@@ -1,6 +1,12 @@
 // function that takes in a str and returns a hashtag
 
-const generateHashtag = (str) => {
-    
+const generateHashtag = str => {
+  let newStrArr = str.split(" ");
+  let newArr = [];
+  for (let word of newStrArr) {
+    newArr.push(word.charAt(0).toUpperCase() + word.slice(1));
+  }
+  return `#${newArr.join("")}`;
+};
 
-}
+console.log(generateHashtag("hi there I am great"));
