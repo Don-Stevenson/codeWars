e16e90128fedbfd1d9f21e9f53f84a858a33e41e
Don-Stevenson <dsteven1@gmail.com>
2020-03-10 23:46:07 -0400

    trying replace

--- stripComments.js
+++ stripComments.js
@@ -1,3 +1,13 @@
 function solution(input, markers) {
-  
-};
+  let inputArr = input.split(" ");
+  inputArr.forEach(element => {
+    element.trim();
+  });
+
+  inputArr.replace(
+    markers.forEach(element => {
+      element, ""
+    })
+  );
+  console.log(inputArr);
+}
