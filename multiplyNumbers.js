d4231fced1d0aad8248f320bba54f1df5f48cb38
Don-Stevenson <dsteven1@gmail.com>
2020-03-12 23:42:33 -0400

    first attempt

---
+++ multiplyNumbers.js
@@ -0,0 +1,8 @@
+const multiply = (a, b) => {
+  const aNum = parseInt(a);
+  const bNum = parseInt(b);
+  return (product = (aNum * bNum).toString());
+};
+
+//expect 6
+console.log(multiply(2, 3));
