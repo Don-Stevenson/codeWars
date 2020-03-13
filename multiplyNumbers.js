003ed5abbc4c587dc40428c440fd1884d014ae3a
Don-Stevenson <dsteven1@gmail.com>
2020-03-12 23:50:40 -0400

    returning scientific notation for large numbers

--- multiplyNumbers.js
+++ multiplyNumbers.js
@@ -1,8 +1,8 @@
 const multiply = (a, b) => {
-  const aNum = parseInt(a);
-  const bNum = parseInt(b);
-  return product = (aNum * bNum).toString();
+//   const aNum = parseInt(a);
+//   const bNum = parseInt(b);
+  return (a * b).toString()
 };
 
 //expect 6
-console.log(multiply(2, 3));
+console.log(multiply(21234565678686889979979, 66868881234489779975677));
