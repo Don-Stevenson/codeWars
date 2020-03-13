2271191c28eee2d6711aab44c4ada4a9a15383d7
Don-Stevenson <dsteven1@gmail.com>
2020-03-13 00:06:11 -0400

    with a do while using modulus

--- multiplyNumbers.js
+++ multiplyNumbers.js
@@ -1,8 +1,16 @@
 const multiply = (a, b) => {
-//   const aNum = parseInt(a);
-//   const bNum = parseInt(b);
-  return (a * b).toLocaleString('fullwide', {useGrouping:false})
+  let num = a * b;
+  const toFix = num => {
+    let string = "";
+    do {
+      let a = num % 10;
+      num = Math.trunc(num / 10);
+      string = a + string;
+    } while (num > 0);
+    return string;
+  };
+  return toFix(num);
 };
 
 //expect 141993165043408650000000000000000000
-console.log(multiply(21234565678686889979979, 66868881234489779975677));
+console.log(multiply(21234565678686889979979, 103036868881234489779975677));
