d5631e696461f191ba28f692f6676b9aabe6e019
Don-Stevenson <dsteven1@gmail.com>
2020-03-13 00:38:23 -0400

    add some comments

--- multiplyNumbers.js
+++ multiplyNumbers.js
@@ -1,3 +1,5 @@
+//returns a large string but loses accuracy over 16 digits or so
+
 // const multiply = (a, b) => {
 //   let num = a * b;
 //   const toFix = num => {
@@ -12,20 +14,24 @@
 //   return toFix(num);
 // };
 
-// trying with return strings to an array
-function multiply(a, b) {
-  const product = Array(a.length + b.length).fill(0);
-  for (let i = a.length; i--; null) {
-    let carry = 0;
-    for (let j = b.length; j--; null) {
-      product[1 + i + j] += carry + a[i] * b[j];
-      carry = Math.floor(product[1 + j + i] / 10);
-      product[1 + i + j] = product[1 + i + j] % 10;
-    }
-    product[i] += carry;
-  }
-  return product.join("").replace(/^0*(\d)/, "$1");
-}
 
-//expect 21879431595849229244644464444866428842804828260
-console.log(multiply("34", "566"));
+
+// trying with return strings to an array
+// function multiply(a, b) {
+//   const product = Array(a.length + b.length).fill(0);
+//   for (let i = a.length; i--; null) {
+//     let carry = 0;
+//     for (let j = b.length; j--; null) {
+//       product[1 + i + j] += carry + a[i] * b[j];
+//       carry = Math.floor(product[1 + j + i] / 10);
+//       product[1 + i + j] = product[1 + i + j] % 10;
+//     }
+//     product[i] += carry;
+//   }
+//   return product.join("").replace(/^0*(\d)/, "$1");
+// }
+
+// //working with above code
+// console.log(multiply("3465657577757757778799997997965665454777", "5980800080800800677565658688887899808776765566"));
+
+// trying with BigInteger
