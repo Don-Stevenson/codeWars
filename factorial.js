a6943b6c21520767a2fa5def8d36207c38ebf035
Don-Stevenson <dsteven1@gmail.com>
2020-03-16 20:43:13 -0400

    add description and try rangeError

--- factorial.js
+++ factorial.js
@@ -1,3 +1,10 @@
-const factorial = (n) => {
+// take in a number and return a non negative factorial product of all the numbers.
+// if n is below 0 or above 12, throw a range error
 
-}
+const factorial = n => {
+  if (n < 0 || n > 12) {
+    return RangeError;
+  } else console.log(n);
+};
+
+console.log(factorial(2));
