7c9373faef99d19bcb13da9e7c192ec122829792
Don-Stevenson <dsteven1@gmail.com>
2020-03-20 16:11:25 -0400

    working with foreach

--- codewarForMar18.js
+++ codewarForMar18.js
@@ -1,7 +1,12 @@
 // takes in an array and mulitplies by given number
 // ************************************************
 const multiplyByX = (arr, num) => {
-    console.log("arr is ", arr + " num is ", num )
-}
+  let newArr = [];
+  arr.forEach(element => {
+    newArr.push(element * num);
+  });
+  console.log("newarr is ", newArr + " num is ", num);
+  return newArr;
+};
 
-console.log(multiplyByX([1,2,3], 3))
+console.log(multiplyByX([1, 2, 3], 3));
