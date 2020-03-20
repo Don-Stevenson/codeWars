c4c61ec34cba94423caa36166f9fab3f9f2f4c67
Don-Stevenson <dsteven1@gmail.com>
2020-03-20 16:06:13 -0400

    working with array and num going in

--- codewarForMar18.js
+++ codewarForMar18.js
@@ -1,7 +1,7 @@
 // takes in an array and mulitplies by given number
 // ************************************************
 const multiplyByX = (arr, num) => {
-    return'does something'
+    console.log("arr is ", arr + " num is ", num )
 }
 
-console.log(multiplyByX())
+console.log(multiplyByX([1,2,3], 3))
