264b06a17e55144916c67021573ff3f4b70bb2ea
Don-Stevenson <dsteven1@gmail.com>
2020-03-23 22:59:32 -0400

    with map setup

--- codewarForMar18.js
+++ codewarForMar18.js
@@ -10,14 +10,21 @@
 // };
 
 
-// with a for loop
-//*********************************/
-const multiplyByX = (arr, num) => {
-  let newArr = [];
-  for (const item of arr) {
-    newArr.push(item * num);
-  }
-  return newArr;
-};
+// // with a for loop
+// //*********************************/
+// const multiplyByX = (arr, num) => {
+//   let newArr = [];
+//   for (const item of arr) {
+//     newArr.push(item * num);
+//   }
+//   return newArr;
+// };
 
-console.log(multiplyByX([1, 2, 3], 3));
+// console.log(multiplyByX([1, 2, 3], 3));
+
+
+// with map
+//********************************** */
+const multiplyByX = (arr, num) => {
+  
+}
