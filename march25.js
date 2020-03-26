28f76ae2cf11a5a47091ae138cda717fbed1e980
Don-Stevenson <dsteven1@gmail.com>
2020-03-26 00:08:59 -0400

    working with c style loop

--- march25.js
+++ march25.js
@@ -1,12 +1,21 @@
 // function that reverse str
+// with split, reverse and join
 //***************************/
 
+// const reverseString = str => {
+//   let reverseStr = str
+//     .split("")
+//     .reverse()
+//     .join("");
+//   return reverseStr;
+// };
+
 const reverseString = str => {
-  let reverseStr = str
-    .split("")
-    .reverse()
-    .join("");
-  return reverseStr;
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
 };
 
 console.log(reverseString("hello"));
