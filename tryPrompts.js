62a89611fd9c89ecfc404cbbb2d06bbc2a803ae6
Don-Stevenson <dsteven1@gmail.com>
2020-05-06 16:39:41 -0400

    bugs fixed

--- tryPrompts.js
+++ tryPrompts.js
@@ -1,24 +1,36 @@
-const prompts = require('prompts');
- 
+const prompts = require("prompts");
+
 const questions = [
   {
-    type: 'number',
-    name: 'A',
-    message: 'What is the first number',
-    initial: 'Enter a number between 1 and 1000000',
-    validate: value => value === typeof(1) ? "please enter a number" : true
+    type: "number",
+    name: "A",
+    message: "What is the first number",
+    initial: "Enter a number between 1 and 1000",
+    validate: value => {
+      if (value > 1000) {
+        return "^^^ please enter a number less than 1000";
+      } else if (value < 0) {
+        return "^^^ please enter a number greater than 0";
+      } else return true;
+    }
   },
   {
-    type: 'number',
-    name: 'B',
-    message: 'What is the second number',
-    initial: 'Enter a number between 1 and 1000000',
-    validate: value => value > 1000 ? "please enter a number between 1 and 1000" : true
-  },
+    type: "number",
+    name: "B",
+    message: "What is the second number",
+    initial: "Enter a number between 1 and 1000000",
+    validate: value =>
+      value > 1000 ? "please enter a number between 1 and 1000" : true
+  }
 ];
- 
+
 (async () => {
   const response = await prompts(questions);
- 
-  console.log(`The answer for ${response.A} X ${response.B} is:`, response.A * response.B)
+
+  console.log(
+    `
+    The answer for ${response.A} X ${response.B} is: 
+    ${response.A * response.B}
+    thank you for using our multiplication program`
+  );
 })();
