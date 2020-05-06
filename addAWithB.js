96e042342779023d30352dcc2a8295b974cdbc28
Don-Stevenson <dsteven1@gmail.com>
2020-05-05 20:59:27 -0400

    adding argvs

--- addAWithB.js
+++ addAWithB.js
@@ -2,8 +2,8 @@ let args = process.argv.slice(2)
 
 
 
-const addTwoNumbers = (a) => {
-  return a;
+const addTwoNumbers = () => {
+  return args[0] + args[1]
 };
 
 console.log(addTwoNumbers(args));
