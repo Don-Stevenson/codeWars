04bfe59706e0ef393581d88d8d42c1d32bb574a8
Don-Stevenson <dsteven1@gmail.com>
2020-05-05 21:02:34 -0400

    with comments

--- addAWithB.js
+++ addAWithB.js
@@ -1,7 +1,6 @@
 let args = process.argv.slice(2)
 
-
-
+// function that adds two numbers from the process argvs array. Uses node.js to run
 const addTwoNumbers = () => {
   return parseInt(args[0]) + parseInt(args[1])
 };
