56e93c06ce36ee3eb07a596e89a4506f7ab65394
Don-Stevenson <dsteven1@gmail.com>
2020-05-07 12:47:23 -0400

    update messages

--- tryPrompts.js
+++ tryPrompts.js
@@ -8,7 +8,6 @@ const questions = [
     Welcome to the Simple Math Program!
     Please enter two numbers to be calculated
     Please chose an operator( +, -, *, / )?`,
-    initial: "+, -, x, /",
     validate: value => {
       if (value === "+" || value === "-" || value === "*" || value === "/") {
         return true;
@@ -18,8 +17,8 @@ const questions = [
   {
     type: "number",
     name: "B",
-    message: "  What is the first number?",
-    initial: "Enter a number between 1 and 1000",
+    message: "  Enter a number between 1 and 1000",
+    initial: "Enter value here",
     validate: value => {
       if (value > 1000 || value < 0 || isNaN(parseFloat(value))) {
         return "Error!! Please enter a number between 0 and 1000";
@@ -29,8 +28,8 @@ const questions = [
   {
     type: "number",
     name: "C",
-    message: "  What is the second number?",
-    initial: "Enter a number between 1 and 1000",
+    message: "  Enter a second number between 1 and 1000",
+    initial: "Enter value here",
     validate: value => {
       if (value > 1000 || value < 0 || isNaN(parseFloat(value))) {
         return "Error!! Please enter a number between 0 and 1000";
@@ -53,7 +52,8 @@ const questions = [
   }
   console.log(
     `
-  ${response.B} ${response.operator} ${response.C} = ${answer}
+    ${response.B} ${response.operator} ${response.C} = ${answer}
+
     Thank you for using The Simple Math Program!!!
     `
   );
