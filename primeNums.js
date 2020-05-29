8648ac6bababcce48dd5b2a04bbc1dcdb917999a
Don-Stevenson <dsteven1@gmail.com>
2020-05-29 12:58:12 -0400

    add comments, remove zero from outer loop because zero is not a prime number

--- primeNums.js
+++ primeNums.js
@@ -1,17 +1,23 @@
+// a function that returns all the prime numbers between 1 and n
+// *************************************************************
+
 const primeNums = n => {
+  // set up an empty array hold the prime numbers
   const result = [];
-  for (let i = 0; i < n; i++) {
+  // C style loop to count from 1 to n
+  for (let i = 1; i <= n; i++) {
+    // set the counter to 0 for each instance of i
     let counter = 0;
+    // Nested C style loop that counts down from whatever i is at the moment to 1
     for (let j = i; j >= 1; j--) {
+      // check to see if i is divisible by j without remainder, if it is increase the count by 1
       if (i % j === 0) counter++;
     }
+    // if counter is exactly equal to two, this is a prime number so push it into the results array
     if (counter === 2) result.push(i);
-    //   console.log(counter);
   }
-  console.log(result);
+  // return the result array
   return result;
 };
 
-primeNums(12);
-
-
+console.log(primeNums(5));
