13bcde14d356192e31ffde07256d55c1b2a15680
Don-Stevenson <dsteven1@gmail.com>
2020-05-29 12:42:55 -0400

    working

--- primeNums.js
+++ primeNums.js
@@ -1,15 +1,17 @@
 const primeNums = n => {
   const result = [];
-  for (let i = 0; (i = n); i++) {
+  for (let i = 0; i < n; i++) {
     let counter = 0;
     for (let j = i; j >= 1; j--) {
       if (i % j === 0) counter++;
-      if (counter === 2) result.push(i);
-    //   console.log(counter);
     }
+    if (counter === 2) result.push(i);
+    //   console.log(counter);
   }
-  console.log(result)
+  console.log(result);
   return result;
 };
 
 primeNums(12);
+
+
