3120c401302398c21447ea90a50a3d2cc3a80543
Don-Stevenson <dsteven1@gmail.com>
2020-05-29 12:39:36 -0400

    trouble shooting

--- primeNums.js
+++ primeNums.js
@@ -1,7 +1,15 @@
-const primeNums = (n) => {
+const primeNums = n => {
+  const result = [];
+  for (let i = 0; (i = n); i++) {
+    let counter = 0;
+    for (let j = i; j >= 1; j--) {
+      if (i % j === 0) counter++;
+      if (counter === 2) result.push(i);
+    //   console.log(counter);
+    }
+  }
+  console.log(result)
+  return result;
+};
 
-for (let i = 1; i < n; i++)
-    console.log(i)
-}
-
-console.log(primeNums(12))
+primeNums(12);
