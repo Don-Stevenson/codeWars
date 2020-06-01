200b02395926218af510860e8311450b7662b325
Don-Stevenson <dsteven1@gmail.com>
2020-06-01 15:26:53 -0400

    update comments

--- recursive.js
+++ recursive.js
@@ -1,11 +1,12 @@
 // practicing recursion to find embeded elements with in an array
+//**************************************************************/
 
-let arr = [[[[1, [[2, [[3, ['b']]]]]]]]]
+let arr = [[[[[1, [[2, [[3, ['b']]]]]]]]]]
 
 const findLetterB = (arr) => {
     for (let item of arr) {
         if (Array.isArray(item)) {
-            console.log('going to the next nested array')
+            console.log('...going to the next nested array')
             return findLetterB(item);
         } else if (item === 'b') {
             return `found ${item} here`;
@@ -16,6 +17,7 @@ const findLetterB = (arr) => {
 console.log(findLetterB(arr))
 
 // counting up with recursion
+//***************************/
 const countUsingRecursion = (count) => {
     while (count < 10) {
         console.log(count);
@@ -23,9 +25,10 @@ const countUsingRecursion = (count) => {
     }
     return count;
 }
-console.log(countUsingRecursion(0))
+// console.log(countUsingRecursion(0))
 
 // counting down with recursion
+//*****************************/
 const countDownFrom = (count) => {
     while (count >= 0) {
         console.log(count);
@@ -33,21 +36,23 @@ const countDownFrom = (count) => {
     }
     return count;
 }
-console.log(countDownFrom(10))
+// console.log(countDownFrom(10))
 
 // old way of counting up, c style loop
+//*************************************/
 const oldCounter = (countTo) => {
     for (let i = 0; i <= countTo; i++) {
         console.log(`the count is: ${i}`);
     }
 }
 
-oldCounter(10);
+// oldCounter(10);
 
-// old way of counting down, c style loop
+// old way of counting down, c style loop 
+//***************************************/
 const oldCountDown = (countFrom) => {
     for (let i = countFrom; i >= 0; i--) {
         console.log(`the count is: ${i}`);
     }
 }
-oldCountDown(10);
+// oldCountDown(10);
