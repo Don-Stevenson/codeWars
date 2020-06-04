39454a1093a018f463386b9d3a06e6d44cdc3c89
Don-Stevenson <dsteven1@gmail.com>
2020-06-04 10:15:24 -0400

    update string to reverse

--- march25.js
+++ march25.js
@@ -27,4 +27,4 @@ const reverseString = str => {
   return(str === "") ? "" : reverseString(str.substr(1)) + str.charAt(0);
 };
 
-console.log(reverseString("hello"));
+console.log(reverseString("12345"));
