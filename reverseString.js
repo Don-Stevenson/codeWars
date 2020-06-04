85c15d410ac0d90454923d100be33ee4e3fcbbad
Don-Stevenson <dsteven1@gmail.com>
2020-06-04 10:16:49 -0400

    add different ways to reverse a string

---
+++ reverseString.js
@@ -0,0 +1,31 @@
+// function that reverse str
+// with split, reverse and join
+//***************************/
+
+// const reverseString = str => {
+//   let reverseStr = str
+//     .split("")
+//     .reverse()
+//     .join("");
+//   return reverseStr;
+// };
+
+// with a c style loop
+
+// const reverseString = str => {
+//   let newStr = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     newStr += str[i];
+//   }
+//   return newStr;
+// };
+
+// with recursion
+// **************
+
+const reverseString = str => {
+    return(str === "") ? "" : reverseString(str.substr(1)) + str.charAt(0);
+  };
+  
+  console.log(reverseString("12345"));
+  
