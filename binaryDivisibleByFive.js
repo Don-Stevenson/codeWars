74138c1ddfbd761d43d1f9218a4b6fb3a49f1e6c
Don-Stevenson <dsteven1@gmail.com>
2020-06-04 10:23:56 -0400

    update comments

--- binaryDivisibleByFive.js
+++ binaryDivisibleByFive.js
@@ -1,11 +1,12 @@
 const divisibleByFive = (str) => {
-    //convert binary num in string to a number
+    // convert binary num in string to a number
     let num = parseInt(str, 2)
+    // prints out what the number is
     console.log("str is ", num)
 
-    //is that num % 5 === 0
+    // is that num % 5 === 0
     return num % 5 === 0 ? true : false
 }
 
-console.log(divisibleByFive('101'))
+console.log(divisibleByFive('001'))
 console.log(divisibleByFive('1010101010010001'))
