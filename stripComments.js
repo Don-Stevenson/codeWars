f40f294d2217eca64848b3a15522316f15a378cc
Don-Stevenson <dsteven1@gmail.com>
2020-06-04 11:38:27 -0400

    update comments

--- stripComments.js
+++ stripComments.js
@@ -1,5 +1,16 @@
+//function that takes in a string and takes out an item
+// that you enter in a addition to input
+
 function solution(input, markers) {
+  // splits the input arr in to each letter as an item
   let inputArr = input.split("");
+  // designates the markers as what needs to be taken out 
+  // (Note: all elements in markers will be deleted)
   let forDeletion = markers;
+  // fiters out the input array, by only returning whats not for deleting,
+  // and joins it back into a string
   return inputArr.filter(item => !forDeletion.includes(item)).join("");
 }
+
+console.log(solution("this is a show", "isahows"))
+// returns t
