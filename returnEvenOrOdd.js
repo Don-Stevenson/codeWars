1ddb32328e02f268ddf801e9051375e8783cefbe
Don-Stevenson <dsteven1@gmail.com>
2020-06-04 10:10:26 -0400

    add comments

--- returnEvenOrOdd.js
+++ returnEvenOrOdd.js
@@ -1,9 +1,16 @@
 let integers = [2, 4, 0, 100, 4, 11, 2602, 36]
 
+// function that returns which integer is the outlier, either odd or even
+// **********************************************************************
 function findOutlier(integers) {
+  // make an array that is only the even integers
   let evenIntsArr = integers.filter(x => x % 2 === 0);
+  // make an array that is only the odd integers
   let oddIntsArr = integers.filter(x => x % 2 !== 0);
+  
+  // if odds array is greater than one, then the outlier integer is even
+  // if the odds array is not greater than one, then the outlier integer is odd
   return oddIntsArr.length > 1 ? evenIntsArr[0] : oddIntsArr[0];
 }
 
-findOutlier(integers)
+console.log(findOutlier(integers))
