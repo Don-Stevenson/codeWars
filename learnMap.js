1522e2c8920d38ef30bd5b1a70ff48ec8a902aa9
Don-Stevenson <dsteven1@gmail.com>
2020-06-15 00:18:56 -0400

    add different scenarios

--- learnMap.js
+++ learnMap.js
@@ -1,7 +1,18 @@
-const array = [1,2,3,4,5,6,7]
+const array = ["a", "b", 3, 4, 5, 6, 7];
 
-//takes in an array, transforms it with a function and returns a new array 
-const mappedArray = array.map(x => x + 1)
+//takes in an array, transforms it with a function and returns a new array
+// returns a true or false statement
+// [false, false, fales, true, true, etc]
+// const mappedArray = array.map(x => x > 3)
 
-console.log("mapped array is", mappedArray)
-console.log("array is still", array)
+// same as squaring each element by itself
+//const mappedArray = array.map(x => x * x)
+
+// returns true or false depending if x === 3
+// const mappedArray = array.map(x => x === 3)
+
+// gives nan where element is a string
+const mappedArray = array.map(x => x * 3);
+
+console.log("mapped array is", mappedArray);
+console.log("array is still", array);
