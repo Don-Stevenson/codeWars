b74f2814ef5cb69e2539ecaee31c3f5fa26abd76
Don-Stevenson <dsteven1@gmail.com>
2020-06-16 17:28:13 -0400

    add pseudo code, set up function

---
+++ whatKindOfTriangle.js
@@ -0,0 +1,10 @@
+// given the 3 sides determine the type of triangle: scalene, equalateral, isoceles
+
+// first takein the 3 side lengths, determin if it is a tringle at all
+
+// if yes, then what type if a = b = c, eqaulateral, a= b or b = c or a = c, isocolese, if not but still a triangle
+// return the type
+
+const whatKindOFTriangle = (a,b,c) => {
+    
+}
