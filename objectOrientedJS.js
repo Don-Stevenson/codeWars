1a44b8b0beba1ec83be5c5a8cef7206b24593683
Don-Stevenson <dsteven1@gmail.com>
2020-07-13 10:35:31 -0400

    update square to work as power

--- objectOrientedJS.js
+++ objectOrientedJS.js
@@ -1,7 +1,6 @@
 let a;
 let b;
 
-
 // object to store the different operations
 // and formula functions
 //******************************************
@@ -10,17 +9,17 @@ const operations = {
   minus: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => a / b,
-  square: (a, b) => Math.pow(a, 2),
+  power: (a, b) => Math.pow(a, b),
   root: a => Math.sqrt(a)
 };
 
-// a higher order function that calls the 
+// a higher order function that calls the
 // above object of operations and passes values
 // ********************************************
 const mathify = (value, a, b) => {
   return operations[value](a, b);
 };
 
-console.log(mathify("square", 8));
+console.log(mathify("power", 3, 3));
 
-console.log(mathify("root",  9));
+console.log(mathify("root", 9));
