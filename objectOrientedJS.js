22a21810d2bf8f0e7143e84ecb4166bf3caa7fa1
Don-Stevenson <dsteven1@gmail.com>
2020-07-13 00:58:02 -0400

    add sqrt and square

--- objectOrientedJS.js
+++ objectOrientedJS.js
@@ -6,14 +6,14 @@ const operations = {
   minus: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => a / b,
-  square: (a, b) => a * a
- 
+  square: (a, b) => Math.pow(a, 2),
+  root: a => Math.sqrt(a)
 };
 
 const mathify = (value, a, b) => {
   return operations[value](a, b);
 };
 
-console.log(mathify("square", 8, 9));
+console.log(mathify("square", 8));
 
-
+console.log(mathify("root",  9));
