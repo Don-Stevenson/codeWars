2f8093e56b2d5968ad6d1e84eb9156b6d72e5621
Don-Stevenson <dsteven1@gmail.com>
2020-07-17 16:29:14 -0400

    trying with null, parseFloat and isNaN

--- parseFloat.js
+++ parseFloat.js
@@ -1,19 +1,20 @@
 // a place to try out parse float and NaN
 // **************************************
 
-console.log(parseFloat("tree")); // => NaN
-console.log(parseFloat(345)); // => 345
-console.log(parseFloat("345")); // => 345
-console.log("345"); //=> 345 as a string
+// console.log(parseFloat("tree")); // => NaN
+// console.log(parseFloat(345)); // => 345
+// console.log(parseFloat("345")); // => 345
+// console.log("345"); //=> 345 as a string
 
-console.log(isNaN("345")); // => false
-console.log(isNaN(345)); // => false
-console.log(isNaN("Hello")); // => false
+// console.log(isNaN("345")); // => false
+// console.log(isNaN(345)); // => false
+// console.log(isNaN("Hello")); // => false
 
-console.log(isNaN(parseFloat("Hello"))); // => True
+// console.log(isNaN(parseFloat("Hello"))); // => True
 
-console.log(isNaN(parseFloat("345"))); 
-// => False - parses the number in parseFloat
-//and then evaluates that as a number, returning false
+// console.log(isNaN(parseFloat("345"))); 
+// // => False - parses the number in parseFloat
+// //and then evaluates that as a number, returning false
 
-console.log(isNaN(parseFloat(345))); // => False
+// console.log(isNaN(parseFloat(345))); // => False
+console.log(isNaN(parseFloat(null))); // => True this is not a number
