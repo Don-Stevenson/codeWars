d8e8804390081a690b60ab579706ebccdb04845a
Don-Stevenson <dsteven1@gmail.com>
2020-07-27 18:42:47 -0400

    getting values from date

---
+++ getDateAndTime.js
@@ -0,0 +1,7 @@
+let date = "12/08/2020"
+
+let day = parseInt(date.slice(0,2))
+let month = parseInt(date.slice(3,5))
+let year = parseInt(date.slice(6,8))
+console.log(day)
+console.log(month, year)
