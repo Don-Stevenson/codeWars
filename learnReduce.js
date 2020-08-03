2446f5c5c94cc8e6882438c26881806206a76851
Don-Stevenson <dsteven1@gmail.com>
2020-08-03 10:08:33 -0400

    trying reduce in with different arrays, writing it out better

--- learnReduce.js
+++ learnReduce.js
@@ -1,11 +1,23 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 const reducer = (accumalator, currentVal, currentIndex, array) => {
-  console.log(accumalator, currentVal, currentIndex, array);
+  console.log(`
+  accumalotor is: ${accumalator}
+  current Value: ${currentVal}
+  current Index: ${currentIndex}
+  array is: ${array}`);
+  return accumalator += currentVal;
 };
 
-//takes in a an array and the runs a callback function on the array
-let reducedArray = array.reduce(reducer);
+
+
+// //takes in a an array and the runs a callback function on the array
+// let reducedArray = array.reduce(reducer);
 
 // in this case should expect 28 to be the answer
-reducedArray;
+// console.log(reducedArray);
+
+
+const letterArr = ["a", "b","c"]
+
+console.log(letterArr.reduce(reducer))
