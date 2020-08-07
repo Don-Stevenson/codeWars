538e8cf9ddb038b884b2ae2b2a1ba33fe7ab2d4a
Don-Stevenson <dsteven1@gmail.com>
2020-08-07 11:56:28 -0400

    using reduce to add objects within an object

--- learnReduce.js
+++ learnReduce.js
@@ -1,32 +1,40 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
+// const array = [1, 2, 3, 4, 5, 6, 7];
 
-const obj = {
-  a: 1,
-  b: 2,
-  c: 3
+// const obj = {
+//   a: 1,
+//   b: 2,
+//   c: 3
 
-}
+// }
 
-const reducer = (accumalator, currentVal, currentIndex, array) => {
-  console.log(`
-  accumalator is: ${accumalator}
-  current Value: ${currentVal}
-  current Index: ${currentIndex}
-  array is: ${array}`);
-  return accumalator += currentVal;
+// const reducer = (accumalator, currentVal, currentIndex, object) => {
+//   console.log(`
+//   accumalator is: ${accumalator}
+//   current Value: ${currentVal}
+//   current Index: ${currentIndex}
+//   object is: ${object}`);
+//   return accumalator += currentVal;
+// };
+
+// // //takes in a an array and the runs a callback function on the array
+// // let reducedArray = array.reduce(reducer);
+
+// // in this case should expect 28 to be the answer
+// // console.log(reducedArray);
+
+// // note that index is not zero based, rather it starts at 1.
+
+// // with a string , result is abcef
+// const letterArr = ["a", "b","c","d","e","f"]
+
+
+
+const addObj = {
+  a: { value: 1 },
+  b: { value: 2 },
+  c: { value: 3 }
 };
 
+const total = Object.values(addObj).reduce((t,  {value} ) => t + value, 0);
 
-
-// //takes in a an array and the runs a callback function on the array
-// let reducedArray = array.reduce(reducer);
-
-// in this case should expect 28 to be the answer
-// console.log(reducedArray);
-
-// note that index is not zero based, rather it starts at 1.
-
-// with a string , result is abcef
-const letterArr = ["a", "b","c","d","e","f"]
-
-console.log(letterArr.reduce(reducer))
+console.log(total); // 6
