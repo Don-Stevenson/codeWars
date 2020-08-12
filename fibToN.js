d0743cf682684a513358b040aa8e0add5f207723
Don-Stevenson <dsteven1@gmail.com>
2020-08-12 13:31:40 -0400

    returning positive fib numbers

--- fibToN.js
+++ fibToN.js
@@ -3,17 +3,29 @@
 // fib(0) := 0
 // fib(1) := 1
 // fin(n + 2) := fib(n + 1) + fib(n)
-// Write an algorithm that can handle n up to 2000000.
+// Write an algorithm that can handle n up to 2,000,000.
 
 // Your algorithm must output the exact integer answer, to full precision. Also, it must correctly handle negative numbers as input.
 
-// HINT I: Can you rearrange the equation fib(n + 2) = fib(n + 1) + fib(n) to find fib(n) if you already know fib(n + 1) and fib(n + 2)? 
+// HINT I: Can you rearrange the equation fib(n + 2) = fib(n + 1) + fib(n) to find fib(n) if you already know fib(n + 1) and fib(n + 2)?
 // Use this to reason what value fib has to have for negative values.
 
 // HINT II: See https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-11.html#%_sec_1.2.4
 
-const fib = (n) => {
-    return 0*n;
-  }
+// generate a list of numbers
+// create a list of fib numbers to n
 
-  console.log(fib(2))
+const fib = n => {
+  let a = 1;
+  let b = 0;
+  let temp;
+  n -= 1;
+  while (n >= 0) {
+    (temp = a), (a = a + b);
+    b = temp;
+    n--;
+  }
+  return BigInt(b);
+};
+
+console.log(fib(10));
