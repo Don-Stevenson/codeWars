1b491801d0005af0682001df37ecf2545b7bba2a
Don-Stevenson <dsteven1@gmail.com>
2020-08-16 19:50:58 -0400

    update obj values

--- learnReduce.js
+++ learnReduce.js
@@ -30,9 +30,9 @@ const reducer = (accumalator, currentVal, currentIndex, array) => {
 // const letterArr = ["a", "b","c","d","e","f"]
 
 const addObj = {
-  a: 1,
-  b: 2,
-  c: 3
+  a: 2,
+  b: 4,
+  c: 6
 };
 
 // const total =
