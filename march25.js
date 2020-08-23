34835ff8af639b2169645d87bdac72b164410f82
Don-Stevenson <dsteven1@gmail.com>
2020-08-23 15:53:56 -0400

    update reverse string

--- march25.js
+++ march25.js
@@ -13,18 +13,21 @@
 // with a c style loop
 
 // const reverseString = str => {
-//   let newStr = "";
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     newStr += str[i];
+//   if (str === "") return "";
+//   else {
+//     let newStr = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//       newStr += str[i];
+//     }
+//     return newStr;
 //   }
-//   return newStr;
 // };
 
 // with recursion
 // **************
 
 const reverseString = str => {
-  return(str === "") ? "" : reverseString(str.substr(1)) + str.charAt(0);
+  return str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
 };
 
-console.log(reverseString("12345"));
+console.log(reverseString("ABCDE"));
