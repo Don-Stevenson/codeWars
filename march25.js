392575146b51dfbd59dd9fa80864d10508e07ee1
Don-Stevenson <dsteven1@gmail.com>
2020-08-27 19:27:23 -0400

    refactor

--- march25.js
+++ march25.js
@@ -26,8 +26,7 @@
 // with recursion
 // **************
 
-const reverseString = str => {
-  return str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
-};
+const reverseString = str => str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
+
 
 console.log(reverseString("ABCDE"));
