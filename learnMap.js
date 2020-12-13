d1bc5138fbd7b1cacab406cf4e26819b6af63676
Don-Stevenson <dsteven1@gmail.com>
2020-12-13 13:34:52 -0500

    update comments

--- learnMap.js
+++ learnMap.js
@@ -1,6 +1,7 @@
 const array = ["a", "b", 3, 4, 5, 6, 7];
 
-//takes in an array, transforms it with a function and returns a new array
+// takes in an array, transforms it with a function 
+// and returns a new array
 // returns a true or false statement
 // [false, false, fales, true, true, etc]
 // const mappedArray = array.map(x => x > 3)
