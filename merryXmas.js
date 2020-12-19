474707ca3a0763ddeea50b9b0a9cf60199027890
Don-Stevenson <dsteven1@gmail.com>
2020-12-19 18:22:31 -0500

    printing merry christmas but in the wrong order

--- merryXmas.js
+++ merryXmas.js
@@ -2,9 +2,24 @@
 // ******************************
 
 const merryXmas = () => {
-  const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+  const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !";
   for (let letter of alphBetStr) {
     if (letter === "M") console.log(letter)
+    if (letter === "E") console.log(letter)
+    if (letter === "R") console.log(letter)
+    if (letter === "R") console.log(letter)
+    if (letter === "Y") console.log(letter)
+    if (letter === " ") console.log(letter)
+    if (letter === "C") console.log(letter)
+    if (letter === "H") console.log(letter)
+    if (letter === "R") console.log(letter)
+    if (letter === "I") console.log(letter)
+    if (letter === "S") console.log(letter)
+    if (letter === "T") console.log(letter)
+    if (letter === "M") console.log(letter)
+    if (letter === "A") console.log(letter)
+    if (letter === "S") console.log(letter)
+    if (letter === "!") console.log(letter)
   }
 };
 
