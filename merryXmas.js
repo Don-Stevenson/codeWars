17e2a2057daded919c1446cf935099c9190f69d0
Don-Stevenson <dsteven1@gmail.com>
2020-12-19 18:16:28 -0500

    prints out alpha bet via loop

--- merryXmas.js
+++ merryXmas.js
@@ -1,6 +1,11 @@
 // function that prints merryXmas
 // ******************************
 
-const merryXmas =() => {
+const merryXmas = () => {
+  const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let letter of alphBetStr) {
+    console.log(letter);
+  }
+};
 
-}
+merryXmas()
