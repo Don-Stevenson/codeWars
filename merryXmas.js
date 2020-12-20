a08da93c03e46c7c6cf16246b672aa0477b1695c
Don-Stevenson <dsteven1@gmail.com>
2020-12-20 12:26:41 -0500

    make horizontal message

--- merryXmas.js
+++ merryXmas.js
@@ -3,24 +3,9 @@
 
 const merryXmasWisher = () => {
   const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !";
-  console.log (`
-  ${alphBetStr[12]}
-  ${alphBetStr[4]} 
-  ${alphBetStr[17]}
-  ${alphBetStr[17]}
-  ${alphBetStr[24]}
-  ${alphBetStr[26]}
-  ${alphBetStr[2]}
-  ${alphBetStr[7]}
-  ${alphBetStr[17]}
-  ${alphBetStr[8]}
-  ${alphBetStr[18]}
-  ${alphBetStr[19]}
-  ${alphBetStr[12]}
-  ${alphBetStr[0]}
-  ${alphBetStr[18]}
-  ${alphBetStr[27]}
-  `)
-}
+  console.log(
+    `${alphBetStr[12]}${alphBetStr[4]}${alphBetStr[17]}${alphBetStr[17]}${alphBetStr[24]}${alphBetStr[26]}${alphBetStr[2]}${alphBetStr[7]}${alphBetStr[17]}${alphBetStr[8]}${alphBetStr[18]}${alphBetStr[19]}${alphBetStr[12]}${alphBetStr[0]}${alphBetStr[18]}${alphBetStr[27]}`
+  );
+};
 
-merryXmasWisher()
+merryXmasWisher();
