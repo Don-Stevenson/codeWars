6666344a615485ab0bed6c74206651189d052c72
Don-Stevenson <dsteven1@gmail.com>
2020-12-19 21:54:34 -0500

    with template literals

--- merryXmas.js
+++ merryXmas.js
@@ -3,22 +3,24 @@
 
 const merryXmasWisher = () => {
   const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !";
-  console.log(alphBetStr[12]);
-  console.log(alphBetStr[4]);
-  console.log(alphBetStr[17]);
-  console.log(alphBetStr[17]);
-  console.log(alphBetStr[24]);
-  console.log(alphBetStr[26]);
-  console.log(alphBetStr[2]);
-  console.log(alphBetStr[7]);
-  console.log(alphBetStr[17]);
-  console.log(alphBetStr[8]);
-  console.log(alphBetStr[18]);
-  console.log(alphBetStr[19]);
-  console.log(alphBetStr[12]);
-  console.log(alphBetStr[0]);
-  console.log(alphBetStr[18]);
-  console.log(alphBetStr[27]);
-};
+ return console.log (`
+  ${alphBetStr[12]}
+  ${alphBetStr[4]} 
+  ${alphBetStr[17]}
+  ${alphBetStr[17]}
+  ${alphBetStr[24]}
+  ${alphBetStr[26]}
+  ${alphBetStr[2]}
+  ${alphBetStr[7]}
+  ${alphBetStr[17]}
+  ${alphBetStr[8]}
+  ${alphBetStr[18]}
+  ${alphBetStr[19]}
+  ${alphBetStr[12]}
+  ${alphBetStr[0]}
+  ${alphBetStr[18]}
+  ${alphBetStr[27]}
+  `)
+}
 
-merryXmasWisher();
+merryXmasWisher()
