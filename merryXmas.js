01d7165c3308143e1bcd9eda0c6200e449d8c856
Don-Stevenson <dsteven1@gmail.com>
2020-12-21 19:52:13 -0500

    add some pseudo code for the project goals

--- merryXmas.js
+++ merryXmas.js
@@ -1,11 +1,22 @@
 // function that prints merry Xmas!
-// *********************************
+// ********************************
 
 const merryXmasWisher = () => {
   const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !";
-  console.log(
-    `${alphBetStr[12]}${alphBetStr[4]}${alphBetStr[17]}${alphBetStr[17]}${alphBetStr[24]}${alphBetStr[26]}${alphBetStr[2]}${alphBetStr[7]}${alphBetStr[17]}${alphBetStr[8]}${alphBetStr[18]}${alphBetStr[19]}${alphBetStr[12]}${alphBetStr[0]}${alphBetStr[18]}${alphBetStr[27]}`
-  );
-};
+  // console.log(
+  //   `${alphBetStr[12]}${alphBetStr[4]}${alphBetStr[17]}${alphBetStr[17]}${alphBetStr[24]}${alphBetStr[26]}${alphBetStr[2]}${alphBetStr[7]}${alphBetStr[17]}${alphBetStr[8]}${alphBetStr[18]}${alphBetStr[19]}${alphBetStr[12]}${alphBetStr[0]}${alphBetStr[18]}${alphBetStr[27]}`
+  // );
+ 
+  // random number generator to 1 27
+  // call it for character we want
+  // 5 space 10
+  // if gen numb 12 print alpha string[gen num]
+  // printing something while you wait for the results/
+  // print current random number???
+
+  // start by placing XXXXXXXXXXXXX 
+  // filling in the X's and fill in the message
+
+}
 
 merryXmasWisher();
