f47d08565a925c641914ce1d41311372e499730f
Don-Stevenson <dsteven1@gmail.com>
2021-01-03 12:41:14 -0500

    trying another type of map

--- learnMap.js
+++ learnMap.js
@@ -10,10 +10,10 @@ const array = ["a", "b", 3, 4, 5, 6, 7];
 // const mappedArray = array.map(x => x * x)
 
 // returns true or false depending if x === 3
-const mappedArray = array.map(x => x === 3)
+// const mappedArray = array.map(x => x === 3)
 
 // gives nan where element is a string
-// const mappedArray = array.map(x => x * 3);
+const mappedArray = array.map(x => x * 3);
 
 console.log(`mapped array is: ${mappedArray} 
 array is still: ${array}`)
