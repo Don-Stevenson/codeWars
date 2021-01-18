fa9a7cfc701a92378dca8bd50da805521933f603
Don-Stevenson <dsteven1@gmail.com>
2021-01-18 12:55:55 -0500

    add subtractTwoNums Function

--- march24.js
+++ march24.js
@@ -2,11 +2,20 @@
 // ****************************
 
 const addOne = (num) => num++;
-console.log(addOne(8));
+// console.log(addOne(8));
 
 // basic function that adds two numbers
 // ************************************
 
 const addTwoNums = (A, B) => A + B;
 
-console.log(addTwoNums(11, 33))
+// console.log(addTwoNums(11, 33))
+
+
+// basic function that adds subtracts two numbers
+// ************************************
+
+const substractTwoNums = (A, B) => A - B ;
+
+
+console.log(substractTwoNums(11, 33))
