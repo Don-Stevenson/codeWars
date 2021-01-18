8bdcd1f93ce433423affee6ae0984f8fc2b1b152
Don-Stevenson <dsteven1@gmail.com>
2021-01-18 00:11:53 -0500

    add addTwoNums function

--- march24.js
+++ march24.js
@@ -1,5 +1,12 @@
 // basic function that adds one
 // ****************************
 
-const addOne = num => num++;
+const addOne = (num) => num++;
 console.log(addOne(8));
+
+// basic function that adds two numbers
+// ************************************
+
+const addTwoNums = (A, B) => A + B;
+
+console.log(addTwoNums(11, 33))
