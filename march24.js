7533a0e6b9806d2c82e019ee24eabdafcb5ebc39
Don-Stevenson <dsteven1@gmail.com>
2021-01-21 12:10:03 -0500

    update comments

--- march24.js
+++ march24.js
@@ -4,6 +4,8 @@
 const addOne = (num) => num++;
 // console.log(addOne(8));
 
+
+
 // basic function that adds two numbers
 // ************************************
 
@@ -12,26 +14,50 @@ const addTwoNums = (A, B) => A + B;
 // console.log(addTwoNums(11, 33))
 
 
-// basic function that adds subtracts two numbers
-// ************************************
+
+
+// basic function that subtracts two numbers
+// ******************************************
 
 const substractTwoNums = (A, B) => A - B ;
 
 
+
+
 // console.log(substractTwoNums(11, 33))
 
+// basic function that divides two numbers
+// ***************************************
 
 const divideTwoNums = (A, B) => A / B ;
 
 // console.log(divideTwoNums(25,2))
 
+
+
+
+// basic function that multiplys two numbers
+// ******************************************
+
 const mulitplyTwoNums = (A, B) => A  *B ;
 
 // console.log(multiplyTwoNums(25,2))
 
+
+
+
+// basic function that sqaures a number
+// ************************************
+
 const squareOneNum = (A) => A * A ;
 
 // console.log(squareOneNum(9))
+
+
+
+
+// basic function that finds the square root of a number
+// *****************************************************
 
 const sqrRootNum = (A) => Math.sqrt(A) ;
 
