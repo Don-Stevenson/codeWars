d5c15c66ecda970116998641b0734e20cd8a7c56
Don-Stevenson <dsteven1@gmail.com>
2021-02-03 12:40:43 -0500

    add title, trying before and after default variable has been updated

--- passingDefaults.js
+++ passingDefaults.js
@@ -1,6 +1,10 @@
+// a function to try out passing default values into a function
+// ************************************************************
+
 const doesFoo = (a = "foo") => {
+    console.log(`${a} has been completed`);
   a = "junk";
-  console.log(`${a} has been completed`);
+  console.log(`Now ${a} has been completed`);
 };
 
 doesFoo();
