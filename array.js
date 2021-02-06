2553eece5317cb05e3a42c3afc6868c28b483ea8
Don-Stevenson <dsteven1@gmail.com>
2021-02-06 16:48:28 -0500

    add comments, refactor code

--- array.js
+++ array.js
@@ -1,12 +1,14 @@
-let array = [1,2,3,4,5,6,7,8,9]
+// function that updates an array's values into a new array by adding one
+// **********************************************************************
 
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+ 
 const addOne = (groupOfNumbers) => {
   let newNumbers = [];
-  for(let item of groupOfNumbers){
-    item = item + 1;
-    newNumbers.push(item);
+  for (const item of groupOfNumbers) {
+    newNumbers.push(item + 1);
   }
   return newNumbers;
-}
+};
 
 console.log(addOne(array));
