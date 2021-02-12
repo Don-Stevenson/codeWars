378a557ca2d1a9a42ef5974cbaaf41f231a1c80f
Don-Stevenson <dsteven1@gmail.com>
2021-02-11 23:45:32 -0500

    add function that takes in 2 numbers and multiplies them

--- addAWithB.js
+++
@@ -1,9 +0,0 @@
-let args = process.argv.slice(2)
-
-// function that adds two numbers from the process argv's array. Uses node.js to run
-// **********************************************************************************
-const multiplyTwoNumbers = () => {
-  return parseInt(args[0]) * parseInt(args[1])
-};
-
-console.log(multiplyTwoNumbers(args));
---
+++ multiplyAWithB.js
@@ -0,0 +1,13 @@
+// Multiply with Args
+// ******************
+
+
+// function that multiply with two numbers from the process argv's array. 
+// *********************************************************************
+const args = process.argv.slice(2)
+
+const multiplyTwoNumbers = () => {
+  return parseInt(args[0]) * parseInt(args[1])
+};
+
+console.log(multiplyTwoNumbers(args));
