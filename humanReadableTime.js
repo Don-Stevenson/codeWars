00af453f122331089f6f621801569ffd05d111ce
Don-Stevenson <dsteven1@gmail.com>
2021-02-20 13:55:39 -0500

    update to ternary statements, update comments and format

--- humanReadableTime.js
+++ humanReadableTime.js
@@ -1,25 +1,26 @@
-// function that turns a number of seconds into hour,
-//  min and sec format
-// needs refactoring on the logic of mins, secs,
+// Function that turns a number of seconds into hour,
+// min and sec format
+// *************************************************
+
+// Notes: needs refactoring on the logic of mins, secs,
 // hours, but produces the requires and passes
-const date = new Date().toLocaleDateString("en-US", {weekday: "long", day: "numeric", month: "long"});
+const date = new Date().toLocaleDateString("en-US", {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+});
 console.log({ date });
 
 const humanReadable = (seconds) => {
-  let mins = Math.floor(seconds / 60);
+  const mins = Math.floor(seconds / 60);
   let hours = Math.floor(mins / 60);
   let minRemain = mins - hours * 60;
   let secsRemain = seconds - mins * 60;
 
-  if (minRemain < 10) {
-    minRemain = `0${minRemain}`;
-  }
-  if (secsRemain < 10) {
-    secsRemain = `0${secsRemain}`;
-  }
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
+  minRemain < 10 ? (minRemain = `0${minRemain}`) : minRemain;
+  secsRemain < 10 ? (secsRemain = `0${secsRemain}`) : secsRemain;
+  hours < 10 ? (hours = `0${hours}`) : hours;
+
   return `${hours}:${minRemain}:${secsRemain}`;
 };
 console.log(humanReadable(30021));
