9f55bb74729fcd7edb80c4578d86ab631c878888
Don-Stevenson <dsteven1@gmail.com>
2021-02-22 17:05:28 -0500

    update to const, add {} for console logs

--- getDateAndTime.js
+++ getDateAndTime.js
@@ -1,7 +1,7 @@
-let date = "12/08/2020"
+const date = "12/08/2020"
 
-let day = parseInt(date.slice(0,2))
-let month = parseInt(date.slice(3,5))
-let year = parseInt(date.slice(6,8))
-console.log(day)
-console.log(month, year)
+const day = parseInt(date.slice(0,2))
+const month = parseInt(date.slice(3,5))
+const year = parseInt(date.slice(6,8))
+console.log({day})
+console.log({month}, {year})
