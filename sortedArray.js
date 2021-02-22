17d99da8e01a724e6f9fe53f9bc8a9baf5c10897
Don-Stevenson <dsteven1@gmail.com>
2021-02-22 17:00:53 -0500

    add arrow function

--- sortedArray.js
+++ sortedArray.js
@@ -1,11 +1,10 @@
-let array = [1, 4, 5, 6, 3]
+let array = [1, 4, 5, 6, 3];
 
-function sortArray(array) {
+const sortArray = (array) => {
   const odds = array
-    .filter(x => x % 2)
-    .sort((a, b) => a - b);
-  return array
-    .map(x => x % 2 ? odds.shift() : x);
-}
+  .filter((x) => x % 2)
+  .sort((a, b) => a - b);
+  return array.map((x) => (x % 2 ? odds.shift() : x));
+};
 
-console.log(sortArray(array))
+console.log(sortArray(array));
