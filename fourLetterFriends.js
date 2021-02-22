cf89b9648b688023f65761afbfae37ae4f8b12af
Don-Stevenson <dsteven1@gmail.com>
2021-02-22 17:07:57 -0500

    update to const + arrow function

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -1,6 +1,6 @@
-let friends = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
+const friends = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
 
-function friend(friends){
- return fourLetterFriends = friends.filter(x => x.length === 4);
-}
-console.log(friend(friends))
+const friend = (friends) => {
+  return (fourLetterFriends = friends.filter((x) => x.length === 4));
+};
+console.log(friend(friends));
