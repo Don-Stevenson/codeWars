db766c69472b9336f4cd6e5bcc0baf94883221af
Don-Stevenson <dsteven1@gmail.com>
2021-02-22 16:57:31 -0500

    update to arrow function, let to const, rename function

--- stripComments.js
+++ stripComments.js
@@ -1,17 +1,17 @@
-//function that takes in a string and takes out an item
+// function that takes in a string and takes out an item
 // that you enter in a addition to input
 // ****************************************************
 
-function solution(input, markers) {
+const removeMarkers = (input, markers) => {
   // splits the input arr in to each letter as an item
-  let inputArr = input.split("");
-  // designates the markers as what needs to be taken out 
+  const inputArr = input.split("");
+  // designates the markers as what needs to be taken out
   // (Note: all elements in markers will be deleted)
-  let forDeletion = markers;
+  const forDeletion = markers;
   // fiters out the input array, by only returning whats not for deleting,
   // and joins it back into a string
-  return inputArr.filter(item => !forDeletion.includes(item)).join("");
-}
+  return inputArr.filter((item) => !forDeletion.includes(item)).join("");
+};
 
-console.log(solution("this is a show", "isahows"))
+console.log(removeMarkers("this is a show", "isahows"));
 // returns t
