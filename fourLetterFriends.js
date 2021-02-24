a90ce2ed15a98618db3c1f901e59390b42049712
Don-Stevenson <dsteven1@gmail.com>
2021-02-24 11:03:32 -0500

    update variable names

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -1,6 +1,6 @@
 const friends = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
 
-const friend = (friends) => {
-  return (fourLetterFriends = friends.filter((x) => x.length === 4));
+const findFriend = (friends) => {
+  return (fourLetterFriends = friends.filter((element) => element.length === 4));
 };
-console.log(friend(friends));
+console.log(findFriend(friends));
