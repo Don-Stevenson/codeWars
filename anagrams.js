6b984d684d88d3f58478b51548d20b0635fff92f
Don-Stevenson <dsteven1@gmail.com>
2021-02-23 21:05:03 -0500

    update comments, update to arrow function

--- anagrams.js
+++ anagrams.js
@@ -1,14 +1,17 @@
+// function that checks a word against a sorted array of words
+// and returns an arrray of anagrams of the submitted word
+// *******************************************************
 
-function anagrams(word, words) {
-  let wordSorted = word.split('').sort().join('');
-  let anagramArr = []
+const anagrams = (word, words) => {
+  let wordSorted = word.split("").sort().join("");
+  let anagramArr = [];
   for (let item of words) {
-    let wordsSorted = item.split('').sort().join('');
+    const wordsSorted = item.split("").sort().join("");
     if (wordSorted === wordsSorted) {
-      anagramArr.push(item)
+      anagramArr.push(item);
     }
   }
   return anagramArr;
-}
+};
 
-console.log(anagrams('hello', ['hello', 'hi', 'ehllo', 'face', 'olelh']))
+console.log(anagrams("hello", ["hello", "hi", "ehllo", "face", "olelh"]));
