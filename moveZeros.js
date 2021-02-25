525f816f173391e4fddc79dfe62cf83ba1434bf0
Don-Stevenson <dsteven1@gmail.com>
2021-02-25 12:30:45 -0500

    add explanation and expected result

--- moveZeros.js
+++ moveZeros.js
@@ -1,8 +1,13 @@
+// function that moves zeros to the end of an array
+
 const moveZeros = (arr) => {
   let arrOfZeros = [];
   let arrWithoutZeros = [];
-  for (let item of arr) {
+  for (const item of arr) {
     item === 0 ? arrOfZeros.push(item) : arrWithoutZeros.push(item);
   }
-  return finalArr = [...arrWithoutZeros, ...arrOfZeros]
-}
+  return (finalArr = [...arrWithoutZeros, ...arrOfZeros]);
+};
+
+console.log(moveZeros([0, 1, 4, 45, 0, 0, 7, 9]));
+// expected result [  1, 4, 45, 7,  9, 0,  0, 0 ]
