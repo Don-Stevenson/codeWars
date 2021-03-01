5178047d4736cf068f88bcd995d7ad972aa37897
Don-Stevenson <dsteven1@gmail.com>
2021-02-28 19:51:35 -0500

    update function description and expected results

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -1,6 +1,11 @@
+// function that takes in an array and 
+// returns an array with elements that's length is equal to 4 
+// **********************************************************
+
 const friendsArr = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
 
 const findFriend = (array) => {
   return (fourLetterFriends = array.filter((element) => element.length === 4));
 };
 console.log(findFriend(friendsArr));
+// expect ['Ryan']
