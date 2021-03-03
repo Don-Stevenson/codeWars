4179375ca902d015f55318e3760f64cbfee131f0
Don-Stevenson <dsteven1@gmail.com>
2021-03-02 23:29:30 -0500

    update spacing

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -7,5 +7,6 @@ const friendsArr = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
 const findFriend = (array) => {
   return (fourLetterFriends = array.filter((element) => element.length === 4));
 };
+
 console.log(findFriend(friendsArr));
 // expect ['Ryan']
