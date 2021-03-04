0af92066b2c93e7f88decb105eca0cb77222cdb2
Don-Stevenson <dsteven1@gmail.com>
2021-03-03 20:11:08 -0500

    set up problem and beginning function

---
+++ letterCountInObject.js
@@ -0,0 +1,16 @@
+// take in a string and update the number of those letters in an object
+
+// "hello" =>
+// returns
+// {
+//  h:1
+//  e:1
+//  l:2
+//  o:1
+//}
+
+const letterCounter = (sentence) => {
+
+
+};
+
