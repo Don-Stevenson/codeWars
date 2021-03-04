8128117b94befe60036a908d5d7ae4c377d36ab4
Don-Stevenson <dsteven1@gmail.com>
2021-03-03 20:25:05 -0500

    c style loop but not updating object yet

--- letterCountInObject.js
+++ letterCountInObject.js
@@ -9,8 +9,18 @@
 //  o:1
 //}
 
-const letterCounter = (sentence) => {
-
-
+const letterCounter = (word) => {
+  let obj = {};
+  for (let i = 0; i < word.length; i++) {
+    // console.log(word[i]);
+    if (!obj[i]) {
+    //   console.log("right here", word[i]);
+      obj += word[i];
+      console.log("obj", Object.values(obj))
+    } else obj += word[i];
+    // console.log("hi", word[i]);
+  }
+  return obj;
 };
 
+letterCounter("hello");
