ad61fdd2542f72e43770b0ef0da53286881a1b74
Don-Stevenson <dsteven1@gmail.com>
2021-03-04 19:08:59 -0500

    add comments to explain the function

--- letterCountInObject.js
+++ letterCountInObject.js
@@ -9,16 +9,29 @@
 //  o:1
 //}
 
+// declare the function and take in a word as a paramater
 const letterCounter = (word) => {
+
+  // set an empty object
   let obj = {};
+  
+  // split word into an array of letters
   const wordLetters = word.split("");
 
+  // for every leller in the array
   for (const letter of wordLetters) {
+    
+    // set the key to each letter
     const key = letter;
+    
+    // set the value to one
     const val = 1;
 
+    // if the object has the letter as a key already then update the object's value at that key +1
     if (obj[key]) {
       obj[key] = obj[key] + 1;
+
+      // else set the object key to the letter and set the value to 1
     } else obj[key] = val;
   }
   return obj;
