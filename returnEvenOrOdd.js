8c10f29ccc05ff0c470ccf164dbea4f2f34f202b
Don-Stevenson <dsteven1@gmail.com>
2021-03-06 17:17:49 -0500

    remove uncessary brackets on params

--- returnEvenOrOdd.js
+++ returnEvenOrOdd.js
@@ -2,7 +2,7 @@ const integers = [2, 4, 0, 100, 4, 11, 2602, 36]
 
 // function that returns which integer is the outlier, either odd or even
 // **********************************************************************
-const findOutlier = (integers) => {
+const findOutlier = integers => {
   // make an array that is only the even integers
   const evenIntsArr = integers.filter(x => x % 2 === 0);
   // make an array that is only the odd integers
