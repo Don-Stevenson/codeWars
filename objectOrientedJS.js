31ae752a5742e2db2fdc53d37ab0e1e7c24d2750
Don-Stevenson <dsteven1@gmail.com>
2021-03-07 10:14:06 -0500

    add handle invalid operations

--- objectOrientedJS.js
+++ objectOrientedJS.js
@@ -7,18 +7,28 @@ const operationsObj = {
   multiply: (a, b) => a * b,
   divide: (a, b) => a / b,
   power: (a, b) => Math.pow(a, b),
-  root: a => Math.sqrt(a)
+  root: (a) => Math.sqrt(a),
 };
 
 // a higher order function that calls the
 // above object of operations and passes operations
 // ********************************************
 const mathify = (operation, a, b) => {
-  return operationsObj[operation](a, b);
+  if (
+    operation === "root" ||
+    operation === "plus" ||
+    operation === "minus" ||
+    operation === "multiply" ||
+    operation === "divide" ||
+    operation === "power"
+  ) {
+    return operationsObj[operation](a, b);
+  } else
+    return "Try again, operation must only be: plus, minus, multiply, divide, power, root";
 };
 
 console.log(mathify("power", 3, 3));
 // expect 27
 
-console.log(mathify("root", 9));
+console.log(mathify("rot", 9));
 // expect 3
