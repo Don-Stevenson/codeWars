a30b6d1f3491a4a284637d89139deddf4f11ba59
Don-Stevenson <dsteven1@gmail.com>
2021-03-10 11:00:03 -0500

    add function to try console assert

--- consoleAssert.js
+++ consoleAssert.js
@@ -3,8 +3,18 @@
 
 // only asserts when evaluates a thing to be false
 
-console.assert("", "its empty")
-// expect "its empty" message
+// console.assert("", "its empty")
+// expect Assertion failed: its empty
 
 console.assert("1", "its empty")
 // expect no message
+
+const arr = [1,2,3,4,5,6,7,"8",""]
+
+const doesArrContainTwo =  (arr) => {
+    arr.map((element) => {
+        console.assert(element !== 2, "found the number 2")
+    })
+}
+doesArrContainTwo(arr)
+// expect Assertion failed: found the number 2
