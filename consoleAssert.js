60b8fb69c05d3d5cae07276cf3195dc1186083f0
Don-Stevenson <dsteven1@gmail.com>
2021-03-10 10:44:53 -0500

    basic setup of the file

---
+++ consoleAssert.js
@@ -0,0 +1,10 @@
+// a place to try out console assert
+// *********************************
+
+// only asserts when evaluates a thing to be false
+
+console.assert("", "its empty")
+// expect "its empty" message
+
+console.assert("1", "its empty")
+// expect no message
