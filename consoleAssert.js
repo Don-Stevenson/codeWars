018b2c4571440c9cc45de6f6d7dabe98dfca3966
Don-Stevenson <dsteven1@gmail.com>
2021-03-11 20:30:57 -0500

    add counter to see when it finds 2

--- consoleAssert.js
+++ consoleAssert.js
@@ -12,10 +12,23 @@ console.assert("1", "its empty")
 const arr = [1,2,3,4,5,6,7,"8",""]
 
 const doesArrContainTwo =  (arr) => {
+    let count = 1
     arr.map((element) => {
+        console.log({count})
         console.assert(element !== 2, "found the number 2")
+        count+=1
     })
 }
 
 doesArrContainTwo(arr)
-// expect Assertion failed: found the number 2
+
+// { count: 1 }
+// { count: 2 }
+// Assertion failed: found the number 2
+// { count: 3 }
+// { count: 4 }
+// { count: 5 }
+// { count: 6 }
+// { count: 7 }
+// { count: 8 }
+// { count: 9 }
