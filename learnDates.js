310f1d4ff01d3e9c7335532105658989103dcbcb
Don-Stevenson <dsteven1@gmail.com>
2021-03-13 11:29:35 -0500

    add expected results

--- learnDates.js
+++ learnDates.js
@@ -7,3 +7,6 @@ const [monthNum, caldendarDateNum, year] = new Date()
   .toLocaleDateString("en-US")
   .split("/");
 console.log({ monthNum }, { caldendarDateNum }, { year });
+
+// 2021-03-13T16:28:46.930Z
+// { monthNum: '3' } { caldendarDateNum: '13' } { year: '2021' }
