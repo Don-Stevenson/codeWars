e65590a33abfdafe1610d5455115da44b4d39fb5
Don-Stevenson <dsteven1@gmail.com>
2021-03-21 11:00:55 -0400

    add valueAbove function, call on array using every

--- learnEvery.js
+++ learnEvery.js
@@ -1,6 +1,11 @@
 // a place to try out the every
-//
+// ****************************
+// looks at every value in an array 
+// 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-arr.every((value) => console.log(value));
-// expect 1
+
+const isValueAbove = (value) => value > 0 ? true : false
+
+console.log(arr.every(isValueAbove));
+// expect true
