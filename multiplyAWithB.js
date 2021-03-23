37e8c3a46f7cb98abbaf19de4f28ba252853e1c5
Don-Stevenson <dsteven1@gmail.com>
2021-03-22 23:20:36 -0400

    reformat code

--- multiplyAWithB.js
+++ multiplyAWithB.js
@@ -1,14 +1,14 @@
 // Multiply with Args
 // ******************
 
-// function that multiply with two numbers from the process argv's array. 
+// function that multiply with two numbers from the process argv's array.
 // *********************************************************************
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
 const multiplyTwoNumbers = () => {
-  return parseInt(args[0]) * parseInt(args[1])
+  return parseInt(args[0]) * parseInt(args[1]);
 };
 
-console.log(multiplyTwoNumbers(args)) /* 3 4 */
+console.log(multiplyTwoNumbers(args)); /* 3 4 */
 // expect 12
