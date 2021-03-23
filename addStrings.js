ea661103ef290eae91939700dbbc9bbf4dbe4ba1
Don-Stevenson <dsteven1@gmail.com>
2021-03-23 17:32:02 -0400

    break the instructions into two lines

--- addStrings.js
+++ addStrings.js
@@ -1,7 +1,7 @@
 // add strings
 // ***********
 
-// Given the string representations of two integers, return the 
+// Given the string representations of two integers, return the
 // string representation of the sum of those integers.
 
 // For example:
@@ -9,7 +9,6 @@
 // sumStrings('1','2') // => '3'
 // A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
 
-const sumStrings = (a, b) =>  (BigInt(a) + BigInt(b)).toString()
-
+const sumStrings = (a, b) => (BigInt(a) + BigInt(b)).toString();
 
 console.log(sumStrings("1", "100000000000000000000000000"));
