73bd80e0780e3b07f3eebf34e04d52e090941072
Don-Stevenson <dsteven1@gmail.com>
2021-03-29 21:55:10 -0400

    remove commented in code, change names to reflect how reverse string isoperating

--- march25.js
+++ march25.js
@@ -2,26 +2,26 @@
 // with split, reverse and join
 // ***************************
 
-// const reverseString = str => {
-//   let reverseStr = str
-//     .split("")
-//     .reverse()
-//     .join("");
-//   return reverseStr;
-// };
+const reverseString = str => {
+  let reverseStr = str
+    .split("")
+    .reverse()
+    .join("");
+  return reverseStr;
+};
 
 // with a c style loop
 
-// const reverseString = str => {
-//   if (str === "") return "";
-//   else {
-//     let newStr = "";
-//     for (let i = str.length - 1; i >= 0; i--) {
-//       newStr += str[i];
-//     }
-//     return newStr;
-//   }
-// };
+const reverseString = str => {
+  if (str === "") return "";
+  else {
+    let newStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      newStr += str[i];
+    }
+    return newStr;
+  }
+};
 
 // with recursion
 // **************
