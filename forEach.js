047eb257b26da7f5067da651d9176dcd1458f009
Don-Stevenson <dsteven1@gmail.com>
2021-04-01 15:47:11 -0400

    printing numbers from an array with 3 methods

---
+++ forEach.js
@@ -0,0 +1,34 @@
+const arr = [1, 2, 3, 4, 5, 6];
+
+
+
+// with c style loop
+
+const cStyleLoop = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+cStyleLoop(arr);
+
+
+
+// with for of loop
+
+const forOfLoop = function (arr) {
+  for (const number of arr) {
+    console.log(number);
+  }
+};
+forOfLoop(arr);
+
+
+
+// with forEach
+
+const tryForEach = function (arr) {
+  arr.forEach((number) => console.log(number));
+};
+
+tryForEach(arr);
