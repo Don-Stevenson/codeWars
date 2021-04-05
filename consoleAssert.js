1138405057c6fa9d600fca20087219fd8ccc49d1
Don-Stevenson <dsteven1@gmail.com>
2021-04-05 13:34:58 -0400

    add expected results

--- consoleAssert.js
+++ consoleAssert.js
@@ -6,24 +6,24 @@
 // console.assert("", "its empty")
 // expect Assertion failed: its empty
 
-console.assert("1", "its empty")
+console.assert("1", "its empty");
 // expect no message
 
-const arr = [1,2,3,4,5,6,7,"8",""]
+const arr = [1, 2, 3, 4, 5, 6, 7, "8", ""];
 
-const doesArrContainTwo =  (arr) => {
-    let count = 1
-    console.table(arr)
-    arr.map((element) => {
-        console.log({count})
-        console.assert(element !== 2, "found the number 2")
-        count+=1
-    
-    })
-}
+const doesArrContainTwo = (arr) => {
+  let count = 1;
+  console.table(arr);
+  arr.map((element) => {
+    console.log({ count });
+    console.assert(element !== 2, "found the number 2");
+    count += 1;
+  });
+};
 
-doesArrContainTwo(arr)
+doesArrContainTwo(arr);
 
+// expect :
 // { count: 1 }
 // { count: 2 }
 // Assertion failed: found the number 2
