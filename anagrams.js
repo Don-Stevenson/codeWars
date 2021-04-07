2f3f200195e1abf35083082b334c8b6616f20439
Don-Stevenson <dsteven1@gmail.com>
2021-04-07 18:31:37 -0400

    now with map

--- anagrams.js
+++ anagrams.js
@@ -1,18 +1,30 @@
 // function that checks a word against an array of submitted words
-// It returns an array of anagrams of the submitted word 
+// It returns an array of anagrams of the submitted word
 // that match the array of submitted potenial anagagram words
 // face + [acef, feca, jerry, fac ] =>  [acef, feca]
 // *******************************************************
 
+// const anagrams = (word, inputWords) => {
+//   const wordSorted = word.split("").sort().join("");
+//   let anagramArr = [];
+//   for (const word of inputWords) {
+//     const wordsSorted = word.split("").sort().join("");
+//     if (wordSorted === wordsSorted) {
+//       anagramArr.push(word);
+//     }
+//   }
+//   return anagramArr;
+// };
+
+// with map
 const anagrams = (word, inputWords) => {
-  const wordSorted = word.split("").sort().join("");
+  const inputWordSorted = word.split("").sort().join("");
   let anagramArr = [];
-  for (const word of inputWords) {
-    const wordsSorted = word.split("").sort().join("");
-    if (wordSorted === wordsSorted) {
-      anagramArr.push(word);
+  inputWords.map((element) => {
+    if (inputWordSorted === element.split("").sort().join("")) {
+      anagramArr.push(element);
     }
-  }
+  });
   return anagramArr;
 };
 
