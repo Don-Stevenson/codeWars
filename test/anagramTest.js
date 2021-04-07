dc7dc7d90fca98c01ed92472aa8c3d63bafc52d0
Don-Stevenson <dsteven1@gmail.com>
2021-04-07 18:40:52 -0400

    add new test for when no anagrams present

--- test/anagramTest.js
+++ test/anagramTest.js
@@ -19,6 +19,12 @@ describe("Array", () => {
         ["hello", "ehllo", "olelh"]
       );
     });
+    it("should return an empty array when there are no anagrams of the original word", () => {
+      assert.deepEqual(
+        anagrams("hello", ["hgllo", "hi", "ihllo", "face", "plelh", "ho"]),
+        []
+      );
+    });
   });
 });
 
