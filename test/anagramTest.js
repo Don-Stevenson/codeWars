208406ed2675ef658e0afc93018f5797b1b7f195
Don-Stevenson <dsteven1@gmail.com>
2021-04-23 10:15:12 -0400

    update to test deepStrictEqual

--- test/anagramTest.js
+++ test/anagramTest.js
@@ -14,13 +14,13 @@ const anagrams = (word, inputWords) => {
 describe("Array", () => {
   describe("#anagrams()", () => {
     it("should return an new array that contains only anagrams of the original word", () => {
-      assert.deepEqual(
+      assert.deepStrictEqual(
         anagrams("hello", ["hello", "hi", "ehllo", "face", "olelh", "ho"]),
         ["hello", "ehllo", "olelh"]
       );
     });
     it("should return an empty array when there are no anagrams of the original word", () => {
-      assert.deepEqual(
+      assert.deepStrictEqual(
         anagrams("hello", ["hgllo", "hi", "ihllo", "face", "plelh", "ho"]),
         []
       );
