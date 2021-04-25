824e3e57c01b4940b5aeff2b7dcdfae40d13ed2a
Don-Stevenson <dsteven1@gmail.com>
2021-04-25 14:22:17 -0400

    add tests for fib and get positive fib

--- test/fibToNTest.js
+++ test/fibToNTest.js
@@ -1,2 +1,18 @@
 const { fib, getPostiveFib } = require("../fibToN");
 
+const assert = require("assert");
+
+describe("fib", () => {
+  it("12 should return 144", function () {
+    assert.deepStrictEqual(fib(12), 144);
+  });
+  it("-12 should return -144", function () {
+    assert.deepStrictEqual(fib(-12), -144);
+  });
+});
+
+describe("Positive fib", () => {
+  it("15 should return 610", function () {
+    assert.deepStrictEqual(getPostiveFib(15), 610);
+  });
+});
