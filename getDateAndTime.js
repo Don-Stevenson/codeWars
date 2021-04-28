d99d20a49d03470310bdeebdd62017a85ceeb3db
Don-Stevenson <dsteven1@gmail.com>
2021-04-27 22:36:57 -0400

    add expected results

--- getDateAndTime.js
+++ getDateAndTime.js
@@ -1,7 +1,9 @@
-const date = "12/08/2020"
+const date = "12/08/2020";
 
-const day = parseInt(date.slice(0,2))
-const month = parseInt(date.slice(3,5))
-const year = parseInt(date.slice(6,8))
-console.log({day})
-console.log({month}, {year})
+const day = parseInt(date.slice(0, 2));
+const month = parseInt(date.slice(3, 5));
+const year = parseInt(date.slice(6, 8));
+console.log({ day });
+console.log({ month }, { year });
+// { day: 12 }
+// { month: 8 } { year: 20 }
