34f5c19556c91acef8cfaa293da5812dfb43c66f
Don-Stevenson <dsteven1@gmail.com>
2021-05-20 14:11:29 -0400

    add comments and negative value array

--- learnEvery.js
+++ learnEvery.js
@@ -1,11 +1,13 @@
 // a place to try out the every
 // ****************************
-// looks at every value in an array 
-// 
+// looks at every value in an array
+//
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+const negArr = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
 
-const isValueAbove = (value) => value > 0 ? true : false
+// function that checks if a value is above 0 and returns true or false
+const isValueAbove = (value) => (value > 0 ? true : false);
 
 console.log(arr.every(isValueAbove));
 // expect true
