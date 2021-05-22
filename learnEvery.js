75758abf0a58caf061c5b6c9e8d858f0c8d35bba
Don-Stevenson <dsteven1@gmail.com>
2021-05-22 14:49:05 -0400

    comment out test console log

--- learnEvery.js
+++ learnEvery.js
@@ -9,7 +9,7 @@ const negArr = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
 // function that checks if a value is above 0 and returns true or false
 const isValueAbove = (value) => (value > 0 ? true : false);
 
-console.log(arr.every(isValueAbove));
+// console.log(arr.every(isValueAbove));
 // expect true
 
 console.log(negArr.every(isValueAbove));
