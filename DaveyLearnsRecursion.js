13bc559fbe9f4322dcdf7e8a8d1dcbb89c11c02a
Don-Stevenson <dsteven1@gmail.com>
2021-05-27 09:50:19 -0400

    initial setup

---
+++ DaveyLearnsRecursion.js
@@ -0,0 +1,12 @@
+// place for Davey to learn Recursion
+// **********************************
+
+// make a function that finds a in a deeply nested array
+
+const nestedArray = [[[[[[[[[[[[[[[[["a"]]]]]]]]]]]]]]]]];
+
+const findsA = (arr) => {
+};
+
+
+findsA()
