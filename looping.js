eea5f99e2bc50046ee92dcbc2b8544e2c7442bcf
Don-Stevenson <dsteven1@gmail.com>
2021-06-04 10:51:16 -0400

    add loop that adds two values of the array together

--- looping.js
+++ looping.js
@@ -1,6 +1,9 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
+// for (const item of array) {
+//   console.log("item is:", item);
+// }
 
-for (const item of array) {
-  console.log("item is:", item);
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i] + array[i + 2]);
 }
