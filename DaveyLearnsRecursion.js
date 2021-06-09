9242a1f48787e3161005ca16677d6fb3941a95f9
Don-Stevenson <dsteven1@gmail.com>
2021-06-09 14:27:21 -0400

    remove brackets on args

--- DaveyLearnsRecursion.js
+++ DaveyLearnsRecursion.js
@@ -13,7 +13,7 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, "a", 5];
 
 // function that finds the string "a" in a nested array
 
-const findsA = (arr) => {
+const findsA = arr => {
   for (const item of arr) {
     console.log({ item });
     if (Array.isArray(item)) {
