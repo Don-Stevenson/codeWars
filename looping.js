844170adb45b0d2d0fb231f5fb12e53205255127
Don-Stevenson <dsteven1@gmail.com>
2021-06-15 10:29:00 -0400

    put loop in a function, add array +2 check

--- looping.js
+++ looping.js
@@ -1,9 +1,22 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
+// a function adding two elements together
+
+const numArray = [1, 2, 3, 4, 5, 6, 7];
 
 // for (const item of array) {
 //   console.log("item is:", item);
 // }
+const addElements = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    // check to see if array element + 2 exists before adding the two values 
+    if (array[i + 2]) console.log(array[i] + array[i + 2]);
+  }
+};
 
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i] + array[i + 2]);
-}
+addElements(numArray);
+// expect
+// 4
+// 6
+// 8
+// 10
+// 12
+
