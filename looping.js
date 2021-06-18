59fd856d0c1b4de85eb95add349ed2a02789cdd6
Don-Stevenson <dsteven1@gmail.com>
2021-06-18 18:50:27 -0400

    with map

--- looping.js
+++ looping.js
@@ -5,12 +5,22 @@ const numArray = [1, 2, 3, 4, 5, 6, 7];
 // for (const item of array) {
 //   console.log("item is:", item);
 // }
+
+// with c style loop
+// const addElements = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     // check to see if array element + 2 exists before adding the two values
+//     if (array[i + 2]) console.log(array[i] + array[i + 2]);
+//     if (!array[i + 2]) console.log("this element does not exist");
+//   }
+// };
+
+// with map
+
 const addElements = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    // check to see if array element + 2 exists before adding the two values
-    if (array[i + 2]) console.log(array[i] + array[i + 2]);
-    if (!array[i + 2]) console.log("this element does not exist");
-  }
+  array.map(element, (index) => {
+    console.log({ element }, { index });
+  });
 };
 
 addElements(numArray);
