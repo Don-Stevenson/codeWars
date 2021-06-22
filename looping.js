aa8a8bc2e77bca55eaa1de86c7f80070c2057176
Don-Stevenson <dsteven1@gmail.com>
2021-06-22 10:18:28 -0400

    solve with reducer

--- looping.js
+++ looping.js
@@ -25,25 +25,22 @@ const numArray = [1, 2, 3, 4, 5, 6, 7];
 // 6 5
 // 7 6
 
-
 // with map -
-// functions like reduce. Haven't figured out how to call an index ahead 
+// functions like reduce. Haven't figured out how to call an index ahead
 // of the current value efficiently with map
 // ********
 
-const addElements = (array) => {
-  let total = 0;
-  array.map((element, index) => {
-    console.log({ element }, { index });
-    total += element;
-    console.log({ total });
-  });
-  return total
- };
+// const addElements = (array) => {
+//   let total = 0;
+//   array.map((element, index) => {
+//     console.log({ element }, { index });
+//     total += element;
+//     console.log({ total });
+//   });
+//   return total
+//  };
 
- console.log(addElements(numArray));
-
-// expect 
+// expect
 // *******
 // { element: 1 } { index: 0 }
 // { total: 1 }
@@ -59,4 +56,48 @@ const addElements = (array) => {
 // { total: 21 }
 // { element: 7 } { index: 6 }
 // { total: 28 }
+// 28
+
+// with reduce
+// ***********
+
+//  (accumalator, currentVal, currentIndex, array) 
+// 
+
+
+const addElements = (array) => {
+  return array.reduce((accum, cur, index, arr) => {
+    console.log({ accum }, { cur }, {index}, { arr });
+    return accum += cur;
+   });
+};
+
+console.log(addElements(numArray));
+
+// expect 
+// 
+// { accum: 1 } { cur: 2 } { index: 1 } { arr: [
+//   1, 2, 3, 4,
+//   5, 6, 7
+// ] }
+// { accum: 3 } { cur: 3 } { index: 2 } { arr: [
+//   1, 2, 3, 4,
+//   5, 6, 7
+// ] }
+// { accum: 6 } { cur: 4 } { index: 3 } { arr: [
+//   1, 2, 3, 4,
+//   5, 6, 7
+// ] }
+// { accum: 10 } { cur: 5 } { index: 4 } { arr: [
+//   1, 2, 3, 4,
+//   5, 6, 7
+// ] }
+// { accum: 15 } { cur: 6 } { index: 5 } { arr: [
+//   1, 2, 3, 4,
+//   5, 6, 7
+// ] }
+// { accum: 21 } { cur: 7 } { index: 6 } { arr: [
+//   1, 2, 3, 4,
+//   5, 6, 7
+// ] }
 // 28
