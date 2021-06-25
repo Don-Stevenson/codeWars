afc0f34169c3fcdbf6f12f4815f95e41ef99aaf4
Don-Stevenson <dsteven1@gmail.com>
2021-06-25 10:00:26 -0400

    update format

--- march24.js
+++ march24.js
@@ -1,11 +1,10 @@
 // basic function that adds one
 // ****************************
 
-const addOne = num => num++;
+const addOne = (num) => num++;
 // console.log(addOne(8));
-// expect 
+// expect
 // 9
-
 
 // basic function that adds two numbers
 // ************************************
@@ -15,14 +14,11 @@ const addTwoNums = (A, B) => A + B;
 // console.log(addTwoNums(11, 33))
 // expect 44
 
-
-
 // basic function that subtracts two numbers
 // ******************************************
 
-const substractTwoNums = (A, B) => A - B ;
+const substractTwoNums = (A, B) => A - B;
 // ***************************************
-
 
 // console.log(substractTwoNums(11, 33))
 // expect
@@ -31,42 +27,33 @@ const substractTwoNums = (A, B) => A - B ;
 // basic function that divides two numbers
 // ***************************************
 
-const divideTwoNums = (A, B) => A / B ;
-
+const divideTwoNums = (A, B) => A / B;
 
 // console.log(divideTwoNums(25,2))
 // expect
 // 12.5
 
-
-
 // basic function that multiplys two numbers
 // *****************************************
 
-const mulitplyTwoNums = (A, B) => A  *B ;
+const mulitplyTwoNums = (A, B) => A * B;
 
 // console.log(multiplyTwoNums(25,2))
 // expect
 // 50
 
-
-
-
 // basic function that sqaures a number
 // ************************************
 
-const squareOneNum = A => A * A ;
+const squareOneNum = (A) => A * A;
 
 // console.log(squareOneNum(9))
 // expect 81
 
-
-
-
 // basic function that finds the square root of a number
 // *****************************************************
 
-const sqrRootNum = A => Math.sqrt(A) ;
+const sqrRootNum = (A) => Math.sqrt(A);
 
 // console.log(sqrRootNum(81))
 // expect 9
