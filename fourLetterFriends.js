429982f6af2e46ea2f3e62b7af1c6ad671ca5ec0
Don-Stevenson <dsteven1@gmail.com>
2021-06-26 12:51:21 -0400

    update comments

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -1,5 +1,5 @@
-// function that takes in an array and 
-// returns an array with elements that's length is equal to 4 
+// function that takes in an array and
+// returns an array with elements that's length is equal to 4
 // **********************************************************
 
 const friendsArr = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
