3903c3cf726cf6108bc4a255dacb3e1d2e51537e
Don-Stevenson <dsteven1@gmail.com>
2021-06-29 09:55:20 -0400

    remove extraneous variable

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -5,7 +5,7 @@
 const friendsArr = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
 
 const findFriend = array => {
-  return (fourLetterFriends = array.filter((element) => element.length === 4));
+  return array.filter((element) => element.length === 4);
 };
 
 console.log(findFriend(friendsArr));
