209506629a79457be16d3b4f58143b92ae4b1a28
Don-Stevenson <dsteven1@gmail.com>
2021-07-02 10:05:50 -0400

    refactor with map

--- alphabetChars.js
+++ alphabetChars.js
@@ -3,20 +3,34 @@
 
 // a function that checks if a string contains all the letters from the alphabet
 
+// const containsAllLetters = str => {
+//   const alpha = new Set("abcdefghijklmnopqrstuvwxyz");
+//   for (const c of str.toLowerCase()) {
+//     alpha.delete(c);
+//     if (alpha.size === 0) return true;
+//   }
+//   return false;
+// };
 
-const containsAllLetters = str => {
+// with map
+
+const containsAllLetters = (str) => {
   const alpha = new Set("abcdefghijklmnopqrstuvwxyz");
-  for (const c of str.toLowerCase()) {
-    alpha.delete(c);
-    if (alpha.size === 0) return true;
-  }
-  return false;
+  str
+    .toLowerCase()
+    .split("")
+    .map((e) => {
+      alpha.delete(e);
+    });
+
+  if (alpha.size === 0) return true;
+  else return false;
 };
 
-console.log(containsAllLetters("the quick brown fox jumps over the lazy brown dog"));
+console.log(containsAllLetters("the quick brown fox jumps over the lazy dog"));
 // expect:
 // true
 
-console.log(containsAllLetters("the quic brown fox jumps over the lazy brown dog"));
+console.log(containsAllLetters("the quic brown fox jumps over the lazy dog"));
 // expect:
 // false
