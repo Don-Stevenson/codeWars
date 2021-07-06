ef16dddb0cfe069ad5c97aff576ffa322cef1d9c
Don-Stevenson <dsteven1@gmail.com>
2021-07-06 10:52:12 -0400

    upadte naming of function

--- reduceJune72021.js
+++ reduceJune72021.js
@@ -5,9 +5,9 @@
 const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // a custom reducing function that adds up the square of the numbers from the array
-const addingFunction = (accumulator, currentVal) =>
+const addingSquares = (accumulator, currentVal) =>
   accumulator + Math.pow(currentVal, 2);
 
 // callling the number array with Reduce and then passing in the adding the adding squares function
-console.log(numArray.reduce(addingFunction));
+console.log(numArray.reduce(addingSquares));
 // expect 285
