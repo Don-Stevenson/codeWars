2238059fec17005de5953ac0772f35b44cc00ef6
Don-Stevenson <dsteven1@gmail.com>
2021-07-08 09:42:03 -0400

    with for of loop

--- pushVsFilter.js
+++ pushVsFilter.js
@@ -1,6 +1,6 @@
 // trying out the difference between filitering to a new array vs pushing
 
-// even nums with push
+// even nums with map and push
 
 // const returnEvens = array => {
 //   let evenArray = [];
@@ -14,10 +14,24 @@
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// even nums with push and for loop
+
+const returnEvens = (array) => {
+  let evenArray = [];
+  for (const e of array) {
+    if (e % 2 === 0) {
+      evenArray.push(e);
+    }
+  }
+  return evenArray;
+};
+
 // console.log(returnEvens(arr));
 // expect
 // [ 2, 4, 6, 8, 10 ]
 
-const returnEvens = (array) => array.filter((e) => e % 2 === 0);
+// const returnEvens = (array) => array.filter((e) => e % 2 === 0);
 
 console.log(returnEvens(arr));
+// expect
+// [ 2, 4, 6, 8, 10 ]
