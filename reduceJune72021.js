67abfcdb542359ba19389abb15bea953e991e909
Don-Stevenson <dsteven1@gmail.com>
2021-07-12 18:36:47 -0400

    add difference of roots

--- reduceJune72021.js
+++ reduceJune72021.js
@@ -4,10 +4,13 @@
 // an array with numbers in it
 const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// a custom reducing function that adds up the square of the numbers from the array
+// a custom reducing function that adds up the square of the numbers from a given set
 const addingSquares = (accumulator, currentVal) =>
   accumulator + Math.pow(currentVal, 2);
 
 // callling the number array with Reduce and then passing in the adding the adding squares function
 console.log(numArray.reduce(addingSquares));
 // expect 285
+
+// function that subtracts the root of each number
+const subtractSquares = (accum, curVal) => accum - Math.sqrt(curVal);
