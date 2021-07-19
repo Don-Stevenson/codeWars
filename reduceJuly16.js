20c43cb382a5ba2c1525776424359326ebb7ecac
Don-Stevenson <dsteven1@gmail.com>
2021-07-18 23:51:10 -0400

    reduce with object calls

--- reduceJuly16.js
+++ reduceJuly16.js
@@ -4,12 +4,21 @@
 const arrOfObjs = [
   { a: 1, b: 2 },
   { a: 3, b: 4 },
+  { a: 3, b: 4 },
+  { a: 19, b: 4 },
 ];
 
 // reducer function
-const reducer = (accum, currentVal) => accum.a + currentVal.a;
-
+const reducer = (accum, currentVal) => {
+  accum.a += currentVal.a
+//  console.log(typeof(accum)) 
+ 
+ 
+ return accum
+}
 // expect
 // 4
 
 console.log(arrOfObjs.reduce(reducer));
+// expect 
+// { a: 26, b: 2 }
