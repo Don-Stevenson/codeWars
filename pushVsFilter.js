5af67157381e80a9ccaf86fd7516427b0735c249
Don-Stevenson <dsteven1@gmail.com>
2021-08-09 12:24:13 -0400

    refactor with ternary statment & console log for odd nums

--- pushVsFilter.js
+++ pushVsFilter.js
@@ -2,29 +2,25 @@
 
 // even nums with map and push
 
-// const returnEvens = array => {
-//   let evenArray = [];
-//   array.map((e) => {
-//     if (e % 2 === 0) {
-//       evenArray.push(e);
-//     }
-//   });
-//   return evenArray;
-// };
+const returnEvens = array => {
+  let evenArray = [];
+  array.map((e) => e % 2 === 0 ? evenArray.push(e) : console.log(`${e} is not even`) );
+  return evenArray;
+};
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // even nums with push and for loop
 
-const returnEvens = (array) => {
-  let evenArray = [];
-  for (const e of array) {
-    if (e % 2 === 0) {
-      evenArray.push(e);
-    }
-  }
-  return evenArray;
-};
+// const returnEvens = (array) => {
+//   let evenArray = [];
+//   for (const e of array) {
+//     if (e % 2 === 0) {
+//       evenArray.push(e);
+//     }
+//   }
+//   return evenArray;
+// };
 
 // console.log(returnEvens(arr));
 // expect
