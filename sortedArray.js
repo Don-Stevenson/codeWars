74831d540e07a6a8f58da3ef2a593177a3337ecc
Don-Stevenson <dsteven1@gmail.com>
2021-08-16 12:38:42 -0400

    update format

--- sortedArray.js
+++ sortedArray.js
@@ -3,7 +3,7 @@ const array = [1, 4, 5, 6, 3];
 // sorting an array be lowest odd then lowest even,
 // then next lowest odd and next lowest even, etc
 
-const sortArray = array => {
+const sortArray = (array) => {
   const odds = array.filter((x) => x % 2).sort((a, b) => a - b);
   return array.map((element) => (element % 2 ? odds.shift() : element));
 };
