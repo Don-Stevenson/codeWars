9db71f7ade7c536ebbfabb9d7bf0775b30bcce19
Don-Stevenson <dsteven1@gmail.com>
2021-08-18 00:25:11 -0400

    refactor and change loop stop statement

--- 3xplus1.js
+++ 3xplus1.js
@@ -5,12 +5,9 @@
 const calculateNumsTo4 = (num) => {
   console.log("num is ", num);
   if (num === 4 || num === 2 || num === 1)
-    return console.log("in the 4 2 1 loop");
-  if (num % 2 === 0) {
-    return calculateNumsTo4(num / 2);
-  } else if (num % 2 !== 0) {
-    return calculateNumsTo4(num * 3 + 1);
-  }
+    return console.log("now in the 4 2 1 loop");
+  if (num % 2 === 0) return calculateNumsTo4(num / 2);
+  else if (num % 2 !== 0) return calculateNumsTo4(num * 3 + 1);
 };
 
 calculateNumsTo4(3);
