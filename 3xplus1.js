11212967df406f00a299667e9307ad27c93f3b2a
Don-Stevenson <dsteven1@gmail.com>
2021-08-18 00:51:29 -0400

    add count

--- 3xplus1.js
+++ 3xplus1.js
@@ -1,14 +1,23 @@
 // if odd 3x +1
 // if even divide by 2
 // 4 2 1
+let count = 1;
 
-const calculateNumsTo4 = num => {
-  console.log("num is ", num);
+const calculateNumsTo4 = (num) => {
+  console.log(`num is ${num}
+count is ${count}
+`);
   if (num === 4 || num === 2 || num === 1)
     return console.log("now in the 4 2 1 loop");
-  if (num % 2 === 0) return calculateNumsTo4(num / 2);
-  else if (num % 2 !== 0) return calculateNumsTo4(num * 3 + 1);
+  if (num % 2 === 0) {
+    count++;
+    return calculateNumsTo4(num / 2);
+  } else if (num % 2 !== 0) {
+    count++;
+    return calculateNumsTo4(num * 3 + 1);
+  }
 };
 
-calculateNumsTo4(3);
-calculateNumsTo4(12);
+// calculateNumsTo4(3);
+// calculateNumsTo4(12);
+calculateNumsTo4(27);
