307cdfe2aa9eac3fca30c8546bd4d923dbe3fe58
Don-Stevenson <dsteven1@gmail.com>
2021-08-19 16:19:28 -0400

    update formating

--- 3xplus1.js
+++ 3xplus1.js
@@ -9,22 +9,22 @@ let count = 1;
 const randomNum = Math.round(Math.random() * 100);
 
 const calculateNumsTo4 = (num) => {
-  console.log(`num is ${num}
-count is ${count}
-`);
-  if (num === 4 || num === 2 || num === 1) {
-    console.log("now in the 4 2 1 loop");
-    count += 3;
-    return console.log(`final count is ${count}
+      console.log(`num is ${num}
+    count is ${count}
     `);
-  }
-  if (num % 2 === 0) {
-    count++;
-    return calculateNumsTo4(num / 2);
-  } else if (num % 2 !== 0) {
-    count++;
-    return calculateNumsTo4(num * 3 + 1);
-  }
+    if (num === 4 || num === 2 || num === 1) {
+      console.log("now in the 4 2 1 loop");
+      count += 3;
+      return console.log(`final count is ${count}
+      `);
+    }
+    if (num % 2 === 0) {
+      count++;
+      return calculateNumsTo4(num / 2);
+    } else if (num % 2 !== 0) {
+      count++;
+      return calculateNumsTo4(num * 3 + 1);
+    }
 };
 
 // calculateNumsTo4(3);
