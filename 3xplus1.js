81080d2203a275cefbdaeee9e317afcc5f13ad8c
Don-Stevenson <dsteven1@gmail.com>
2021-08-22 12:25:56 -0400

    remove last if condition that is uneccessary. Add expected results

--- 3xplus1.js
+++ 3xplus1.js
@@ -27,13 +27,29 @@ const calculateNumsTo4 = (num) => {
   if (num % 2 === 0) {
     count++;
     return calculateNumsTo4(num / 2);
-  } else if (num % 2 !== 0) {
-    count++;
-    return calculateNumsTo4(num * 3 + 1);
-  }
+  } else count++;
+  return calculateNumsTo4(num * 3 + 1);
 };
 
-// calculateNumsTo4(3);
+calculateNumsTo4(3); // expect num is 3
+// count is 1
+
+// num is 10
+// count is 2
+
+// num is 5
+// count is 3
+
+// num is 16
+// count is 4
+
+// num is 8
+// count is 5
+
+// num is 4. 
+// Now in the 4 2 1 loop, so +3 to count has been added.
+// The final count is 9
+//
 // calculateNumsTo4(12);
 // calculateNumsTo4(27);
-calculateNumsTo4(randomNum);
+// calculateNumsTo4(randomNum);
