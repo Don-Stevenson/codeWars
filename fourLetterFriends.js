b8c88cc409130af639fd56b7c1694484a61cebc6
Don-Stevenson <dsteven1@gmail.com>
2021-08-25 06:19:53 -0400

    to single line implicit returns

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -4,9 +4,7 @@
 
 const friendsArr = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
 
-const findFriend = array => {
-  return array.filter((element) => element.length === 4);
-};
+const findFriend = array => array.filter((element) => element.length === 4);
 
 console.log(findFriend(friendsArr));
 // expect ['Ryan']
