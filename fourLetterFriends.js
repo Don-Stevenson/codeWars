075d2d498d8c4d41167eb012c3a1bab5d31f4914
Don-Stevenson <dsteven1@gmail.com>
2021-08-25 06:20:23 -0400

    remove unecessary brackets

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -4,7 +4,7 @@
 
 const friendsArr = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
 
-const findFriend = array => array.filter((element) => element.length === 4);
+const findFriend = array => array.filter(element => element.length === 4);
 
 console.log(findFriend(friendsArr));
 // expect ['Ryan']
