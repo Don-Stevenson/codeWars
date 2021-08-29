9b29bc26eb882b9979b06aeaa13f77a6e037723a
Don-Stevenson <dsteven1@gmail.com>
2021-08-28 22:52:30 -0400

    add log of object and key

--- letterCountInObject.js
+++ letterCountInObject.js
@@ -42,12 +42,13 @@
 // with map
 // ***********
 
-const letterCounter = word => {
+const letterCounter = (word) => {
   // set an empty object
   let obj = {};
 
   // split word into an array of letters using map
-  word.split("").map(key => {
+  word.split("").map((key) => {
+    console.log({ obj }, { key });
     // if the object has the letter as a key already then update the object's value at that key +1
     if (obj[key]) {
       obj[key] = obj[key] + 1;
@@ -61,4 +62,4 @@ const letterCounter = word => {
 };
 
 console.log(letterCounter("hello")); // ==> { h: 1, e: 1, l: 2, o: 1 }
-console.log(letterCounter("stardust")) // ==> { s: 2, t: 2, a: 1, r: 1, d: 1, u: 1 }
+console.log(letterCounter("stardust")); // ==> { s: 2, t: 2, a: 1, r: 1, d: 1, u: 1 }
