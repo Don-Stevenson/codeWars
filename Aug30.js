ac8649168ee9095be2014206b86a349624d4f030
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-04 12:39:30 -0400

    add function call on array. add expected results

--- Aug30.js
+++ Aug30.js
@@ -1,6 +1,10 @@
 // filtering an array and return numbers that are even
 //
 
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 const evenFilter = (array) => {
   return array.filter((e) => e % 2 === 0);
 };
+
+console.log(evenFilter(numberArray)); // => [ 2, 4, 6, 8, 10 ]
