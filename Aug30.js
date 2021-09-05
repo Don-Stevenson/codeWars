f09312588d5ac48457ba17b31e30a56d8d743cb7
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-05 13:28:34 -0400

    with prettier

--- Aug30.js
+++ Aug30.js
@@ -3,6 +3,6 @@
 
 const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const evenFilter = array => array.filter(e => e % 2 === 0)
+const evenFilter = (array) => array.filter((e) => e % 2 === 0);
 
 console.log(evenFilter(numberArray)); // => [ 2, 4, 6, 8, 10 ]
