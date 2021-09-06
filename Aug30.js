51aa6a0d2ab7c76aa7afc915c44f5afd06eb26de
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-06 15:54:05 -0400

    update formatting

--- Aug30.js
+++ Aug30.js
@@ -1,8 +1,8 @@
 // filtering an array and return numbers that are even
 //
 
-const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const evenFilter = array => array.filter(e => e % 2 === 0);
+const evenFilter = array => array.filter(e => e % 2 === 0)
 
-console.log(evenFilter(numberArray)); // => [ 2, 4, 6, 8, 10 ]
+console.log(evenFilter(numberArray)) // => [ 2, 4, 6, 8, 10 ]
