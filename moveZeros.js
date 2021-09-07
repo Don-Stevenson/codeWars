70b05102f765795ad3a86966c7589e9bfb913b36
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-07 09:00:03 -0400

    remove parameter brackets and semi colons

--- moveZeros.js
+++ moveZeros.js
@@ -2,14 +2,14 @@
 // ************************************************
 
 const moveZeros = arr => {
-  let arrOfZeros = [];
-  let arrWithoutZeros = [];
+  let arrOfZeros = []
+  let arrWithoutZeros = []
 
-  arr.map((e) => {
-    e === 0 ? arrOfZeros.push(e) : arrWithoutZeros.push(e);
-  });
-  return (finalArr = [...arrWithoutZeros, ...arrOfZeros]);
-};
+  arr.map(e => {
+    e === 0 ? arrOfZeros.push(e) : arrWithoutZeros.push(e)
+  })
+  return (finalArr = [...arrWithoutZeros, ...arrOfZeros])
+}
 
-console.log(moveZeros([0, 1, 4, 45, 0, 0, 7, 9]));
+console.log(moveZeros([0, 1, 4, 45, 0, 0, 7, 9]))
 // expected result [  1, 4, 45, 7,  9, 0,  0, 0 ]
