776c09011ff8c17e33cbbffb5e62e9107624a48a
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-07 09:00:36 -0400

    remove semi colons

--- multiplyAWithB.js
+++ multiplyAWithB.js
@@ -4,11 +4,11 @@
 // function that multiply with two numbers from the process argv's array.
 // *********************************************************************
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
 const multiplyTwoNumbers = () => {
-  return parseInt(args[0]) * parseInt(args[1]);
-};
+  return parseInt(args[0]) * parseInt(args[1])
+}
 
-console.log(multiplyTwoNumbers(args)); /* 3 4 */
+console.log(multiplyTwoNumbers(args)) /* 3 4 */
 // expect 12
