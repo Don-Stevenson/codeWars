36451ea18907dbb6c9aac7f27adb5b800f662bf3
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-07 08:58:22 -0400

    remove semi colons

--- stripComments.js
+++ stripComments.js
@@ -4,14 +4,14 @@
 
 const removeMarkers = (input, markers) => {
   // splits the input arr in to each letter as an item
-  const inputArr = input.split("");
+  const inputArr = input.split("")
   // designates the markers as what needs to be taken out
   // (Note: all elements in markers will be deleted)
-  const forDeletion = markers;
+  const forDeletion = markers
   // fiters out the input array, by only returning whats not for deleting,
   // and joins it back into a string
-  return inputArr.filter(item => !forDeletion.includes(item)).join("");
-};
+  return inputArr.filter(item => !forDeletion.includes(item)).join("")
+}
 
-console.log(removeMarkers("this is a show", "isahows"));
+console.log(removeMarkers("this is a show", "isahows"))
 // returns t
