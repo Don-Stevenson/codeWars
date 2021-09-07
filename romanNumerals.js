07eaad9e80c8ffee2ac20d33a944ddc6c528597e
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-07 08:58:54 -0400

    remove semi colons

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,7 +1,7 @@
 const RomanNumerals = {
   // function that creates a roman numeral based on an arabic number input
   //**********************************************************************/
-  toRoman: (num) => {
+  toRoman: num => {
     const romanKeyObj = {
       M: 1000,
       CM: 900,
@@ -16,19 +16,19 @@ const RomanNumerals = {
       V: 5,
       IV: 4,
       I: 1,
-    };
+    }
 
-    let romanNum = "";
+    let romanNum = ""
     for (let index in romanKeyObj) {
       while (num >= romanKeyObj[index]) {
-        romanNum += index;
-        num -= romanKeyObj[index];
+        romanNum += index
+        num -= romanKeyObj[index]
       }
     }
-    return romanNum;
+    return romanNum
   },
 
-  fromRoman: (romanNum) => {
+  fromRoman: romanNum => {
     // function that creates an arabic numeral based on an roman numeral input
     //***********************************************************************/
     const romanKeyObj = {
@@ -39,24 +39,24 @@ const RomanNumerals = {
       C: 100,
       D: 500,
       M: 1000,
-    };
-    let total = 0;
-    let input = romanNum.split("");
+    }
+    let total = 0
+    let input = romanNum.split("")
 
     for (let i = 0; i < input.length; i++) {
-      let currentLetter = romanKeyObj[input[i]];
-      let nextLetter = romanKeyObj[input[i + 1]];
-      if (currentLetter === undefined) return null;
+      let currentLetter = romanKeyObj[input[i]]
+      let nextLetter = romanKeyObj[input[i + 1]]
+      if (currentLetter === undefined) return null
       else {
         if (currentLetter < nextLetter) {
-          total += nextLetter - currentLetter;
-          i++;
-        } else total += currentLetter;
+          total += nextLetter - currentLetter
+          i++
+        } else total += currentLetter
       }
     }
-    return total;
+    return total
   },
-};
+}
 
-console.log("roman num to arabic nums ", RomanNumerals.fromRoman("XII"));
-console.log("arabic num to Roman nums", RomanNumerals.toRoman(201));
+console.log("roman num to arabic nums ", RomanNumerals.fromRoman("XII"))
+console.log("arabic num to Roman nums", RomanNumerals.toRoman(201))
