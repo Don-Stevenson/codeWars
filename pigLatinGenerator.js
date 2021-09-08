22271ef1e2c931cab2b73adf40e3754127fe7223
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-08 18:31:52 -0400

    remove semi colons and remove parameter brackets

--- pigLatinGenerator.js
+++ pigLatinGenerator.js
@@ -5,12 +5,13 @@
 // ************************
 
 const pigIt = str => {
-  const splitStr = str.split(" ");
-  return pigLatinStr = splitStr.map(element =>
-    /[a-zA-Z]+/.test(element)
-      ? element.slice(1) + element[0] + "ay"
-      : element).join(" ")
-};
+  const splitStr = str.split(" ")
+  return (pigLatinStr = splitStr
+    .map(element =>
+      /[a-zA-Z]+/.test(element) ? element.slice(1) + element[0] + "ay" : element
+    )
+    .join(" "))
+}
 
 // with replace
 // where \w is the first character and \w+ is the rest of the word
@@ -18,9 +19,7 @@ const pigIt = str => {
 // ************
 // const pigIt = (str) => {return str.replace(/(\w)(\w+)/g, '$2$1ay')};
 
-
-
-console.log(pigIt("hello Pig latin is cool"));  // => ellohay igPay atinlay siay oolcay
-console.log(pigIt("hello my name is Miss Piggy"));  // => ellohay igPay atinlay siay oolcay
-console.log(pigIt("farenheit 451"));  // => arenheitfay 451
-console.log(pigIt("No F!!&&in' way dude!"));  // => oNay !!&&in'Fay ayway ude!day
+console.log(pigIt("hello Pig latin is cool")) // => ellohay igPay atinlay siay oolcay
+console.log(pigIt("hello my name is Miss Piggy")) // => ellohay igPay atinlay siay oolcay
+console.log(pigIt("farenheit 451")) // => arenheitfay 451
+console.log(pigIt("No F!!&&in' way dude!")) // => oNay !!&&in'Fay ayway ude!day
