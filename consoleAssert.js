93be892b879f3c29b8aa0878330e078939ece682
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-08 18:38:06 -0400

    remove semi colons

--- consoleAssert.js
+++ consoleAssert.js
@@ -6,22 +6,22 @@
 // console.assert("", "its empty")
 // expect Assertion failed: its empty
 
-console.assert("1", "its empty");
+console.assert("1", "its empty")
 // expect no message
 
-const arr = [1, 2, 3, 4, 5, 6, 7, "8", ""];
+const arr = [1, 2, 3, 4, 5, 6, 7, "8", ""]
 
 const doesArrContainTwo = arr => {
-  let count = 1;
-  console.table(arr);
-  arr.map((element) => {
-    console.log({ count });
-    console.assert(element !== 2, "found the number 2");
-    count += 1;
-  });
-};
+  let count = 1
+  console.table(arr)
+  arr.map(element => {
+    console.log({ count })
+    console.assert(element !== 2, "found the number 2")
+    count += 1
+  })
+}
 
-doesArrContainTwo(arr);
+doesArrContainTwo(arr)
 
 // expect :
 // { count: 1 }
