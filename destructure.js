987d1ad9eff5a3ce77add1846ea23e260c8e5eaa
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-08 19:41:49 -0400

    remove semi colons

--- destructure.js
+++ destructure.js
@@ -5,12 +5,12 @@ const object = {
   a: 12,
   b: 34,
   c: () => console.log("hi there"),
-};
+}
 
-const { a, c } = object;
+const { a, c } = object
 
 // must destructure into variables that already exist in object like a, b, c
-const { foo, barr } = object;
+const { foo, barr } = object
 
 // console.log(a, c());
 // expect: hi there
@@ -20,14 +20,14 @@ const { foo, barr } = object;
 // expect: undefined undefined
 
 // destructuring an array
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // note that you can call the elements of an array any new variable you like
-const [p, q, five] = arr;
+const [p, q, five] = arr
 // console.log("p, q, five: ", p, q, five);
 // expect: p, q, five:  1 2 3
 
-const addTwoDigits = (a, b) => a + b;
+const addTwoDigits = (a, b) => a + b
 
-console.log(addTwoDigits(p, q));
+console.log(addTwoDigits(p, q))
 // expect 3
