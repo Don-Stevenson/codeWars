5a7706d7c503c8d233a20f7b04f59d7e16c96161
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-09 23:45:18 -0400

    remove semi colons

--- learnEvery.js
+++ learnEvery.js
@@ -2,15 +2,15 @@
 // ****************************
 // looks at every value in an array
 //
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const negArr = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
 
 // function that checks if a value is above 0 and returns true or false
-const isValueAbove = value => (value > 0 ? true : false);
+const isValueAbove = value => (value > 0 ? true : false)
 
 // console.log(arr.every(isValueAbove));
 // expect true
 
-console.log(negArr.every(isValueAbove));
+console.log(negArr.every(isValueAbove))
 // expect false
