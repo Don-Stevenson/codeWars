66adee7e3054adfd167a3d1c2ffaf63052f81fcc
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-09 23:44:05 -0400

    remove semi colons

--- fourLetterFriends.js
+++ fourLetterFriends.js
@@ -2,9 +2,9 @@
 // returns an array with elements that's length is equal to 4
 // **********************************************************
 
-const friendsArr = ["Ryan", "Jimmy", "123", "4", "Cool Man"];
+const friendsArr = ["Ryan", "Jimmy", "123", "4", "Cool Man"]
 
-const findFriend = array => array.filter(element => element.length === 4);
+const findFriend = array => array.filter(element => element.length === 4)
 
-console.log(findFriend(friendsArr));
+console.log(findFriend(friendsArr))
 // expect ['Ryan']
