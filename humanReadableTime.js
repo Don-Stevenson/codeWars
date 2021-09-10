e765c140d2c293a4a3bc8c8e8c00e71dcc8722a0
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-09 23:44:47 -0400

    remove semi colons

--- humanReadableTime.js
+++ humanReadableTime.js
@@ -8,22 +8,22 @@ const date = new Date().toLocaleDateString("en-US", {
   weekday: "long",
   day: "numeric",
   month: "long",
-});
-console.log({ date });
+})
+console.log({ date })
 
-const humanReadable = (seconds) => {
-  const mins = Math.floor(seconds / 60);
-  let hours = Math.floor(mins / 60);
-  let minRemain = mins - hours * 60;
-  let secsRemain = seconds - mins * 60;
+const humanReadable = seconds => {
+  const mins = Math.floor(seconds / 60)
+  let hours = Math.floor(mins / 60)
+  let minRemain = mins - hours * 60
+  let secsRemain = seconds - mins * 60
 
-  minRemain < 10 ? (minRemain = `0${minRemain}`) : minRemain;
-  secsRemain < 10 ? (secsRemain = `0${secsRemain}`) : secsRemain;
-  hours < 10 ? (hours = `0${hours}`) : hours;
+  minRemain < 10 ? (minRemain = `0${minRemain}`) : minRemain
+  secsRemain < 10 ? (secsRemain = `0${secsRemain}`) : secsRemain
+  hours < 10 ? (hours = `0${hours}`) : hours
 
-  return `${hours}:${minRemain}:${secsRemain}`;
-};
-console.log(humanReadable(30021));
-// expect 
+  return `${hours}:${minRemain}:${secsRemain}`
+}
+console.log(humanReadable(30021))
+// expect
 // { date: 'Wednesday, March 24' }
 // 08:20:21
