970573adb0b4b472ce0c7d9b08fc9defb7e4455d
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-09 23:45:50 -0400

    remove semi colons

--- learnMap.js
+++ learnMap.js
@@ -1,7 +1,7 @@
 // learning map
 // ************
 
-// takes in an array, transforms it with a function 
+// takes in an array, transforms it with a function
 // and returns a new array
 // returns a true or false statement
 // [false, false, fales, true, true, etc]
@@ -14,12 +14,12 @@
 // const mappedArray = array.map(x => x === 3)
 
 // gives nan where element is a string
-const array = ["a", "b", 3, 4, 5, 6, 7];
-const mappedArray = array.map(x => x * 3);
+const array = ["a", "b", 3, 4, 5, 6, 7]
+const mappedArray = array.map(x => x * 3)
 
 console.table(`mapped array is: ${mappedArray} 
 array is still: ${array}`)
 
 // expect:
-// mapped array is: NaN,NaN,9,12,15,18,21 
+// mapped array is: NaN,NaN,9,12,15,18,21
 // array is still: a,b,3,4,5,6,7
