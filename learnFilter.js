de8d4dda47036c6eed56d1264ad3636537aa33a7
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-13 21:24:48 -0400

    remove semicolons

--- learnFilter.js
+++ learnFilter.js
@@ -4,7 +4,7 @@
 const arr = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26, 27, 28, 29, 30,
-];
+]
 
 // const filterByGreaterThan3 = (arr) => {
 //  return arr.filter(num => num > 3)
@@ -20,16 +20,16 @@ const arr = [
 // console.log(arr.filter((element) => element >= 5));
 
 const isPrime = num => {
-  let counter = 0;
+  let counter = 0
   for (let i = 1; i <= num; i++) {
     if (num % i === 0) {
-      counter++;
+      counter++
     }
   }
   if (counter === 2) {
-    return `Yes, ${num} is a prime number.`;
-  } else return `No, ${num} is not a prime number.`;
-};
+    return `Yes, ${num} is a prime number.`
+  } else return `No, ${num} is not a prime number.`
+}
 
 // using filter to return an array of only prime nums by checking
 // if numbers are prime or not
@@ -42,6 +42,5 @@ const isPrime = num => {
 // console.log(isPrime(9));
 // No, 9 is not a prime number.
 
-
-console.log(isPrime(5));
+console.log(isPrime(5))
 // Yes, 5 is a prime number.
