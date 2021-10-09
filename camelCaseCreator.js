de2450308f73c2666b3272ca9fb82911738ff775
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-10-09 13:26:44 -0400

    with map, needs more work

--- camelCaseCreator.js
+++ camelCaseCreator.js
@@ -1,31 +1,52 @@
+// const toCamelCase = str => {
+//   // set up camel case array to add
+//   let camelCaseStr = []
+
+//   // if the string entered is empty, return an empty string
+//   if (str === "") {
+//     return ""
+//   }
+
+//   // c style loop that goes from 0 to the length of the string
+//   for (let i = 0; i < str.length; i++) {
+//     // if the instance of the string is either a space or a dash,
+//     // turn the next letter to upper case and add it to the camel case string
+//     if (str[i] === " " || str[i] === "-") {
+//       camelCaseStr.push(str[i + 1].toUpperCase())
+//       i++
+
+//       // if the above conditions are not met,
+//       // add the the letter to the camel case string unaltered
+//     } else camelCaseStr.push(str[i])
+//   }
+//   // turn the camel case array into a string, with no spaces or commas
+//   // return camelCaseStr.join("");
+
+//   // more complicated than join, change the array into a string,
+//   //replacing all the commas globally with no spaces ""
+//   return camelCaseStr.toString().replace(/,/gi, "")
+// }
+
+// console.log(toCamelCase("snakes on a plane"))
+// // expect : snakesOnAPlane
+
+
+// with Map
 const toCamelCase = str => {
-  // set up camel case array to add
-  let camelCaseStr = []
 
   // if the string entered is empty, return an empty string
   if (str === "") {
     return ""
   }
 
-  // c style loop that goes from 0 to the length of the string
-  for (let i = 0; i < str.length; i++) {
-    // if the instance of the string is either a space or a dash,
-    // turn the next letter to upper case and add it to the camel case string
-    if (str[i] === " " || str[i] === "-") {
-      camelCaseStr.push(str[i + 1].toUpperCase())
-      i++
 
-      // if the above conditions are not met,
-      // add the the letter to the camel case string unaltered
-    } else camelCaseStr.push(str[i])
-  }
-  // turn the camel case array into a string, with no spaces or commas
-  // return camelCaseStr.join("");
-
-  // more complicated than join, change the array into a string,
-  //replacing all the commas globally with no spaces ""
-  return camelCaseStr.toString().replace(/,/gi, "")
+  return str.split("").map((e, index) => {
+    // console.log(e, index)
+    if (e === " " || e === "-") return str[index + 1].toUpperCase()
+    else return e
+  }).toString().replace(/,/gi, "")
 }
 
-console.log(toCamelCase("snakes on a plane"))
+console.log(toCamelCase("snakes on a plane")) // returns => snakesOonAaPplane
 // expect : snakesOnAPlane
+
