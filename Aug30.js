44e090ece7405b51a0550226dc470f201bd43456
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-10-10 19:33:20 -0400

    Add odd filter

--- Aug30.js
+++ Aug30.js
@@ -5,4 +5,8 @@ const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const evenFilter = array => array.filter(e => e % 2 === 0)
 
-console.log(evenFilter(numberArray)) // => [ 2, 4, 6, 8, 10 ]
+const oddFilter = array => array.filter(e => e % 2 !== 0)
+
+console.log("evens", evenFilter(numberArray)) // => [ 2, 4, 6, 8, 10 ]
+
+console.log("odds", oddFilter(numberArray)) // => [ 1, 3, 5, 7, 9 ]
