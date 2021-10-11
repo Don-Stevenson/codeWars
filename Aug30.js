21c1657c73cf22165a7042e20d9cab7c4fdfb6a1
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-10-11 11:13:19 -0400

    add find five

--- Aug30.js
+++ Aug30.js
@@ -6,7 +6,10 @@ const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const evenFilter = array => array.filter(e => e % 2 === 0)
 
 const oddFilter = array => array.filter(e => e % 2 !== 0)
+const findFive = array => array.filter(e => e === 5)
 
 console.log("evens", evenFilter(numberArray)) // => [ 2, 4, 6, 8, 10 ]
 
 console.log("odds", oddFilter(numberArray)) // => [ 1, 3, 5, 7, 9 ]
+
+console.log("find 5", findFive(numberArray)) // => [ 1, 3, 5, 7, 9 ]
