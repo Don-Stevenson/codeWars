403f9451f8ffc15764da4c433d0217608476c18d
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-11-22 23:46:08 -0500

    make into one liner

--- reverseString.js
+++ reverseString.js
@@ -2,24 +2,23 @@
 // with split, reverse and join
 //***************************/
 
-const reverseString = str => str.split("").reverse().join("")
+// const reverseString = str => str.split("").reverse().join("")
 
 // with a c style loop
 // *******************
 
 // const reverseString = str => {
-//   let newStr = "";
+//   let newStr = ""
 //   for (let i = str.length - 1; i >= 0; i--) {
-//     newStr += str[i];
+//     newStr += str[i]
 //   }
-//   return newStr;
-// };
+//   return newStr
+// }
 
 // with recursion
 // **************
 
-// const reverseString = str => {
-//     return(str === "") ? "" : reverseString(str.substr(1)) + str.charAt(0);
-//   };
+const reverseString = str => (str === "") ? "" : reverseString(str.substr(1)) + str.charAt(0)
+
 
 console.log(reverseString("12345")) // => 54321
