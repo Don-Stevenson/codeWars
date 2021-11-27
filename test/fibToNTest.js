2c3e0ec8b4e4458b296e7af76dbc3894d57f43c4
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-11-27 17:40:49 -0500

    update to arrow functions and remove ;

--- test/fibToNTest.js
+++ test/fibToNTest.js
@@ -1,18 +1,18 @@
-const { fib, getPostiveFib } = require("../fibToN");
+const { fib, getPostiveFib } = require("../fibToN")
 
-const assert = require("assert");
+const assert = require("assert")
 
 describe("fib", () => {
-  it("12 should return 144", function () {
-    assert.deepStrictEqual(fib(12), 144);
-  });
-  it("-12 should return -144", function () {
-    assert.deepStrictEqual(fib(-12), -144);
-  });
-});
+  it("12 should return 144", () => {
+    assert.deepStrictEqual(fib(12), 144)
+  })
+  it("-12 should return -144", () => {
+    assert.deepStrictEqual(fib(-12), -144)
+  })
+})
 
 describe("Positive fib", () => {
-  it("15 should return 610", function () {
-    assert.deepStrictEqual(getPostiveFib(15), 610);
-  });
-});
+  it("15 should return 610", () => {
+    assert.deepStrictEqual(getPostiveFib(15), 610)
+  })
+})
