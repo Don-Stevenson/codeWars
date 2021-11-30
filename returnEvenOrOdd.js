7283730f308497a33e97ddab17544fb0e0dfadf6
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-11-30 12:09:33 -0500

    Remove semicolons

--- returnEvenOrOdd.js
+++ returnEvenOrOdd.js
@@ -6,14 +6,14 @@ const integers = [2, 4, 0, 100, 4, 11, 2602, 36]
 const findOutlier = integers => {
 
   // make an array that is only the even integers
-  const evenIntsArr = integers.filter(x => x % 2 === 0);
+  const evenIntsArr = integers.filter(x => x % 2 === 0)
   
   // make an array that is only the odd integers
 
-  const oddIntsArr = integers.filter(x => x % 2 !== 0);
+  const oddIntsArr = integers.filter(x => x % 2 !== 0)
   // if odds array is greater than one, then the outlier integer is even
   // if the odds array is not greater than one, then the outlier integer is odd
-  return oddIntsArr.length > 1 ? evenIntsArr[0] : oddIntsArr[0];
+  return oddIntsArr.length > 1 ? evenIntsArr[0] : oddIntsArr[0]
 }
 
 console.log(findOutlier(integers))
