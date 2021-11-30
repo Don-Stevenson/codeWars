d9bb75c37e5206f7fc8c52ac1c647975dbc504ca
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-11-30 13:42:43 -0500

    Remove semicolons

--- twoArraysHaveSameVal.js
+++ twoArraysHaveSameVal.js
@@ -1,11 +1,11 @@
 // check if the two arrays contain any of the same values,
 // if they do, remove the shared values only from the 1st array
-const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const array2 = [1, "b", "c", "d", "e", "f", "g", "h", "i"];
+const array2 = [1, "b", "c", "d", "e", "f", "g", "h", "i"]
 
 // filter out the one shared value from array1
-console.log(array1.filter((val) => !array2.includes(val))); // => [ 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(array1.filter((val) => !array2.includes(val))) // => [ 2, 3, 4, 5, 6, 7, 8, 9]
 
 // filter out all the values except those shared value from array1 
-console.log(array1.filter((val) => array2.includes(val))); // => [1]
+console.log(array1.filter((val) => array2.includes(val))) // => [1]
