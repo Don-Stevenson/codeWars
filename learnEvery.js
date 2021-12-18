c8bc74290cbed28eb549c91fb5d60b5c1365dd54
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-12-18 11:45:27 -0500

    Trying every on mixed and single value arrays

--- learnEvery.js
+++ learnEvery.js
@@ -7,7 +7,7 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const negArr = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
 
 // function that checks if a value is above 0 and returns true or false
-const isValueAbove = value => (value > 0 ? true : false)
+const isValueAbove = value => value > 0 ? true : false
 
 // console.log(arr.every(isValueAbove));
 // expect true
@@ -15,9 +15,12 @@ const isValueAbove = value => (value > 0 ? true : false)
 // console.log(negArr.every(isValueAbove))
 // expect false
 
+const mixedLetters = ["a", "c", "d", "e", "f", "g", "h"]
 
-const letterArr = ["a", "b", "c", "d", "e", "f", "g"]
+const letterArr = ["f", "f", "f", "f", "f", "f", "f"]
 
-const whenValueIsF = value=> console.log(value);
+const whenValueIsF = value => value === "f" ? true : false
 
-console.log(letterArr.every(whenValueIsF));
+console.log(letterArr.every(whenValueIsF)) // => true
+
+console.log(mixedLetters.every(whenValueIsF)) // => false
