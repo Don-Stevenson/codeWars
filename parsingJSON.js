f47feedffb81d1f2a5a0304150cfe989b3be162e
Don Stevenson <dsteven1@gmail.com>
2022-01-16 19:00:34 -0500

    Add catch fetch function

---
+++ parsingJSON.js
@@ -0,0 +1,17 @@
+// works in react
+
+const catFetch = async () => {
+    try {
+      const catResponse = await fetch(
+        "https://api.thecatapi.com/v1/breeds/search?q=maine%20coon"
+      )
+      console.log({catResponse});
+      const catJSON = await catResponse.json()
+      console.log("here as JSON", catJSON[0].description)
+      return catJSON
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  catFetch() // calls and logs to console 
