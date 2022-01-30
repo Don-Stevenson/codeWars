0c01ac2187a08cf15dc7b015db874009d5e74480
Don Stevenson <dsteven1@gmail.com>
2022-01-30 11:21:22 -0500

    Remove semicolons

--- learnReduce.js
+++ learnReduce.js
@@ -1,9 +1,9 @@
 // learn Reduce
 // ************
 
-// const array = [1, 2, 3, 4, 5, 6, 7];
+// const array = [1, 2, 3, 4, 5, 6, 7]
 
-const { add } = require("date-fns");
+const { add } = require("date-fns")
 
 // const obj = {
 //   a: 1,
@@ -16,15 +16,15 @@ const reducerFunc = (accumalator, currentVal, currentIndex, array) => {
   accumalator is: ${accumalator}
   current Value: ${currentVal}
   current Index: ${currentIndex}
-  array is: ${array}`);
+  array is: ${array}`)
   return array.push(accumalator)
-};
+}
 
 // takes in a an array and the runs a callback function on the array
-// let reducedArray = array.reduce(reducerFunc);
+// let reducedArray = array.reduce(reducerFunc)
 
 // in this case should expect 28 to be the answer
-// console.log(reducedArray);
+// console.log(reducedArray)
 
 // note that index is not zero based, rather it starts at 1.
 
@@ -35,13 +35,13 @@ const addObj = {
   a: 2,
   b: 4,
   c: 6
-};
+}
 
 // const total =
 // Getts the values out of add object
-// Object.values(addObj).reduce((t, value) => t + value);
+// Object.values(addObj).reduce((t, value) => t + value)
 // reduce where t is the accumilator, then value from each key
-// console.log(total); // 6
+// console.log(total) // 6
 
 // object values returns an array of values, that you can run .reduce on and use the function called reduce in
 Object.values(addObj).reduce(reducerFunc)
