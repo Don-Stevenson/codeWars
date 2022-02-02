03c15b3261bbaf5f6d639aea04d56d779efe651a
Don Stevenson <dsteven1@gmail.com>
2022-02-02 12:34:47 -0500

    Remove semicolons

--- test/addOneArrayTest.js
+++ test/addOneArrayTest.js
@@ -1,19 +1,19 @@
 // Testing the function that updates an array's values into a new array by adding one
 // **********************************************************************************
 
-const assert = require("assert");
+const assert = require("assert")
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const addOne = (groupOfNumbers) => {
-  return groupOfNumbers.map((element) => element + 1);
-};
+  return groupOfNumbers.map((element) => element + 1)
+}
 
 
 describe("Array", () => {
   describe("#addOne()", () => {
     it("should return an new array that adds one to each element", () => {
-      assert.deepEqual(addOne(array), [2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    });
-  });
-});
+      assert.deepEqual(addOne(array), [2, 3, 4, 5, 6, 7, 8, 9, 10])
+    })
+  })
+})
