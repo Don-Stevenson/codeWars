c31e4c753a87b3c68f28b038ac74ea953b33314e
Don Stevenson <dsteven1@gmail.com>
2022-02-05 12:54:31 -0500

    Remove semicolons

--- functionalJS/functional.js
+++ functionalJS/functional.js
@@ -3,31 +3,31 @@ module.exports = {
   addTwo,
   partialAdd,
   pipe,
-};
+}
 
 // prev = 1, current = addOne
 // prev = 2, current = addTwo
 // prev = 4, current = addOne
 // pipe:: fns... -> Number -> Number
 function pipe(...fns) {
-  return (initialValue) => fns.reduce(applyFn, initialValue);
+  return (initialValue) => fns.reduce(applyFn, initialValue)
 }
 
 function applyFn(value, fn) {
-  return fn(value);
+  return fn(value)
 }
 
 // partial application
 // partialAdd:: Number -> Number -> Number
 function partialAdd(adder) {
-  return (num) => num + adder;
+  return (num) => num + adder
 }
 
 // addOne:: Number -> Number
 function addOne(num) {
-  return num + 1;
+  return num + 1
 }
 
 function addTwo(num) {
-  return num + 2;
+  return num + 2
 }
