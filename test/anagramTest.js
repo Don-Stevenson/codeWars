c5dd8e277cb67ed6f5062214d0c572c4fae27199
Don Stevenson <dsteven1@gmail.com>
2022-02-06 13:53:23 -0500

    Remove semicolons

--- test/anagramTest.js
+++ test/anagramTest.js
@@ -1,6 +1,6 @@
-const assert = require("assert");
+const assert = require("assert")
 
-const { anagrams } = require("../anagrams");
+const { anagrams } = require("../anagrams")
 
 describe("Array", () => {
   describe("#anagrams()", () => {
@@ -8,16 +8,16 @@ describe("Array", () => {
       assert.deepStrictEqual(
         anagrams("hello", ["hello", "hi", "ehllo", "face", "olelh", "ho"]),
         ["hello", "ehllo", "olelh"]
-      );
-    });
+      )
+    })
     it("should return an empty array when there are no anagrams of the original word", () => {
       assert.deepStrictEqual(
         anagrams("hello", ["hgllo", "hi", "ihllo", "face", "plelh", "ho"]),
         []
-      );
-    });
-  });
-});
+      )
+    })
+  })
+})
 
-// console.log(anagrams("hello", ["hello", "hi", "ehllo", "face", "olelh", "ho"]));
+// console.log(anagrams("hello", ["hello", "hi", "ehllo", "face", "olelh", "ho"]))
 // expect [ 'hello', 'ehllo', 'olelh' ]
