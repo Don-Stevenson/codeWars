1c1e9727759f0334cf1c0d6ef6420ee4909fea22
Don Stevenson <dstevenson@tribalscale.com>
2022-02-07 10:03:27 -0500

    Add solution that uses ofNullable instead of if statement

---
+++ Twofer.java
@@ -0,0 +1,7 @@
+import static java.util.Optional.ofNullable;
+
+public class Twofer {
+    public String twofer(String name) {
+        return String.format("One for %s, one for me.", ofNullable(name).orElse("you"));
+    }
+}
