dd4d7762bb5a5a042982c67fdbdcf7614059e88f
Don Stevenson <dstevenson@tribalscale.com>
2022-03-05 14:03:54 -0500

    Update expected results for logVals

--- reduceJune72021.js
+++ reduceJune72021.js
@@ -9,7 +9,7 @@ const addingSquares = (accumulator, currentVal) =>
   accumulator + Math.pow(currentVal, 2)
 
 // callling the number array with Reduce and then passing in the adding the adding squares function
-console.log(numArray.reduce(addingSquares))
+// console.log(numArray.reduce(addingSquares))
 // expect 285
 
 // function that subtracts the root of each number
@@ -21,22 +21,14 @@ const multiplyCurrentVals = (accum, curVal) => accum * curVal
 // calling the number array with reduce which takes
 // in a function that finds the difference of sqrts
 
-console.log(numArray.reduce(subtractSquares))
+// console.log(numArray.reduce(subtractSquares))
 // expect -17.306000526035717
 
-console.log(numArray.reduce(multiplyCurrentVals))
+// console.log(numArray.reduce(multiplyCurrentVals))
 // expect 362880
 
-const logVals = (accum, curVal) => console.log({ accum }, { curVal })
+const logVals = (accum, curVal) => {return { accum }, { curVal }}
 
 console.log(numArray.reduce(logVals))
 // expect
-// { accum: 1 } { curVal: 2 }
-// { accum: undefined } { curVal: 3 }
-// { accum: undefined } { curVal: 4 }
-// { accum: undefined } { curVal: 5 }
-// { accum: undefined } { curVal: 6 }
-// { accum: undefined } { curVal: 7 }
-// { accum: undefined } { curVal: 8 }
-// { accum: undefined } { curVal: 9 }
-// undefined
+// { curVal: 9 }
