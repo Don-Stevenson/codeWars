6118c0ae21a58baa6e2dabfc09603da9ff0dbdb1
Don Stevenson <dstevenson@tribalscale.com>
2022-03-12 17:30:12 -0500

    Refactor code

--- march24.js
+++ march24.js
@@ -61,17 +61,15 @@ const sqrRootNum = A => Math.sqrt(A)
 const temps = [0, -5, -6, -11, -12, -3, -1]
 
 
-const meanTemps = (array) => array.reduce((a, b) => a + b) / array.length
+const meanTemps = array => array.reduce((a, b) => a + b) / array.length
 
 // console.log(meanTemps(temps))
 
-const standardDeviation = (popArray) => {
-   const popSize = popArray.length
-   const meanTemps = popArray.reduce((a, b) => a + b) / popArray.length
-   return Math.sqrt(popArray
-    .map(x => Math.pow(x - meanTemps, 2))
-    .reduce((a, b) => a + b) / popSize)
-}
+const standardDeviation = popArray => { 
+        return Math.sqrt(popArray
+                .map(x => Math.pow(x - meanTemps(popArray), 2))
+                .reduce((a, b) => a + b) / popArray.length)}
+
 console.log("Standard deviation is: ",standardDeviation(temps))
 // console.log(standardDeviation())
 // expect
