534765d0dd4033de9033973717691bef0dc53a20
Don Stevenson <dstevenson@tribalscale.com>
2022-11-05 12:57:30 -0400

    Handle no args

--- multiplyAWithB.js
+++ multiplyAWithB.js
@@ -6,7 +6,9 @@
 
 const args = process.argv.slice(2)
 
-const multiplyTwoNumbers = () => parseInt(args[0]) * parseInt(args[1])
-
+const multiplyTwoNumbers = () => {
+  if (!args.length) return "no args"
+  return parseInt(args[0]) * parseInt(args[1])
+}
 console.log(multiplyTwoNumbers(args)) /* 3 4 */
 // expect 12
