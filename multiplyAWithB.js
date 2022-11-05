855d80f78f6e2951fb6766832aaaa8da65c98191
Don Stevenson <dstevenson@tribalscale.com>
2022-11-05 12:52:46 -0400

    Refactor to implicit return

--- multiplyAWithB.js
+++ multiplyAWithB.js
@@ -6,9 +6,7 @@
 
 const args = process.argv.slice(2)
 
-const multiplyTwoNumbers = () => {
-  return parseInt(args[0]) * parseInt(args[1])
-}
+const multiplyTwoNumbers = () => parseInt(args[0]) * parseInt(args[1])
 
 console.log(multiplyTwoNumbers(args)) /* 3 4 */
 // expect 12
