bf5bf854969adbca3af65c81f488824c9dac5334
Don Stevenson <dstevenson@tribalscale.com>
2022-11-05 21:47:12 -0400

    Update args requirment and refactor to one line

--- multiplyAWithB.js
+++ multiplyAWithB.js
@@ -6,9 +6,8 @@
 
 const args = process.argv.slice(2)
 
-const multiplyTwoNumbers = () => {
-  if (!args.length) return "no args"
-  return parseInt(args[0]) * parseInt(args[1])
-}
+const multiplyTwoNumbers = () =>
+  args.length < 2 ? "not enough args" : parseInt(args[0]) * parseInt(args[1])
+
 console.log(multiplyTwoNumbers(args)) /* 3 4 */
 // expect 12
