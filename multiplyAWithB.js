ba84acb4e6d04472ed7a25c376f30640be6666bb
Don Stevenson <dstevenson@tribalscale.com>
2022-11-05 21:48:56 -0400

    Update not enough arg statement

--- multiplyAWithB.js
+++ multiplyAWithB.js
@@ -7,7 +7,9 @@
 const args = process.argv.slice(2)
 
 const multiplyTwoNumbers = () =>
-  args.length < 2 ? "not enough args" : parseInt(args[0]) * parseInt(args[1])
+  args.length < 2
+    ? "this program requires at least 2 numbers to be multiplied"
+    : parseInt(args[0]) * parseInt(args[1])
 
 console.log(multiplyTwoNumbers(args)) /* 3 4 */
 // expect 12
