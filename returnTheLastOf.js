bdfb98b355126f59b5aa5bb1472eca22b667cc03
Don Stevenson <dstevenson@tribalscale.com>
2022-12-08 20:15:56 -0500

    Work in progress

--- returnTheLastOf.js
+++ returnTheLastOf.js
@@ -1,14 +1,29 @@
 // returns the last n things of an array
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+// const lastNItems = (arr, numOfItems) => {
+//   let itemsArr = []
+
+//   for (let i = 1; i <= numOfItems; i++) {
+//     itemsArr.push(arr[arr.length - i])
+//   }
+//   return itemsArr
+// }
 
 const lastNItems = (arr, numOfItems) => {
-  let itemsArr = []
+  return arr.map((e, index) => {
+    console.log({ e }, { index }, { numOfItems })
 
-  for (let i = 1; i <= numOfItems; i++) {
-    itemsArr.push(arr[arr.length - i])
-  }
-  return itemsArr
+    let finalArr = []
+    if (index === arr.length - numOfItems) {
+      console.log(arr[index])
+      for (let i = 0; i < numOfItems; i++) {
+        finalArr.push(arr[index + i])
+      }
+      return finalArr
+    }
+  })
 }
 
 console.log(lastNItems(array, 3))
