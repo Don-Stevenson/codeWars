a8663a73f3d835775fda7cee412c3e643dba5f7a
Don Stevenson <dsteven1@gmail.com>
2022-12-26 12:34:35 -0500

    Add test case

--- overlappinArrays.js
+++ overlappinArrays.js
@@ -4,6 +4,8 @@
 const arrOfPoints = [2, 4, 55, 68] // false
 const arrOfPoints2 = [2, 5, 3, 6] // true
 const arrOfPoints3 = [1, 2, 2, 6] // true
+const arrOfPoints4 = [1, 1, 3, 4] // false
+
 // true cases
 // a ---- b
 ///       c ---- d
@@ -23,3 +25,4 @@ const doPointsOverlap = ([a, b, c, d]) =>
 console.log(doPointsOverlap(arrOfPoints)) // => false
 console.log(doPointsOverlap(arrOfPoints2)) // => true
 console.log(doPointsOverlap(arrOfPoints3)) // => true
+console.log(doPointsOverlap(arrOfPoints4)) // => false
