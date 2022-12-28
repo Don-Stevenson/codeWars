76a9bd0819b36e7eec652041c6ff1ab7332676ec
Don Stevenson <dsteven1@gmail.com>
2022-12-28 00:15:13 -0600

    Remove consts

--- test/overLappingArrays.test.js
+++ test/overLappingArrays.test.js
@@ -1,12 +1,5 @@
 const { doPointsOverlap } = require("../overlappinArrays.js")
 
-const arrOfPoints = [2, 4, 55, 68] // false
-const arrOfPoints2 = [2, 5, 3, 6] // true
-const arrOfPoints3 = [1, 2, 2, 6] // true
-const arrOfPoints4 = [1, 1, 3, 4] // false
-const arrOfPoints5 = [0, 0, 0, 0] // true
-
-
 const assert = require("assert")
 
 describe("test doPointsOverlap", () => {
