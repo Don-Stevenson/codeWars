cc763c45e44095ea49f8e140322213c79b87a4ed
Don Stevenson <dsteven1@gmail.com>
2022-12-28 00:14:44 -0600

    refactor code

--- test/overLappingArrays.test.js
+++ test/overLappingArrays.test.js
@@ -10,18 +10,20 @@ const arrOfPoints5 = [0, 0, 0, 0] // true
 const assert = require("assert")
 
 describe("test doPointsOverlap", () => {
-    it("[0,0,0,0] should return true", () => {
+    it("[0,0,0,0] should return true", () =>
         assert.deepStrictEqual(doPointsOverlap([0, 0, 0, 0]), true)
-    })
-    it("[1, 1, 3, 4] should return false", () => {
+    )
+    it("[1, 1, 3, 4] should return false", () =>
         assert.deepStrictEqual(doPointsOverlap([1, 1, 3, 4]), false)
-    })
-    it("[1, 2, 2, 6] should return true", () => {
+    )
+    it("[1, 2, 2, 6] should return true", () =>
         assert.deepStrictEqual(doPointsOverlap([1, 2, 2, 6]), true)
-    })
-    it("[2, 5, 3, 6] should return true", () => {
+    )
+    it("[2, 5, 3, 6] should return true", () =>
         assert.deepStrictEqual(doPointsOverlap([2, 5, 3, 6]), true)
-    })
-    it("[2, 4, 55, 68] shoukd return false", () => assert.deepStrictEqual(doPointsOverlap([2, 4, 55, 68]), false))
+    )
+    it("[2, 4, 55, 68] should return false", () =>
+        assert.deepStrictEqual(doPointsOverlap([2, 4, 55, 68]), false)
+    )
 })
 
