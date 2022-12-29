244b5c6c3ac661ff7cc30a12338c39ccabe5afb3
Don Stevenson <dsteven1@gmail.com>
2022-12-28 22:36:05 -0600

    Update to const

--- test/addOneArray.test.js
+++ test/addOneArray.test.js
@@ -3,7 +3,7 @@
 
 const assert = require("assert")
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const addOne = (groupOfNumbers) => {
   return groupOfNumbers.map((element) => element + 1)
