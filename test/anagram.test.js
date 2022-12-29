d233c66bb326ae7834e7549dfedd42016506fed3
Don Stevenson <dsteven1@gmail.com>
2022-12-29 00:39:22 -0600

    Remove unnecessary describe ()=>

--- test/anagram.test.js
+++ test/anagram.test.js
@@ -2,20 +2,18 @@ const assert = require("assert")
 
 const { anagrams } = require("../anagrams")
 
-describe("Array", () => {
-  describe("#anagrams()", () => {
-    it("should return an new array that contains only anagrams of the original word", () => {
-      assert.deepStrictEqual(
-        anagrams("hello", ["hello", "hi", "ehllo", "face", "olelh", "ho"]),
-        ["hello", "ehllo", "olelh"]
-      )
-    })
-    it("should return an empty array when there are no anagrams of the original word", () => {
-      assert.deepStrictEqual(
-        anagrams("hello", ["hgllo", "hi", "ihllo", "face", "plelh", "ho"]),
-        []
-      )
-    })
+describe("#anagrams()", () => {
+  it("should return an new array that contains only anagrams of the original word", () => {
+    assert.deepStrictEqual(
+      anagrams("hello", ["hello", "hi", "ehllo", "face", "olelh", "ho"]),
+      ["hello", "ehllo", "olelh"]
+    )
+  })
+  it("should return an empty array when there are no anagrams of the original word", () => {
+    assert.deepStrictEqual(
+      anagrams("hello", ["hgllo", "hi", "ihllo", "face", "plelh", "ho"]),
+      []
+    )
   })
 })
 
