50d3a1ce95b4f1c39c7bb6b384d7d59f9c71ba88
Don Stevenson <dsteven1@gmail.com>
2023-01-21 00:14:33 -0500

    Filtering after the fetch has been made works

--- filterCats.js
+++ filterCats.js
@@ -3,10 +3,19 @@
 // can you filter in the fetch? 
 // i.e. does filter handle async?
 
-const catFetchFilter = async () => {
-    const filteredCatsArr = await fetch("catapi")
+import fetch from 'node-fetch'
 
-    return filteredCatsArr
+
+const catFetchFilter = async () => {
+    const filteredCatsArr = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+    // const filteredCatsArrJSON = await filteredCatsArr.json().filter((cat) => cat.id === '4d7')
+    // console.log(filteredCatsArrJSON) => throws and error because you cant use filter on a async call
+
+
+    const filteredCatsArrJSON = await filteredCatsArr.json()
+    const longCats = filteredCatsArrJSON.filter((cat) => cat.height > 500)
+    console.log(longCats)
+    return longCats
 }
 
-console.log(catFetchFilter());
+catFetchFilter()
