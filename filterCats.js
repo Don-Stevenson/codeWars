6fa37c992797d7a56006ef98fa41daf27641a15a
Don Stevenson <dsteven1@gmail.com>
2023-01-21 00:26:03 -0500

    Put inside try catch block

--- filterCats.js
+++ filterCats.js
@@ -7,16 +7,21 @@ import fetch from 'node-fetch'
 
 
 const catFetchFilter = async () => {
-    const filteredCatsArr = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-    // const filteredCatsArrJSON = await filteredCatsArr.json().filter((cat) => cat.height > 500)
-    // console.log(filteredCatsArrJSON) => throws and error because you cant use filter on a async call
+    try {
+        const filteredCatsArr = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+        // const filteredCatsArrJSON = await filteredCatsArr.json().filter((cat) => cat.height > 500)
+        // console.log(filteredCatsArrJSON) => throws and error because you cant use filter on a async call
 
-    const filteredCatsArrJSON = await filteredCatsArr.json()
+        const filteredCatsArrJSON = await filteredCatsArr.json()
 
-    //  Filtering after the fetch has been made works
-    const longCats = filteredCatsArrJSON.filter((cat) => cat.height > 500)
-    console.log(longCats)
-    return longCats
+        //  Filtering after the fetch has been made works
+        const longCats = filteredCatsArrJSON.filter((cat) => cat.height > 500)
+        console.log(longCats)
+        return longCats
+
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 catFetchFilter()
