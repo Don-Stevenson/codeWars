75a2da2e65bcf1b718617f0b332776540fbc7bb0
Don Stevenson <dsteven1@gmail.com>
2023-01-22 17:27:49 -0500

    Update format

--- filterCats.js
+++ filterCats.js
@@ -1,14 +1,15 @@
 // fetch cats from cat api
 // filter them on some condition
-// can you filter in the fetch? 
+// can you filter in the fetch?
 // i.e. does filter handle async?
 
-import fetch from 'node-fetch'
-
+import fetch from "node-fetch"
 
 const catFetchFilter = async () => {
     try {
-        const filteredCatsArr = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+        const filteredCatsArr = await fetch(
+            "https://api.thecatapi.com/v1/images/search?limit=10"
+        )
         // const filteredCatsArrJSON = await filteredCatsArr.json().filter((cat) => cat.height > 500)
         // console.log(filteredCatsArrJSON) => throws and error because you cant use filter on a async call
 
@@ -18,10 +19,8 @@ const catFetchFilter = async () => {
         const longCats = filteredCatsArrJSON.filter((cat) => cat.height > 500)
         console.log(longCats)
         return longCats
-
     } catch (error) {
         console.error(error)
     }
 }
-
 catFetchFilter()
