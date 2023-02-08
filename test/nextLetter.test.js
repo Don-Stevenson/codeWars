d983658063b564979c0ad4a8c3175dd0e6190a05
Don Stevenson <dsteven1@gmail.com>
2023-02-08 14:44:49 -0500

    Update test descriptions

--- test/nextLetter.test.js
+++ test/nextLetter.test.js
@@ -3,22 +3,22 @@ const assert = require("assert")
 const { nextLetter } = require("../nextLetter")
 
 describe("next letter", () => {
-    it("given a should return b", () => {
+    it("when given 'a', it should return 'b'", () => {
         assert.deepStrictEqual(
             nextLetter("a"), "b"
         )
     })
-    it("given f should return g", () => {
+    it("when given 'f', it should return 'g'", () => {
         assert.deepStrictEqual(
             nextLetter("f"), "g"
         )
     })
-    it("given h should return i", () => {
+    it("when given 'h', it should return 'i'", () => {
         assert.deepStrictEqual(
             nextLetter("h"), "i"
         )
     })
-    it("given h should return i", () => {
+    it("when given 'h', it should return 'i'", () => {
         assert.deepStrictEqual(
             nextLetter("z"), undefined
         )
