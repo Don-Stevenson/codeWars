381a7d673219efc49ad49c666c5eb437c26d7705
Don Stevenson <dsteven1@gmail.com>
2023-02-07 20:45:23 -0500

    Test passes; f returns g

--- test/nextLetter.test.js
+++ test/nextLetter.test.js
@@ -3,7 +3,9 @@ const assert = require("assert")
 const { nextLetter } = require("../nextLetter")
 
 describe("next letter", () => {
-    assert.deepStrictEqual("f to return g", () => assert.deepStrictEqual(
-        anagrams("f"), "g"
-    ))
+    it("given f should return g", () => {
+        assert.deepStrictEqual(
+            nextLetter("f"), "g"
+        )
+    })
 })
