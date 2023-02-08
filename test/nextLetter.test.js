f3de0c8da314c1066fa6ad0d0dc2146390331eab
Don Stevenson <dsteven1@gmail.com>
2023-02-08 14:42:39 -0500

    Add next letter test

--- test/nextLetter.test.js
+++ test/nextLetter.test.js
@@ -3,6 +3,11 @@ const assert = require("assert")
 const { nextLetter } = require("../nextLetter")
 
 describe("next letter", () => {
+    it("given a should return b", () => {
+        assert.deepStrictEqual(
+            nextLetter("a"), "b"
+        )
+    })
     it("given f should return g", () => {
         assert.deepStrictEqual(
             nextLetter("f"), "g"
