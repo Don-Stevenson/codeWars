5955c8dd2b2396958ccd35bafa262ddec8bd2fc2
Don Stevenson <dsteven1@gmail.com>
2023-03-05 20:25:31 -0500

    Add -1 not equal test

--- test/addOneArray.test.js
+++ test/addOneArray.test.js
@@ -1,9 +1,9 @@
 // Testing the function that updates an array's values into a new array by adding one
 // **********************************************************************************
 
-const assert = require("assert")
+const assert = require("assert");
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const addOne = (groupOfNumbers) => {
   return groupOfNumbers.map((element) => element + 1)
@@ -11,12 +11,11 @@ const addOne = (groupOfNumbers) => {
 
 describe("#addOne()", () => {
   it("should return an new array that adds one to each element", () =>
-    assert.deepEqual(addOne(array), [2, 3, 4, 5, 6, 7, 8, 9, 10])
-  )
-})
+    assert.deepEqual(addOne(array), [2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
-describe("#addOne()", () => {
   it("should NOT equal an array of the same integers", () =>
-    assert.notDeepEqual(addOne(array), [1, 2, 3, 4, 5, 6, 7, 8, 9])
-  )
+    assert.notDeepEqual(addOne(array), [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+  it("should NOT equal an array of the of '-1' integers", () =>
+    assert.notDeepEqual(addOne(array), [0, 1, 2, 3, 4, 5, 6, 7, 8]))
 })
