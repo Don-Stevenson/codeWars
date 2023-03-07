8cb956df100aa76a685d5c1226686ff243fd9fae
Don Stevenson <dsteven1@gmail.com>
2023-03-06 23:52:33 -0500

    Add -2 not equal test

--- test/addOneArray.test.js
+++ test/addOneArray.test.js
@@ -18,4 +18,7 @@ describe("#addOne()", () => {
 
   it("should NOT equal an array of the of '-1' integers", () =>
     assert.notDeepEqual(addOne(array), [0, 1, 2, 3, 4, 5, 6, 7, 8]))
+
+  it("should NOT equal an array of the of '-2' integers", () =>
+    assert.notDeepEqual(addOne(array), [-1, 0, 1, 2, 3, 4, 5, 6, 7]))
 })
