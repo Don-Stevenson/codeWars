8f0a52fab3d278a7da4c46abf219a1f7d8c9be5c
Don Stevenson <dsteven1@gmail.com>
2023-03-09 20:44:37 -0500

    Refactor function

--- addOne.js
+++ addOne.js
@@ -1,6 +1,4 @@
-const addOne = (groupOfNumbers) => {
-    return groupOfNumbers.map((element) => element + 1)
-}
+const addOne = groupOfNumbers => groupOfNumbers.map(element => element + 1)
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
