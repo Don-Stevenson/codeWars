56c91b62213043ec24390512fc67654f25ee8b74
Don Stevenson <dstevenson@tribalscale.com>
2023-03-22 22:05:08 -0400

    Add postive fib test for 20

--- tests/fibToN.test.js
+++ tests/fibToN.test.js
@@ -11,6 +11,9 @@ describe("Positive fib", () => {
   it("15 should return 610", () =>
     assert.deepStrictEqual(getPostiveFib(15), 610))
 
-  it("15 should return 610", () =>
+  it("17 should return 1597", () =>
     assert.deepStrictEqual(getPostiveFib(17), 1597))
+
+  it("20 should return 6765", () =>
+    assert.deepStrictEqual(getPostiveFib(20), 6765))
 })
