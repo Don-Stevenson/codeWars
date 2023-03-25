b4aa164bcd8bec269bc5524c4abae91a3d0d8903
Don Stevenson <dsteven1@gmail.com>
2023-03-25 11:49:02 -0400

    Refactor pig latin generator

--- pigLatinGenerator.js
+++ pigLatinGenerator.js
@@ -4,14 +4,13 @@
 // with map, test and regex
 // ************************
 
-const pigIt = str => {
-  const splitStr = str.split(" ")
-  return (pigLatinStr = splitStr
-    .map(element =>
+const pigIt = (str) =>
+  str
+    .split(" ")
+    .map((element) =>
       /[a-zA-Z]+/.test(element) ? element.slice(1) + element[0] + "ay" : element
     )
-    .join(" "))
-}
+    .join(" ");
 
 // with replace
 // where \w is the first character and \w+ is the rest of the word
@@ -19,7 +18,7 @@ const pigIt = str => {
 // ************
 // const pigIt = (str) => {return str.replace(/(\w)(\w+)/g, '$2$1ay')};
 
-console.log(pigIt("hello Pig latin is cool")) // => ellohay igPay atinlay siay oolcay
-console.log(pigIt("hello my name is Miss Piggy")) // => ellohay igPay atinlay siay oolcay
-console.log(pigIt("farenheit 451")) // => arenheitfay 451
-console.log(pigIt("No F!!&&in' way dude!")) // => oNay !!&&in'Fay ayway ude!day
+console.log(pigIt("hello Pig latin is cool")); // => ellohay igPay atinlay siay oolcay
+console.log(pigIt("hello my name is Miss Piggy")); // => ellohay igPay atinlay siay oolcay
+console.log(pigIt("farenheit 451")); // => arenheitfay 451
+console.log(pigIt("No F!!&&in' way dude!")); // => oNay !!&&in'Fay ayway ude!day
