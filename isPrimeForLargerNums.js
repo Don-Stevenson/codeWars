0ab0947938819542658c89bff6e065a0c1b63dff
Don Stevenson <dstevenson@tribalscale.com>
2023-04-03 00:10:33 -0400

    Add biggest number before rounding errors creep in

--- isPrimeForLargerNums.js
+++ isPrimeForLargerNums.js
@@ -4,7 +4,7 @@ const isPrimeForLargerNums = num => {
   const last_number = +num.toString().slice(-1)
 
   if (last_number % 2 === 0 || last_number % 5 === 0)
-    return `${num} is not a prime number.`
+    return `here ${num} is not a prime number.`
   else {
     let counter = 0
     for (let i = 1; i <= num; i++) {
@@ -17,5 +17,5 @@ const isPrimeForLargerNums = num => {
   }
 }
 // console.log(isPrimeForLargerNums(11))
-console.log(isPrimeForLargerNums(1346001801791))
-// Yes, 1346001801791 is a prime number.
+console.log(isPrimeForLargerNums(1346001801791331))
+//Yes, 1346001801791331 is a prime number.
