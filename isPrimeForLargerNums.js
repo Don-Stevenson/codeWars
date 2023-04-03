d1af3221d60a788f17fa80cc2e097fdc94c35e7d
Don Stevenson <dstevenson@tribalscale.com>
2023-04-02 21:55:35 -0400

    Add modulo checks for last number

--- isPrimeForLargerNums.js
+++ isPrimeForLargerNums.js
@@ -1,15 +1,20 @@
 const isPrimeForLargerNums = num => {
-  let counter = 0
-  for (let i = 1; i <= num; i++) {
-    if (num % i === 0) {
-      counter++
-      if (counter > 2) {
-        return `No, ${num} is not a prime number.`
+  const last_number = +num.toString().slice(-1)
+
+  if (last_number % 2 === 0 || last_number % 5 === 0)
+    return `${num} is not a prime number.`
+  else {
+    let counter = 0
+    for (let i = 1; i <= num; i++) {
+      if (num % i === 0) {
+        counter++
+        if (counter > 2) {
+          return `${num} is not a prime number.`
+        }
       }
     }
+    return `Yes, ${num} is a prime number.`
   }
-  return `Yes, ${num} is a prime number.`
 }
-
 // console.log(isPrimeForLargerNums(11))
-console.log(isPrimeForLargerNums(11001013))
+console.log(isPrimeForLargerNums(134600180))
