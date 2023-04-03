f874f633126f19ae2d850cebbe2e07be25244282
Don Stevenson <dstevenson@tribalscale.com>
2023-04-02 21:07:50 -0400

    Move counter condition to eleminate non-primes ASAP

--- isPrimeForLargerNums.js
+++ isPrimeForLargerNums.js
@@ -3,13 +3,14 @@ const isPrimeForLargerNums = num => {
   for (let i = 1; i <= num; i++) {
     if (num % i === 0) {
       counter++
+      if (counter > 2) {
+        return `No, ${num} is not a prime number.`
+      }
     }
+    console.log({ counter })
   }
-  while (counter > 2) {
-    return `No, ${num} is not a prime number.`
-  }
-
   return `Yes, ${num} is a prime number.`
 }
 
-console.log(isPrimeForLargerNums(11))
+// console.log(isPrimeForLargerNums(11))
+console.log(isPrimeForLargerNums(110))
