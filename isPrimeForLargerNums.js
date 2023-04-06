c5ba75a78449038f0e260160dceb0d8e2092d015
Don Stevenson <dsteven1@gmail.com>
2023-04-06 18:52:47 -0400

    Add parse Int

--- isPrimeForLargerNums.js
+++ isPrimeForLargerNums.js
@@ -3,23 +3,23 @@
 const isPrimeForLargerNums = num => {
 
   if (parseInt(num) <= 2)
-    return `Yes, ${num} is a prime number.`
+    return `Yes, ${parseInt(num)} is a prime number.`
 
-  const last_number = +num.toString().slice(-1)
+  const last_number = +parseInt(num).toString().slice(-1)
 
   if (last_number % 2 === 0 || last_number % 5 === 0)
     return `${num} is not a prime number.`
   else {
     let counter = 0
-    for (let i = 1; i <= num; i++) {
-      if (num % i === 0) {
+    for (let i = 1; i <= parseInt(num); i++) {
+      if (parseInt(num) % i === 0) {
         counter++
-        if (counter > 2)`${num} is not a prime number.`
+        if (counter > 2)`${parseInt(num)} is not a prime number.`
       }
-      return `Yes, ${num} is a prime number.`
+      return `Yes, ${parseInt(num)} is a prime number.`
     }
   }
 }
 
-console.log(isPrimeForLargerNums((`13460018017913313`)))
+console.log(isPrimeForLargerNums((`13460018017913313102021`)))
 //Yes, 1346001801791331 is a prime number.
