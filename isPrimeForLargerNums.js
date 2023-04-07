b0fc42dde89e6eb60b2c1bc8ca44715273c9a457
Don Stevenson <dstevenson@tribalscale.com>
2023-04-06 23:42:47 -0400

    Remove unnecessary parse int

--- isPrimeForLargerNums.js
+++ isPrimeForLargerNums.js
@@ -1,25 +1,22 @@
 // add some checks to avoid running counter unneccessarily
 
 const isPrimeForLargerNums = num => {
+  if (num <= 2) return `Yes, ${num} is a prime number.`
 
-  if (parseInt(num) <= 2)
-    return `Yes, ${parseInt(num)} is a prime number.`
-
-  const last_number = +parseInt(num).toString().slice(-1)
-
+  const last_number = +num.toString().slice(-1)
   if (last_number % 2 === 0 || last_number % 5 === 0)
-    return `${num} is not a prime number.`
+    return `No, ${num} is not a prime number.`
   else {
     let counter = 0
-    for (let i = 1; i <= parseInt(num); i++) {
-      if (parseInt(num) % i === 0) {
+    for (let i = 1; i <= num; i++) {
+      if (num % i === 0) {
         counter++
-        if (counter > 2)`${parseInt(num)} is not a prime number.`
+        if (counter > 2) `No, ${num} is not a prime number.`
       }
-      return `Yes, ${parseInt(num)} is a prime number.`
     }
+    return `Yes, ${num} is a prime number.`
   }
 }
 
-console.log(isPrimeForLargerNums((`13460018017913313102021`)))
-//Yes, 1346001801791331 is a prime number.
+console.log(isPrimeForLargerNums(13460013131))
+//Yes, 13460013131 is a prime number.
