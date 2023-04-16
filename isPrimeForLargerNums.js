4b6cec64b4163cc5f811ce1d551d38f86f13f3a1
Don Stevenson <dstevenson@tribalscale.com>
2023-04-16 15:16:37 -0400

    Add new number

--- isPrimeForLargerNums.js
+++ isPrimeForLargerNums.js
@@ -30,7 +30,7 @@ const startTime = performance.now()
 const endTime = performance.now()
 
 console.log(
-  `${isPrimeForLargerNums(121212113)}
+  `${isPrimeForLargerNums(121212117)}
 Call to run isPrimeForLargerNums took ${(endTime - startTime) / 1000} seconds`
 )
 //Yes, 134600131311 is a prime number.
