6ba8bfc6ffa7602e038211a16d277ef7e06aeacc
Don Stevenson <dsteven1@gmail.com>
2023-04-26 23:37:37 -0400

    Add number of license plates possible, calculate number of cars for each person

--- licensePlateGen.js
+++ licensePlateGen.js
@@ -1,5 +1,14 @@
-// return a string that randomly generates a license plate that is valid in Ontario
+// how many possible license plates in Ontario?
+const howManyLicensePlates = () => 26 * 26 * 26 * 26 * 10 * 10 * 10
+console.log(howManyLicensePlates())
 
+// 14 570 000 million people in ontario
+// if each person gets one car, how many cars with different liscense plates can each person own?
+console.log(`With 14 570 000 million people in ontario, and approx 456976000 license plates possible given our current format. 
+Each person can own max of ${Math.round((456976000 / 14570000))} cars with different license plates`);
+
+
+// return a string that randomly generates a license plate that is valid in Ontario
 const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 const licensePlateGen = () => {
