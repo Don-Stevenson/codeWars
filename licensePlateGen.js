d6b3a93fd14eb3ceab1f89af824d2384d93165c6
Don Stevenson <dsteven1@gmail.com>
2023-04-26 23:08:10 -0400

    Generate letters with alphabet str

--- licensePlateGen.js
+++ licensePlateGen.js
@@ -1,13 +1,13 @@
-const alphaArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
+// const alphaBet = new Set("abcdefghijklmnopqrstuvwxyz")
 // return a string that randomly generates a license plate that is valid in Ontario
 
 const licensePlateGen = () => {
     const randAlphaNum = () => Math.floor(Math.random() * 28)
     const randNum = () => Math.floor(Math.random() * 10)
+
     const threeNums = () => `${randNum()}${randNum()}${randNum()}`
-    const randomLetter = () => alphaArr[randAlphaNum()].toUpperCase()
+    const randomLetter = () => alphBetStr[randAlphaNum()]
     const fourLetters = () => `${randomLetter()}${randomLetter()}${randomLetter()}${randomLetter()}`
     return `${fourLetters()}-${threeNums()}`
 }
