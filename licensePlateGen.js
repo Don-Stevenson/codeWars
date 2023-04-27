41ebd892bdcb5ad4964b348ff3fd0e0c7cdaafdb
Don Stevenson <dsteven1@gmail.com>
2023-04-26 23:23:40 -0400

    Add check to see if random letter is undefined. Handle with recursion

--- licensePlateGen.js
+++ licensePlateGen.js
@@ -1,14 +1,17 @@
-const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// const alphaBet = new Set("abcdefghijklmnopqrstuvwxyz")
 // return a string that randomly generates a license plate that is valid in Ontario
 
+const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 const licensePlateGen = () => {
-    const randAlphaNum = () => Math.floor(Math.random() * 28)
     const randNum = () => Math.floor(Math.random() * 10)
 
+    const randomLetter = () => {
+        const generatedAlphabetLetter = alphBetStr[Math.floor(Math.random() * 26)]
+        return generatedAlphabetLetter ? generatedAlphabetLetter : randomLetter()
+    }
     const threeNums = () => `${randNum()}${randNum()}${randNum()}`
-    const randomLetter = () => alphBetStr[randAlphaNum()]
     const fourLetters = () => `${randomLetter()}${randomLetter()}${randomLetter()}${randomLetter()}`
+
     return `${fourLetters()}-${threeNums()}`
 }
 
