9c595dcc6917822b594b90b7571980e1331a8f28
Don Stevenson <dsteven1@gmail.com>
2023-04-26 23:04:49 -0400

    Generating 4 letters now

--- licensePlateGen.js
+++ licensePlateGen.js
@@ -6,11 +6,10 @@ const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const licensePlateGen = () => {
     const randAlphaNum = () => Math.floor(Math.random() * 28)
     const randNum = () => Math.floor(Math.random() * 10)
-    const threeLicensePlateNums = () => `${randNum()}${randNum()}${randNum()}`
-
-
-
-    return `ABCD-${threeLicensePlateNums()}`
+    const threeNums = () => `${randNum()}${randNum()}${randNum()}`
+    const randomLetter = () => alphaArr[randAlphaNum()].toUpperCase()
+    const fourLetters = () => `${randomLetter()}${randomLetter()}${randomLetter()}${randomLetter()}`
+    return `${fourLetters()}-${threeNums()}`
 }
 
 console.log(licensePlateGen());
