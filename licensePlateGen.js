19cf69bb45f5cec4a74c607eead80e151ec7114c
Don Stevenson <dsteven1@gmail.com>
2023-04-28 23:43:07 -0400

    Format document

--- licensePlateGen.js
+++ licensePlateGen.js
@@ -1,29 +1,29 @@
 // how many possible license plates in Ontario?
-const howManyLicensePlates = 26 * 26 * 26 * 26 * 10 * 10 * 10;
+const howManyLicensePlates = 26 * 26 * 26 * 26 * 10 * 10 * 10
 
 // 14 570 000 million people in ontario
 // if each person gets one car, how many cars with different liscense plates can each person own?
 console.log(`With 14 570 000 million people in ontario, and approx 456976000 license plates possible given our current format. 
 Each person can own max of ${Math.round(
-    howManyLicensePlates / 14570000
-)} cars with different license plates`);
+  howManyLicensePlates / 14570000
+)} cars with different license plates`)
 // expect ==> "... Each person can own max of 31 cars with different license plates`"
 
 // return a string that randomly generates a license plate that is valid in Ontario
-const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-const randNum = () => Math.floor(Math.random() * 10);
+const randNum = () => Math.floor(Math.random() * 10)
 
 const randomLetter = () => {
-    const generatedAlphabetLetter = alphBetStr[Math.floor(Math.random() * 26)];
-    return generatedAlphabetLetter ? generatedAlphabetLetter : randomLetter();
-};
+  const generatedAlphabetLetter = alphBetStr[Math.floor(Math.random() * 26)]
+  return generatedAlphabetLetter ? generatedAlphabetLetter : randomLetter()
+}
 
-const threeNums = () => `${randNum()}${randNum()}${randNum()}`;
+const threeNums = () => `${randNum()}${randNum()}${randNum()}`
 
 const fourLetters = () =>
-    `${randomLetter()}${randomLetter()}${randomLetter()}${randomLetter()}`;
+  `${randomLetter()}${randomLetter()}${randomLetter()}${randomLetter()}`
 
-const licensePlateGen = () => `${fourLetters()}-${threeNums()}`;
+const licensePlateGen = () => `${fourLetters()}-${threeNums()}`
 
-console.log(licensePlateGen());
+console.log(licensePlateGen())
