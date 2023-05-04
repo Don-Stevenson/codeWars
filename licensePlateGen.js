c0412724622c8678a7b1a1fa34192af2c80dc49a
Don Stevenson <dstevenson@tribalscale.com>
2023-05-04 17:51:08 -0400

    Refactor license plate

--- licensePlateGen.js
+++ licensePlateGen.js
@@ -3,8 +3,8 @@ const howManyLicensePlates = 26 * 26 * 26 * 26 * 10 * 10 * 10
 
 // 14 570 000 million people in ontario
 // if each person gets one car, how many cars with different liscense plates can each person own?
-console.log(`With 14.57 million people in Ontario, and given our current format approximately 456.976 million license plates possible
-, each person currently in Ontario own max of ${Math.round(
+console.log(`With 14.57 million people in Ontario, and given our current format approximately 456.976 million license plates possible, 
+each person currently in Ontario own max of ${Math.round(
   howManyLicensePlates / 14570000
 )} cars with different license plates`)
 // expect ==> "... Each person can own max of 31 cars with different license plates`"
@@ -12,18 +12,18 @@ console.log(`With 14.57 million people in Ontario, and given our current format 
 // return a string that randomly generates a license plate that is valid in Ontario
 const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-const randNum = () => Math.floor(Math.random() * 10)
+const randNum = () => Math.floor(Math.random() * 10).toString()
 
-const randomLetter = () => {
-  const generatedAlphabetLetter = alphBetStr[Math.floor(Math.random() * 26)]
-  return generatedAlphabetLetter ? generatedAlphabetLetter : randomLetter()
-}
+const randomLetter = () => alphBetStr[Math.floor(Math.random() * 26)]
 
-const threeNums = () => `${randNum()}${randNum()}${randNum()}`
-
-const fourLetters = () =>
-  `${randomLetter()}${randomLetter()}${randomLetter()}${randomLetter()}`
-
-const licensePlateGen = () => `${fourLetters()}-${threeNums()}`
+const licensePlateGen = () =>
+  randomLetter() +
+  randomLetter() +
+  randomLetter() +
+  randomLetter() +
+  "-" +
+  randNum() +
+  randNum() +
+  randNum()
 
 console.log(licensePlateGen())
