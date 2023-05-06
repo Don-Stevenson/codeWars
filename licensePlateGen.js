a0582908a54a9a0ac5f6533314d8d4615f4cfbf6
Don Stevenson <dsteven1@gmail.com>
2023-05-06 03:51:38 -0400

    Update comments

--- licensePlateGen.js
+++ licensePlateGen.js
@@ -1,6 +1,5 @@
 // how many possible license plates in Ontario?
 const howManyLicensePlates = 26 * 26 * 26 * 26 * 10 * 10 * 10
-
 // 14 570 000 million people in ontario
 // if each person gets one car, how many cars with different liscense plates can each person own?
 console.log(`With 14.57 million people in Ontario, and given our current format, approximately 456.976 million license plates are possible. Given these numbers, then
@@ -9,6 +8,7 @@ each person currently in Ontario can currently own a max of ${Math.round(
 )} cars with different license plates`)
 // expect ==> "... Each person can own max of 31 cars with different license plates`"
 
+// ********************
 // return a string that randomly generates a license plate that is valid in Ontario
 const alphBetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
