43bb19a6635537accc587d5207949458fe6de752
Don Stevenson <dsteven1@gmail.com>
2023-05-31 23:25:55 -0400

    Add man reverse str console log

--- reverseString.js
+++ reverseString.js
@@ -25,3 +25,5 @@ console.log(reverseString("12345")) // => 54321
 
 console.log(reverseString("larry")) // => yrral
 console.log(reverseString("reverse")) // => esrever
+
+console.log(reverseString("man")) // => nam
