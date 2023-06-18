5493c1afebeac8c9580f2d05602b220b3f5c0fdb
Don Stevenson <dstevenson@tribalscale.com>
2023-06-17 23:34:05 -0400

    Clean up zeros

--- moveZeros.js
+++ moveZeros.js
@@ -15,4 +15,4 @@ console.log(moveZeros([0, 1, 4, 45, 0, 0, 7, 9]))
 // expected result [  1, 4, 45, 7,  9, 0,  0, 0 ]
 
 console.log(moveZeros([9, 0, 0, 12, 123, 0, 0, 5]))
-// expected result [  1, 4, 45, 7,  9, 0,  0, 0 ]
+// expected result [ 9, 12, 123, 5, 0, 0, 0, 0 ]
