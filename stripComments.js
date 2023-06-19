0a01952fafc0120743f351de5528b73afee51ef8
Don Stevenson <dsteven1@gmail.com>
2023-06-19 00:47:55 -0400

    refactor out unnecesary variable

--- stripComments.js
+++ stripComments.js
@@ -2,15 +2,12 @@
 // that you enter in a addition to input
 // ****************************************************
 
-const removeMarkers = (input, markers) => {
+const removeMarkers = (input, markersForDeletion) => {
   // splits the input arr in to each letter as an item
   const inputArr = input.split("")
-  // designates the markers as what needs to be taken out
-  // (Note: all elements in markers will be deleted)
-  const forDeletion = markers
   // fiters out the input array, by only returning whats not for deleting,
   // and joins it back into a string
-  return inputArr.filter(item => !forDeletion.includes(item)).join("")
+  return inputArr.filter(item => !markersForDeletion.includes(item)).join("")n
 }
 
 console.log(removeMarkers("this is a show", "isahows"))
