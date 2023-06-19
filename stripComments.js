ed06b4123354fb5b80744b30bd27d93ff6682266
Don Stevenson <dsteven1@gmail.com>
2023-06-19 00:50:37 -0400

    Refactor code

--- stripComments.js
+++ stripComments.js
@@ -2,13 +2,14 @@
 // that you enter in a addition to input
 // ****************************************************
 
-const removeMarkers = (input, markersForDeletion) => {
+const removeMarkers = (input, markersForDeletion) =>
   // splits the input arr in to each letter as an item
-  const inputArr = input.split("")
   // fiters out the input array, by only returning whats not for deleting,
   // and joins it back into a string
-  return inputArr.filter(item => !markersForDeletion.includes(item)).join("")
-}
+  input
+    .split("")
+    .filter(item => !markersForDeletion.includes(item))
+    .join("")
 
 console.log(removeMarkers("this is a show", "isahows"))
 // returns t
