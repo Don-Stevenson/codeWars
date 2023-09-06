db67c1819bfec95a9f3cea71a6fe956aa0398731
Don Stevenson <dstevenson@tribalscale.com>
2023-09-06 17:02:52 -0400

    Clean up file and rename functions

--- returnTheLastOf.js
+++ returnTheLastOf.js
@@ -2,31 +2,34 @@
 
 const array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 
-// const lastNItems = (arr, numOfItems) => {
+// const returnsTheLastNItems = (arr, numOfItems) => {
 //   let itemsArr = []
-
 //   for (let i = 1; i <= numOfItems; i++) {
 //     itemsArr.push(arr[arr.length - i])
 //   }
 //   return itemsArr
 // }
+// console.log(returnsTheLastNItems(array, 2))
+// with c style loop, Results:
+// [ 'j', 'i' ]
 
-// const lastNItems = (arr, numOfItems) => {
+// const returnsTheLastNItems = (arr, numOfItems) => {
 //   let finalArr = []
 //   arr.map((e, index) => {
-//     const lastNItems = arr.length - numOfItems
-//     if (index >= lastNItems) {
+//     if (index >= arr.length - numOfItems) {
 //       finalArr.push(e)
 //     }
 //   })
 //   return finalArr
 // }
-// console.log(lastNItems(array, 6))
+// console.log(returnsTheLastNItems(array, 6))
+// with map, but finalArr is required to remove undefined entries to the array
+// [ 'e', 'f', 'g', 'h', 'i', 'j' ]
 
-const returnsTheLastNItems = (arr, numOfItems) =>
-  arr.filter((e, index) => index >= arr.length - numOfItems)
+// const returnsTheLastNItems = (arr, numOfItems) =>
+//   arr.filter((e, index) => index >= arr.length - numOfItems)
 
-console.log(returnsTheLastNItems(array, 2))
+// console.log(returnsTheLastNItems(array, 2))
 
 // result:
 // [ 'e', 'f', 'g', 'h', 'i', 'j' ]
