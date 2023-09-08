e183346803839fbf242eaa3f3776a50972e643b3
Don Stevenson <dstevenson@tribalscale.com>
2023-09-07 23:15:35 -0400

    Add filter for numbers greater than 6

--- noFunction.js
+++ noFunction.js
@@ -4,5 +4,8 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // array.map(e => console.log(e * 10))
 
 const initialValue = 0
+
+console.log(array.filter(e => e >= 7))
+// expect [7, 8, 9]
 console.log(array.reduce((prev, curr) => prev + curr, initialValue))
 // expect => 45
