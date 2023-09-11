c85dd79fdd19318c967b3a996427b15f26e45318
Don Stevenson <dsteven1@gmail.com>
2023-09-11 15:48:06 -0400

    Add find less than 5 function

--- Aug30.js
+++ Aug30.js
@@ -7,8 +7,9 @@ const oddFilter = array => array.filter(e => e % 2 !== 0)
 const findFive = array => array.filter(e => e === 5)
 const findTen = array => array.filter(e => e === 10)
 const findGreaterThanFive = array => array.filter(e => e > 5)
+const findLessThanFive = array => array.filter(e => e < 5)
 console.log("evens", evenFilter(numberArray)) // => [ 2, 4, 6, 8, 10 ]
 console.log("odds", oddFilter(numberArray)) // => [ 1, 3, 5, 7, 9 ]
 console.log("find 5", findFive(numberArray)) // => [ 5 ]
 console.log("find 10", findTen(numberArray)) // => [ 10 ]
-console.log("find greater than 5", findGreaterThanFive(numberArray)) // => [ 6, 7, 8, 9, 10 ]
+console.log("find less than 5", findLessThanFive(numberArray)) // => [ 1, 2, 3, 4 ]
