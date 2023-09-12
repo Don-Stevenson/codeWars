4fb04728ce2c229f4d68fe9e06ef61a42b6c5390
Don Stevenson <dstevenson@tribalscale.com>
2023-09-12 14:00:21 -0400

    Add function that returns non strings

--- Aug30.js
+++ Aug30.js
@@ -8,11 +8,13 @@ const findFive = array => array.filter(e => e === 5)
 const findTen = array => array.filter(e => e === 10)
 const findGreaterThanFive = array => array.filter(e => e > 5)
 const findLessThanFive = array => array.filter(e => e < 5)
-const elementIsAStringFive = array =>
+const elementIsAString = array =>
   array.filter(e => typeof e === typeof "string")
+const elementIsANum = array => array.filter(e => typeof e !== typeof "string")
 console.log("evens", evenFilter(numberArray)) // => [ 2, 4, 6, 8, 10 ]
 console.log("odds", oddFilter(numberArray)) // => [ 1, 3, 5, 7, 9 ]
 console.log("find 5", findFive(numberArray)) // => [ 5 ]
 console.log("find 10", findTen(numberArray)) // => [ 10 ]
 console.log("find less than 5", findLessThanFive(numberArray)) // => [ 1, 2, 3, 4 ]
-console.log("find the string element", elementIsAStringFive(numberArray)) // => ["a"]
+console.log("find the string element(s)", elementIsAString(numberArray)) // => ["a"]
+console.log("find the number element(s)", elementIsANum(numberArray)) // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
