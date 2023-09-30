3dd792320091983ed3a9a90ca744fef07879e529
Don Stevenson <dstevenson@tribalscale.com>
2023-09-30 13:20:51 -0400

    With array . sort

--- quickSort.js
+++ quickSort.js
@@ -2,6 +2,8 @@
 // to [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const array = [9, 2, 3, 5, 7, 8, 1, 4, 6]
 
+const sortedArrary = array.sort().toString()
+console.log({ sortedArrary })
 const quickSort = array => {
   if (array.length === 0) return array
   const pivot = array[0]
@@ -12,6 +14,6 @@ const quickSort = array => {
     if (e < pivot) smallerThanPivot.push(e)
     if (e > pivot) largerThanPivot.push(e)
   })
-  return `${quickSort(smallerThanPivot)}${pivot},${quickSort(largerThanPivot)}`
+  return `${quickSort(smallerThanPivot)}${pivot} ${quickSort(largerThanPivot)}`
 }
 console.log(quickSort(array))
