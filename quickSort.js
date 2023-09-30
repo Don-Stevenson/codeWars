04fcbc43361008b951a561fecb72ec8b716eb5b5
Don Stevenson <dstevenson@tribalscale.com>
2023-09-30 09:31:04 -0400

    Add quick sort problem setup

---
+++ quickSort.js
@@ -0,0 +1,18 @@
+// sort array from [ 9, 2, 3, 5, 7, 8, 1, 4, 6]
+// to [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const array = [9, 2, 3, 5, 7, 8, 1, 4, 6]
+
+const quickSort = array => {
+  if (array.length === 0) return array
+
+  let pivot = array[0]
+  let smallerThanPivot = []
+  let largerThanPivot = []
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] <= pivot) smallerThanPivot.push(array[i])
+    if (array[i] >= pivot) largerThanPivot.push(array[i])
+  }
+  return array
+}
+console.log(quickSort(array))
