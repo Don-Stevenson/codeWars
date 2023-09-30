1a2160440583ec3896ea37bab809ed4461b04d43
Don Stevenson <dstevenson@tribalscale.com>
2023-09-30 13:07:40 -0400

    Add working solution

--- quickSort.js
+++ quickSort.js
@@ -4,15 +4,14 @@ const array = [9, 2, 3, 5, 7, 8, 1, 4, 6]
 
 const quickSort = array => {
   if (array.length === 0) return array
-
-  let pivot = array[0]
-  let smallerThanPivot = []
-  let largerThanPivot = []
+  const pivot = array[0]
+  const smallerThanPivot = []
+  const largerThanPivot = []
 
   for (let i = 0; i < array.length; i++) {
-    if (array[i] <= pivot) smallerThanPivot.push(array[i])
-    if (array[i] >= pivot) largerThanPivot.push(array[i])
+    if (array[i] < pivot) smallerThanPivot.push(array[i])
+    if (array[i] > pivot) largerThanPivot.push(array[i])
   }
-  return array
+  return `${quickSort(smallerThanPivot)}${pivot},${quickSort(largerThanPivot)}`
 }
 console.log(quickSort(array))
