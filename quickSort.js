bf9ab84736cbb0cdbcb44a2f7893781083b59f15
Don Stevenson <dstevenson@tribalscale.com>
2023-09-30 21:46:35 -0400

    Comment out sortedArray

--- quickSort.js
+++ quickSort.js
@@ -2,8 +2,8 @@
 // to [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const array = [9, 2, 3, 5, 7, 8, 1, 4, 6]
 
-const sortedArrary = array.sort().toString()
-console.log({ sortedArrary })
+// const sortedArrary = array.sort().toString()
+// console.log({ sortedArrary })
 const quickSort = array => {
   if (array.length === 0) return array
   const pivot = array[0]
