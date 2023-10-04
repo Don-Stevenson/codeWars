538cd9b87ccd6335036c05adcb607b869ce1ea34
Don Stevenson <dsteven1@gmail.com>
2023-10-04 17:11:44 -0400

    Add expected results

--- parsingJSON.js
+++ parsingJSON.js
@@ -6,7 +6,7 @@ const catFetch = async () => {
       "https://api.thecatapi.com/v1/breeds/search?q=maine%20coon"
     )
     const catJSON = await catResponse.json()
-    const catJSONobj = await catJSON[0]
+    const catJSONobj = catJSON[0]
     console.log(catJSONobj.description)
   } catch (error) {
     console.error(error)
@@ -15,3 +15,8 @@ const catFetch = async () => {
 
 // logs catFetch to the console
 catFetch()
+// expect
+// They are known for their size and luxurious long coat Maine Coons are considered a gentle giant.
+// The good-natured and affable Maine Coon adapts well to many lifestyles and personalities.
+// She likes being with people and has the habit of following them around, but isn’t needy.
+// Most Maine Coons love water and they can be quite good swimmers.
