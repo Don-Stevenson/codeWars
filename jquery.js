57f49bdd48f8f9db699350e8cdde6a2072eab928
Don Stevenson <dstevenson@tribalscale.com>
2023-10-16 20:03:37 -0400

    Remove resize call

--- jquery.js
+++ jquery.js
@@ -1,9 +1,5 @@
 const mobileViewCheck = () => $(window).width() <= 450
 
-$(window).on("resize", function () {
-  return mobileViewCheck()
-})
-
 $(".ftitle").click(function () {
   if (mobileViewCheck()) {
     $(this).next().find(".hs-menu-wrapper ul").slideToggle(250)
