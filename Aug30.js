aadb64b6a23b03739918a02e7e55ecb6c915e04a
Don Stevenson <dstevenson@tribalscale.com>
2023-11-05 15:15:50 -0500

    Update comments:

--- Aug30.js
+++ Aug30.js
@@ -9,8 +9,8 @@ const findTen = array => array.filter(e => e === 10)
 const findGreaterThanFive = array => array.filter(e => e > 5)
 const findLessThanFive = array => array.filter(e => e < 5)
 const elementIsAString = array =>
-  array.filter(e => typeof e === typeof "string")
-const elementIsANum = array => array.filter(e => typeof e !== typeof "string")
+  array.filter(e => typeof e === typeof "string") // => ["a"]
+const elementIsANum = array => array.filter(e => typeof e !== typeof "string") // => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
 console.log("evens", evenFilter(numberArray)) // => [ 2, 4, 6, 8, 10 ]
 console.log("odds", oddFilter(numberArray)) // => [ 1, 3, 5, 7, 9 ]
 console.log("find 5", findFive(numberArray)) // => [ 5 ]
