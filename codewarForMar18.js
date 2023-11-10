38615de5665c0588ad14d1a26f933ff779d9d717
Don Stevenson <dstevenson@tribalscale.com>
2023-11-10 01:04:28 -0500

    Update expected results console log

--- codewarForMar18.js
+++ codewarForMar18.js
@@ -31,3 +31,6 @@ const multiplyByX = (arr, num) => arr.map(e => e * num)
 
 console.log(multiplyByX([1, 2, 3], 10))
 // expect : [ 10, 20, 30 ]
+
+console.log(multiplyByX([1, 2, 3], 100))
+// expect : [ 100, 200, 300 ]
