80a61337918206bc84c0d7d95b4cdc3a568046ad
Don Stevenson <dstevenson@tribalscale.com>
2023-11-24 15:43:52 -0500

    Add export of addTwo function

--- addOne.js
+++ addOne.js
@@ -4,4 +4,4 @@ const addTwo = groupOfNumbers => groupOfNumbers.map(element => element + 2)
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-module.exports = { addOne, array }
+module.exports = { addOne, addTwo, array }
