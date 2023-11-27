e54edb0eb40cd2db0870b3aaec62a6b5687e6bdd
Don Stevenson <dstevenson@tribalscale.com>
2023-11-26 19:55:06 -0500

    Update error returned

--- factorial.js
+++ factorial.js
@@ -3,7 +3,7 @@
 
 const factorial = n => {
   if (!(n >= 0 && n <= 12)) {
-    throw new RangeError("the value must be between 0 and 12")
+    return "Error: the value must be between 0 and 12"
   } else if (n === 0) {
     return 1
   } else return n * factorial(n - 1)
