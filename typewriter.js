cede004ad324459c42979240489ae7474e9e2ff5
Don Stevenson <dstevenson@tribalscale.com>
2023-12-10 13:37:07 -0500

    Update amount of time for each character

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,7 @@
 const sentence = "hello there! This is a typewriter effect! \n"
 
 let count = 0
-const randomTime = () => Math.floor(Math.random() * 250)
+const randomTime = () => Math.floor(100 + Math.random() * 250)
 
 const typewriter = sentence => {
   for (const char of sentence) {
