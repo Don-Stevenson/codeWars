242097683e1c62c2f0dc0472c13adb3b1f1949d0
Don Stevenson <dstevenson@tribalscale.com>
2023-12-10 13:26:17 -0500

    Wrapp in a function

--- typewriter.js
+++ typewriter.js
@@ -2,8 +2,13 @@ const sentence = "hello there! This is a typewriter effect! \n"
 let count = 0
 
 const randomTime = () => Math.floor(Math.random() * 400)
-for (const char of sentence) {
-  setTimeout(() => {
-    process.stdout.write(char)
-  }, (count += randomTime()))
+
+const typewriter = sentence => {
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char)
+    }, (count += randomTime()))
+  }
 }
+
+typewriter(sentence)
