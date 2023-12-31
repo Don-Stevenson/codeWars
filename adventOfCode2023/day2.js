45b10f6967ea48faf691400cb177779d1d3d372a
Don Stevenson <dsteven1@gmail.com>
2023-12-30 22:01:19 -0500

    Update function name

--- adventOfCode2023/day2.js
+++ adventOfCode2023/day2.js
@@ -12,16 +12,13 @@
 // As you walk, the Elf shows you a small bag and some cubes which are either red, green, or blue.
 // Each time you play this game, he will hide a secret number of cubes of each color in the bag,
 // and your goal is to figure out information about the number of cubes.
-
 // To get information, once a bag has been loaded with cubes, the Elf will reach into the bag,
 // grab a handful of random cubes, show them to you, and then put them back in the bag.
 // He'll do this a few times per game.
-
 // You play several games and record the information from each game (your puzzle input).
 // Each game is listed with its ID number (like the 11 in Game 11: ...)
 // followed by a semicolon-separated list of subsets of cubes that were revealed from the bag
 // (like 3 red, 5 green, 4 blue).
-
 // For example, the record of a few games might look like this:
 
 // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -32,10 +29,8 @@
 
 // In game 1, three sets of cubes are revealed from the bag (and then put back again). The first set is 3 blue cubes
 // and 4 red cubes; the second set is 1 red cube, 2 green cubes, and 6 blue cubes; the third set is only 2 green cubes.
-
 // The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes,
 // 13 green cubes, and 14 blue cubes?
-
 // In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration.
 // However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once;
 // similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once.
@@ -63,7 +58,7 @@ const possibleGamesIndex = [1, 2, 3, 4, 5]
 
 const createGamesArray = games => games.split("; ").join(" ").split(" ")
 
-const areGamesPossibleChecker = (games, bagContents) => {
+const sumOfIdsOfPossibleGames = (games, bagContents) => {
   let impossibleGames = []
 
   games.map((game, index) => {
@@ -94,4 +89,4 @@ const areGamesPossibleChecker = (games, bagContents) => {
     .reduce((a, b) => a + b, 0)
 }
 
-console.log(areGamesPossibleChecker(gamesArray, bagContents)) // expected output: 8
+console.log(sumOfIdsOfPossibleGames(gamesArray, bagContents)) // expected output: 8
