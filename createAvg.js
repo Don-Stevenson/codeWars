a0e2f64d55244834358afb095d4e1654bae32583
Don Stevenson <dstevenson@tribalscale.com>
2024-01-25 00:53:23 -0500

    Add returns avg with map

--- createAvg.js
+++ createAvg.js
@@ -1,3 +1,5 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 const retunsAvg = arr => {
   let sum = 0
   for (let i = 0; i < arr.length; i++) {
@@ -9,9 +11,14 @@ const retunsAvg = arr => {
 const returnsAvgWithReduce = arr =>
   arr.reduce((acc, cur) => acc + cur, 0) / arr.length
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const returnsAvgWithMap = arr => {
+  let sum = 0
+  arr.map(num => (sum += num))
+  return sum / arr.length
+}
 
-console.log(retunsAvg(arr))
-// expect 5.5
+console.log(retunsAvg(arr)) // expect 5.5
 
 console.log(returnsAvgWithReduce(arr)) // expect 5.5
+
+console.log(returnsAvgWithMap(arr)) // expect 5.5
