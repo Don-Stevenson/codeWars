eb29de797669d9bc7afe0f6de8e6b1ddb8287d19
Don Stevenson <dstevenson@tribalscale.com>
2024-02-04 00:36:06 -0500

    Add solution with filter

--- quickSort.js
+++ quickSort.js
@@ -45,26 +45,34 @@
 
 // Chat GPT solution here
 
-function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr // Base case: already sorted
-  }
+// function quickSort(arr) {
+//   if (arr.length <= 1) {
+//     return arr // Base case: already sorted
+//   }
 
+//   const pivot = arr[0]
+//   const left = []
+//   const right = []
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < pivot) {
+//       left.push(arr[i])
+//     } else {
+//       right.push(arr[i])
+//     }
+//   }
+
+//   return [...quickSort(left), pivot, ...quickSort(right)]
+// }
+
+const quickSort = arr => {
+  if (arr.length <= 1) return arr // Base case
   const pivot = arr[0]
-  const left = []
-  const right = []
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i])
-    } else {
-      right.push(arr[i])
-    }
-  }
-
+  const left = arr.filter(e => e < pivot)
+  const right = arr.filter(e => e > pivot)
   return [...quickSort(left), pivot, ...quickSort(right)]
 }
-
 const unsortedArray = [9, 2, 1, 3, 8, 4, 7, 5, 6]
 const sortedArray = quickSort(unsortedArray)
 
