752a3dcff2f81a2d0810ef6ed2ec2c1a3931612b
Don Stevenson <dstevenson@tribalscale.com>
2024-03-13 22:04:37 -0400

    Add saysGoodbye, saysGoodByeToEveryone

--- saysHello.js
+++ saysHello.js
@@ -10,3 +10,13 @@ console.log(saysGreeting("hi"))
 
 console.log(saysGreeting("what's up?"))
 // expect "what's up?"
+
+const saysGoodbye = (greeting, name) => `${greeting}, ${name}`
+console.log(saysGoodbye("goodbye", "Bob"))
+// expect "goodbye, Bob"
+
+const saysGoodbyeToEveryone = (greeting, ...names) =>
+  names.map(name => `${greeting}, ${name}`)
+
+console.log(saysGoodbyeToEveryone("goodbye", "Bob", "Sue", "Jim"))
+// expect ["goodbye, Bob", "goodbye, Sue", "goodbye, Jim"]
