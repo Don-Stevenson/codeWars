67c9be169f988dab2170ed38d26b8b2b66c0955c
Don Stevenson <dstevenson@tribalscale.com>
2024-06-01 21:20:30 -0400

    Refactor quick sort code

--- quickSort.py
+++ quickSort.py
@@ -7,19 +7,33 @@
 #     return quick_sort(left) + [pivot] + quick_sort(right)
 
 
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     pivot = arr[0]
+#     left_arr = []
+#     right_arr = []
+#     for i in range(1, len(arr)):
+#         if arr[i] < pivot:
+#             left_arr.append(arr[i])
+#         else:
+#             right_arr.append(arr[i])
+#     return quick_sort(left_arr) + [pivot] + quick_sort(right_arr)
+
+
+# unsorted_array = [9, 2, 1, 3, 8, 4, 7, 5, 6]
+# sorted_array = quick_sort(unsorted_array)
+
+# print("Unsorted array:", unsorted_array)
+# print("Sorted array:", sorted_array)
+
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     pivot = arr[0]
-    left_arr = []
-    right_arr = []
-    for i in range(1, len(arr)):
-        if arr[i] < pivot:
-            left_arr.append(arr[i])
-        else:
-            right_arr.append(arr[i])
+    left_arr = [x for x in arr[1:] if x < pivot]
+    right_arr = [x for x in arr[1:] if x >= pivot]
     return quick_sort(left_arr) + [pivot] + quick_sort(right_arr)
-
 
 unsorted_array = [9, 2, 1, 3, 8, 4, 7, 5, 6]
 sorted_array = quick_sort(unsorted_array)
