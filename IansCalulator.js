988a6af1a73e60691df30336612b326ae9041286
Don Stevenson <dstevenson@tribalscale.com>
2024-06-28 15:31:06 -0500

    Remove function call

--- IansCalulator.js
+++ IansCalulator.js
@@ -2,20 +2,12 @@ const num1 = process.argv[2]
 const operator = process.argv[3]
 const num2 = process.argv[4]
 
-const addition = (firstNumber, operator, secondnumber) => {
-  if (operator === "+") {
-    return parseInt(firstNumber) + parseInt(secondnumber)
-  }
-  if (operator === "-") {
-    return parseInt(firstNumber) - parseInt(secondnumber)
-  }
-  if (operator === "*") {
-    return parseInt(firstNumber) * parseInt(secondnumber)
-  }
-  if (operator === "/") {
-    return parseInt(firstNumber) / parseInt(secondnumber)
-  }
-  return "\x1b[41m that dont work fool"
-}
-
-console.log(addition(num1, operator, num2))
+if (operator === "+") {
+  console.log(parseInt(num1) + parseInt(num2))
+} else if (operator === "-") {
+  console.log(parseInt(num1) - parseInt(num2))
+} else if (operator === "*") {
+  console.log(parseInt(num1) * parseInt(num2))
+} else if (operator === "/") {
+  console.log(parseInt(num1) / parseInt(num2))
+} else console.log("\x1b[41m that don't work fool")
