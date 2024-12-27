9ad1dac994e2703098694bfe7dca273abd75ded7
Don Stevenson <dstevenson@tribalscale.com>
2024-12-27 10:40:35 -0500

    Add another simple reduce function that adds the values in an array

--- learnReduce.js
+++ learnReduce.js
@@ -3,25 +3,23 @@
 
 // const array = [1, 2, 3, 4, 5, 6, 7]
 
-// const { add } = require("date-fns")
-
 // const obj = {
 //   a: 1,
 //   b: 2,
 //   c: 3
 // }
 
-const reducerFunc = (accumalator, currentVal, currentIndex, array) => {
-  console.log(`
-  accumalator is: ${accumalator}
-  current Value: ${currentVal}
-  current Index: ${currentIndex}
-  array is: ${array}`)
-  return array.push(accumalator)
-}
+// const reducerFunc = (accumulator, currentVal, currentIndex, array) => {
+//   console.log(`
+//   accumulator is: ${accumulator}
+//   current Value: ${currentVal}
+//   current Index: ${currentIndex}
+//   array is: ${array}`)
+//   return array.push(accumulator)
+// }
 
 // takes in a an array and the runs a callback function on the array
-// let reducedArray = array.reduce(reducerFunc)
+// const reducedArray = array.reduce(reducerFunc)
 
 // in this case should expect 28 to be the answer
 // console.log(reducedArray)
@@ -31,11 +29,11 @@ const reducerFunc = (accumalator, currentVal, currentIndex, array) => {
 // with a string , result is abcef
 // const letterArr = ["a", "b","c","d","e","f"]
 
-const addObj = {
-  a: 2,
-  b: 4,
-  c: 6,
-}
+// const addObj = {
+//   a: 2,
+//   b: 4,
+//   c: 6,
+// }
 
 // const total =
 // Getts the values out of add object
@@ -44,7 +42,7 @@ const addObj = {
 // console.log(total) // 6
 
 // object values returns an array of values, that you can run .reduce on and use the function called reduce in
-Object.values(addObj).reduce(reducerFunc)
+// Object.values(addObj).reduce(reducerFunc)
 
 // expected results:
 // accumalator is: 2
@@ -57,16 +55,20 @@ Object.values(addObj).reduce(reducerFunc)
 // current Index: 2
 // array is: 2,4,6,2
 
-// reduce right
-const numbers = [1, 2, 3]
+// // reduce right
+// const numbers = [1, 2, 3]
 
-const sum = numbers.reduceRight((previousValue, currentValue) => {
-  console.log({ previousValue, currentValue })
-  return previousValue + currentValue
-})
+// const sum = numbers.reduceRight((previousValue, currentValue) => {
+//   console.log({ previousValue, currentValue })
+//   return previousValue + currentValue
+// })
 
-console.log(`Result:${sum}`)
+// console.log(`Result:${sum}`)
 // expect
 // { previousValue: 3, currentValue: 2 }
 // { previousValue: 5, currentValue: 1 }
 // Result:6
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+console.log(arr.reduce((accumalator, currVal) => accumalator + currVal))
