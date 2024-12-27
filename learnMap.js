722129ab1133ff6789d1541b406ffb49a6c087bf
Don Stevenson <dstevenson@tribalscale.com>
2024-12-27 11:03:05 -0500

    Add conditional for num check

--- learnMap.js
+++ learnMap.js
@@ -17,7 +17,11 @@
 const array = ["a", "b", 3, 4, 5, 6, 7]
 const mappedArray = array.map(x => x * 3)
 
-const mappedWithBraces = array.map(x => x * 3)
+const mappedWithBraces = array.map(x => {
+  if (x === typeof 2) {
+    return x * 3
+  } else return x
+})
 
 console.table(`mapped array is: ${mappedArray} 
 mappedWithBraces array is: ${mappedWithBraces} 
