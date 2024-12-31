5e6dbcb1f7a6e491f95009ac2c68787fd771058c
Don Stevenson <dstevenson@tribalscale.com>
2024-12-31 14:45:36 -0600

    Working code

---
+++ magicEightBall.js
@@ -0,0 +1,34 @@
+const question = process.argv[2]
+
+const responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+]
+const randomNumberFromZeroToTwenty = Math.floor(Math.random() * 20)
+
+console.log(`
+    
+    Welcome to Renice's Magic 8 ball
+    You asked: ${question}
+
+    The mythical 8 ball has determined:
+    ${responses[randomNumberFromZeroToTwenty]}
+    `)
