eeb7d491056de0b8a3b88b90c8e82f0bdb95ebcb
Don Stevenson <dstevenson@tribalscale.com>
2024-12-31 09:54:48 -0600

    Update expected results

--- learnMap.js
+++ learnMap.js
@@ -28,5 +28,5 @@ mappedWithBraces array is: ${mappedWithBraces}
 array is still: ${array}`)
 
 // mapped array is: NaN,NaN,9,12,15,18,21
-// mappedWithBraces array is: NaN,NaN,9,12,15,18,21
+// mappedWithBraces array is: a,b,9,12,15,18,21
 // array is still: a,b,3,4,5,6,7
