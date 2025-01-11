c0124cecda50b3d8bf555baa1db9240bdd41fa3a
Don Stevenson <dstevenson@tribalscale.com>
2025-01-11 12:56:40 -0600

    Round to 2 decimals

--- fudge.js
+++ fudge.js
@@ -1,4 +1,4 @@
-const fudge = score => Math.random() * 3 + score - 1.5
+const fudge = score => Number((Math.random() * 3 + score - 1.5).toFixed(2))
 
 console.log("initial score was 20", fudge(20))
 console.log("initial score was 20", fudge(20))
