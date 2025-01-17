74c79282c1baada65dbfb3106855f0d4545ce621
Don Stevenson <dstevenson@tribalscale.com>
2025-01-17 00:01:15 -0500

    function returning new array multiplying each element by five

--- mapWithTypeScript.ts
+++ mapWithTypeScript.ts
@@ -1,2 +1,5 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const newArrayTimesfive = arr[number] ()=> {arr.map(num => num * 5)}
+const newArrayTimesfive = (arr: number[]) => {
+  return arr.map(num => num * 5)
+}
+console.log(newArrayTimesfive(arr)) // expect [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
