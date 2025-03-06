bf1db9e44e723a95f044ae33b80c12f0019e3435
Don Stevenson <dstevenson@tribalscale.com>
2025-03-05 22:05:30 -0500

    Refactor into class based approach

--- magicEightBall.js
+++ magicEightBall.js
@@ -21,21 +21,40 @@ const responses = [
   "Very doubtful",
 ]
 
-function getRandomResponse(responses) {
-  const randomIndex = Math.floor(Math.random() * responses.length)
-  return responses[randomIndex]
-}
+class MagicEightBall {
+  constructor(responses) {
+    this.responses = responses
+  }
 
-function formatResponse(question, answer) {
-  return `
-    Welcome to Renice's Magic 8 ball
+  getRandomResponse() {
+    const randomIndex = Math.floor(Math.random() * this.responses.length)
+    return this.responses[randomIndex]
+  }
+
+  formatResponse(question, answer) {
+    return `
+    Welcome to Renice's Magic 8 Ball
     You asked: ${question}
 
     The mythical 8 ball has determined:
     ${answer}
     `
+  }
+
+  shake(question) {
+    if (!question || typeof question !== "string") {
+      throw new Error("Please ask a valid question!")
+    }
+    const answer = this.getRandomResponse()
+    return this.formatResponse(question, answer)
+  }
 }
 
-const question = process.argv[2]
-const answer = getRandomResponse(responses)
-console.log(formatResponse(question, answer))
+try {
+  const question = process.argv[2]
+  const magicBall = new MagicEightBall(responses)
+  console.log(magicBall.shake(question))
+} catch (error) {
+  console.error("Error:", error.message)
+  process.exit(1)
+}
