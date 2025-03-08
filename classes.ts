7ec1cef6c054fca88260bf8c0936855d4a1d8afa
Don Stevenson <dstevenson@tribalscale.com>
2025-03-07 22:00:20 -0500

    Fix toppings and baking times

--- classes.ts
+++ classes.ts
@@ -3,12 +3,15 @@ class Pizza {
   private toppings: string[]
 
   constructor(type: string) {
+    this.topping = type
     this.toppings = [this.topping]
   }
 
-  public bakingTime(time: number) {
-    return this.toppings
+  public bakingTime(time: number): string {
+    return `Your ${this.topping} pizza will take ${time} minutes to bake`
   }
 }
 
-console.log(new Pizza("cheese"))
+// Create a pizza and check its baking time
+const myPizza = new Pizza("cheese")
+console.log(myPizza.bakingTime(30))
