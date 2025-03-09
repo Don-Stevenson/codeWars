4dc132c9a889cfffca0f5e1be1cd852dbed7805e
Don Stevenson <dstevenson@tribalscale.com>
2025-03-08 22:14:05 -0500

    Add skateboard class constructor

--- classes.ts
+++ classes.ts
@@ -15,3 +15,20 @@ class Pizza {
 // Create a pizza and check its baking time
 const myPizza = new Pizza("cheese")
 console.log(myPizza.bakingTime(30))
+
+class skateBoard {
+  private fitting1: string
+  private fitting2: string
+
+  constructor(fitting1: string, fitting2: string) {
+    this.fitting1 = fitting1
+    this.fitting2 = fitting2
+  }
+
+  public deckColour(colour: string): string {
+    return `Your skate board will have the following gear: ${this.fitting1} and ${this.fitting2}, and it will be ${colour} in colour`
+  }
+}
+
+const mySkateBoard = new skateBoard("swiss bearings", "bananaboard")
+console.log(mySkateBoard.deckColour("blue"))
