4393085c619fdef21d0f595e668b8658ef794bd3
Don Stevenson <dstevenson@tribalscale.com>
2025-03-19 21:16:47 -0400

    Refactor for readibility

--- classes.ts
+++ classes.ts
@@ -1,33 +1,27 @@
 class Pizza {
-  private toppings: string[]
+  constructor(private readonly toppings: string[]) {}
 
-  constructor(toppings: string[]) {
-    this.toppings = toppings
-  }
-
-  public bakingTime(time: number): string {
+  public getBakingTimeMessage(minutes: number): string {
     const toppingsList = this.toppings.join(", ")
-    return `Your ${toppingsList} pizza will take ${time} minutes to bake`
+    return `Your ${toppingsList} pizza will take ${minutes} minutes to bake`
   }
 }
 
-// Create a pizza and check its baking time
+// Example usage
 const myPizza = new Pizza(["cheese", "mushrooms", "pepperoni"])
-console.log(myPizza.bakingTime(30))
+console.log(myPizza.getBakingTimeMessage(30))
 
 class Skateboard {
-  private readonly fitting1: string
-  private readonly fitting2: string
-
-  constructor(fitting1: string, fitting2: string) {
-    this.fitting1 = fitting1
-    this.fitting2 = fitting2
-  }
+  constructor(
+    private readonly fitting1: string,
+    private readonly fitting2: string
+  ) {}
 
   public getDescription(color: string): string {
     return `Your skateboard will have the following gear: ${this.fitting1} and ${this.fitting2}, and it will be ${color} in color`
   }
 }
 
+// Example usage
 const mySkateBoard = new Skateboard("swiss bearings", "bananaboard")
 console.log(mySkateBoard.getDescription("blue"))
