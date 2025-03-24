eb6309cf5874f4e793c279f8da5e1e92aa019778
Don Stevenson <dstevenson@tribalscale.com>
2025-03-24 13:26:00 -0400

    Add working solution

---
+++ etl.ts
@@ -0,0 +1,13 @@
+export function transform(old: { [key: string]: string[] }): {
+  [key: string]: number
+} {
+  const result: { [key: string]: number } = {}
+
+  for (const [score, letters] of Object.entries(old)) {
+    for (const letter of letters) {
+      result[letter.toLowerCase()] = Number(score)
+    }
+  }
+
+  return result
+}
