6f22e15cca671dd2d46cd46708d2c67782e39cd0
Don Stevenson <dstevenson@tribalscale.com>
2025-04-13 22:48:42 -0400

    Refactor and break down into smaller functions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,28 @@
-const getFizzBuzzValue = num => {
-  if (num % 3 === 0 && num % 5 === 0) return "FizzBuzz"
-  if (num % 3 === 0) return "Fizz"
-  if (num % 5 === 0) return "Buzz"
-  return num.toString()
+const FIZZ_NUMBER = 3
+const BUZZ_NUMBER = 5
+const DEFAULT_START = 1
+const DEFAULT_END = 100
+
+const isDivisibleBy = (number, divisor) => number % divisor === 0
+
+const getFizzBuzzValue = number => {
+  const isFizz = isDivisibleBy(number, FIZZ_NUMBER)
+  const isBuzz = isDivisibleBy(number, BUZZ_NUMBER)
+
+  if (isFizz && isBuzz) return "FizzBuzz"
+  if (isFizz) return "Fizz"
+  if (isBuzz) return "Buzz"
+
+  return number.toString()
 }
 
-const fizzBuzz = (start = 1, end = 100) => {
+const fizzBuzz = (start = DEFAULT_START, end = DEFAULT_END) => {
   const results = []
+
   for (let i = start; i <= end; i++) {
     results.push(getFizzBuzzValue(i))
   }
+
   return results
 }
 
