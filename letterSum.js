f6f11a7b401f1e20b27e1e3c8a901d471db73296
Don Stevenson <dstevenson@tribalscale.com>
2025-06-04 23:12:10 -0500

    Add hello score

--- letterSum.js
+++ letterSum.js
@@ -90,3 +90,4 @@ console.log(letterSum("&*")) // => NaN
 console.log(letterSum("Hi&*")) // => NaN
 console.log(letterSum("ddd---")) // => NaN
 console.log(letterSum("test")) // => 64
+console.log(letterSum("hello")) // => 52
