6d7a3d08c8334cedb052304a9f3ff45476f380b3
Don Stevenson <dstevenson@tribalscale.com>
2025-06-09 23:17:04 -0400

    Add made test

--- letterSum.js
+++ letterSum.js
@@ -94,3 +94,4 @@ console.log(letterSum("hello")) // => 52
 console.log(letterSum("moto")) // => 63
 console.log(letterSum("antidisestablishmentarianism")) // => 307
 console.log(letterSum("stuff")) // => 72
+console.log(letterSum("made")) // => 23
