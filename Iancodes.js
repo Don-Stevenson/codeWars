c03e3787c8697dd11d39ef880f98829938ea0939
Don Stevenson <dstevenson@tribalscale.com>
2025-10-11 12:36:17 -0400

    remove unnecessary brakets

--- Iancodes.js
+++ Iancodes.js
@@ -1,13 +1,9 @@
 const input = process.argv[2]
 
 const slap = input => {
-  if (input === "racoon says hi") {
-    console.log("racoon eats trash")
-  }
-  if (input === "if there is a trash can, there is a racoon") {
+  if (input === "racoon says hi") console.log("racoon eats trash")
+  if (input === "if there is a trash can, there is a racoon")
     console.log("racoon is a trash")
-  } else {
-    console.log("car hits trash can")
-  }
+  else console.log("car hits trash can")
 }
 slap(input)
