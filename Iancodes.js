4e7721b8275d54582d41bcdc33ca3e347a4d83bd
Don Stevenson <dstevenson@tribalscale.com>
2025-10-14 21:28:05 -0400

    Update log strings

--- Iancodes.js
+++ Iancodes.js
@@ -3,7 +3,7 @@ const input = process.argv[2]
 const slap = input => {
   if (input === "racoon says hi") console.log("racoon eats trash")
   if (input === "if there is a trash can, there is a racoon")
-    console.log("racoon is a trash")
+    console.log("racoon is in the trash can")
   else console.log("car hits trash can")
 }
 slap(input)
