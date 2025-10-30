4c31c444f1be7409f5c444accf13abfcef58baf2
Don Stevenson <dstevenson@tribalscale.com>
2025-10-29 23:50:14 -0400

    Update function name

--- Iancodes.js
+++ Iancodes.js
@@ -1,9 +1,10 @@
 const input = process.argv[2]
 
-const slap = input => {
+const racoonDoesWhat = input => {
   if (input === "racoon says hi") console.log("racoon eats trash")
   if (input === "if there is a trash can, there is a racoon")
     console.log("racoon is in the trash can")
   else console.log("car hits trash can")
 }
-slap(input)
+
+racoonDoesWhat(input)
